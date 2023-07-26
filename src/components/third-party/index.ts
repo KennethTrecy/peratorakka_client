e64d3f1ae91742b4b1b735eb9bc03b3f237dedf7
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-26 22:13:28 +0800

    internal(shell): import dependencies to style

---
+++ src/components/third-party/index.ts
@@ -0,0 +1,2 @@
+import beercss from "beercss/dist/cdn/beer.min.js"
+import materialDynamicColors from "material-dynamic-colors/dist/cdn/material-dynamic-colors.min.js"
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
+	import "@/components/third-party"
 </script>
 
 <section>
 	<slot></slot>
 </section>
+
+<style lang="scss">
+	@import "@/components/third-party/index"
+</style>
