81431509d8c90cc6033a0dfb1669ba4340223039
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2023-07-26 21:00:59 +0800

    dx(lint): set up the linting
    The configuration was mostly inspired from my [personal website's
    configuration].
    
    [personal website's configuration]: https://github.com/KennethTrecy/kennethtrecy.pages.dev/blob/master/.eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+const { resolve } = require("path");
+
+module.exports = {
+	"parserOptions": {
+		"ecmaVersion": 2015,
+		"sourceType": "module",
+		"project": [
+			"tsconfig.json"
+		],
+		"tsconfigRootDir": __dirname,
+		"extraFileExtensions": [ ".svelte" ],
+	},
+	"ignorePatterns": [ ".eslintrc.cjs", "*.config.js" ],
+	"env": {
+		"browser": true,
+		"es6": true,
+		"node": true
+	},
+	"plugins": [
+		"@typescript-eslint"
+	],
+	"parser": "@typescript-eslint/parser",
+	"overrides": [
+		{
+			"files": [
+				"*.svelte"
+			],
+			"parser": "svelte-eslint-parser",
+			"parserOptions": {
+				"parser": "@typescript-eslint/parser",
+			}
+		}
+	],
+	"extends": [
+		"eslint:all",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/recommended-requiring-type-checking",
+		"plugin:svelte/recommended",
+		"whitelistyle"
+	]
+}
