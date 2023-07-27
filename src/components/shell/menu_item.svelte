e6c13d006f61c568f4b2eea5038ec9f345ff6caa
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-27 23:23:20 +0800

    internal(shell): make generalization of menu item

---
+++ src/components/shell/menu_item.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { MenuItemInfo } from "@/components/shell/types"
+
+	let info: MenuItemInfo
+</script>
+
+<a href={info.link}>
+	<i>{info.icon}</i>
+	<span>{info.label}</span>
+</a>
