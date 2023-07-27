043e29163802cfa9476c9e84493a051d3f9542f9
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-27 20:40:08 +0800

    style(shell): ensure main has maximum height

--- src/components/shell.svelte
+++ src/components/shell.svelte
@@ -13,28 +13,48 @@
 	</style>
 </svelte:head>
 
-<header class="primary-container">
-	<nav>
-		<button class="circle transparent">
-			<i>menu</i>
-		</button>
-		<p class="max center-align">Peratorakka</p>
-		<button class="circle transparent">
-			<img class="responsive" src="" alt="Peratorakka logo"/>
-		</button>
-	</nav>
-</header>
-<main>
-	<slot name="main"></slot>
-</main>
-<footer class="">
-	Copyright © 2023 by Kenneth Trecy Tobias.
-</footer>
+<div class="shell">
+	<header class="primary-container">
+		<nav>
+			<button class="circle transparent">
+				<i>menu</i>
+			</button>
+			<p class="max center-align">Peratorakka</p>
+			<button class="circle transparent">
+				<img class="responsive" src="" alt="Peratorakka logo"/>
+			</button>
+		</nav>
+	</header>
+	<main>
+		<slot name="main"></slot>
+	</main>
+	<footer class="responsive max primary medium-line">
+		<p class="small-text">
+			Copyright © 2023 by Kenneth Trecy Tobias.
+		</p>
+	</footer>
+</div>
 
 <style lang="scss">
 	@use "@/components/third-party/index";
 
+	.shell {
+		width: 100%;
+		height: 100%;
+
+		display: flex;
+		flex-flow: column nowrap;
+
+		> main {
+			flex: 1;
+		}
+	}
+
 	p {
 		@extend h5
+	}
+
+	footer {
+		padding: 1rem;
 	}
 </style>
