e2c856aeed73f3203da74b025619accd409dadfa
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-29 20:29:46 +0800

    internal(component): include user email in component

--- src/components/public/global_state.ts
+++ src/components/public/global_state.ts
@@ -2,19 +2,25 @@ import type { Unsubscriber } from "svelte/store"
 
 import { derived, writable } from "svelte/store"
 
-import { SERVER_URL_KEY, CSRF_TOKEN_KEY } from "#/storage_keys"
+import { SERVER_URL_KEY, CSRF_TOKEN_KEY, USER_EMAIL_KEY } from "#/storage_keys"
 
 export const hasLoadedGlobalStates = writable<boolean>(false)
 export const serverURL = writable<string>("")
 export const CSRFToken = writable<string>("")
+export const userEmail = writable<string>("")
 
 export const hasRequirements = writable<boolean>(false)
 export const mustHaveToken = writable<boolean>(false)
+export const mustHaveEmail = writable<boolean>(false)
 
 export const hasServer = derived(serverURL, currentServerURL => currentServerURL !== "")
 export const hasToken = derived(
 	[ hasServer, CSRFToken ],
 	([ hasServerCurrently,currentCSRFToken ]) => hasServerCurrently && currentCSRFToken !== ""
+)
+export const hasEmail = derived(
+	[ hasToken, userEmail ],
+	([ hasTokenCurrently, currentUserEmail ]) => hasTokenCurrently && currentUserEmail !== ""
 )
 
 export const serverIcon = derived<string>(
@@ -30,6 +36,7 @@ export const serverIcon = derived<string>(
 
 let stopStoringServerURL: Unsubscriber = () => null as void
 let stopStoringCSRFToken: Unsubscriber = () => null as void
+let stopStoringUserEmail: Unsubscriber = () => null as void
 
 export const redirectPath = derived<string>(
 	[
@@ -59,10 +66,12 @@ export const redirectPath = derived<string>(
 export function initializeGlobalStates() {
 	const storedServerURL = window.localStorage.getItem(SERVER_URL_KEY) ?? ""
 	const storedCSRFToken = window.localStorage.getItem(CSRF_TOKEN_KEY) ?? ""
+	const storedUserEmail = window.localStorage.getItem(USER_EMAIL_KEY) ?? ""
 
 	setTimeout(() => {
 		serverURL.set(storedServerURL)
 		CSRFToken.set(storedCSRFToken)
+		userEmail.set(storedUserEmail)
 		hasLoadedGlobalStates.set(true)
 	}, 250)
 
@@ -75,6 +84,12 @@ export function initializeGlobalStates() {
 	stopStoringCSRFToken = CSRFToken.subscribe(newCSRFToken => {
 		if (typeof window !== "undefined") {
 			window.localStorage.setItem(CSRF_TOKEN_KEY, newCSRFToken)
+			if (newCSRFToken === "") userEmail.set("")
+		}
+	})
+	stopStoringUserEmail = userEmail.subscribe(newUserEmail => {
+		if (typeof window !== "undefined") {
+			window.localStorage.setItem(USER_EMAIL_KEY, newUserEmail)
 		}
 	})
 }
@@ -83,5 +98,6 @@ export function unsubscribeWatchedGlobalStates() {
 	if (typeof window !== "undefined") {
 		stopStoringServerURL()
 		stopStoringCSRFToken()
+		stopStoringUserEmail()
 	}
 }
