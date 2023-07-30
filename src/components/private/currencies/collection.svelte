be567c51e934d3ef17e4f9d5553d7cd07279b103
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-30 23:23:32 +0800

    feat(currency): show currency collection

---
+++ src/components/private/currencies/collection.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import type { Entity } from "%/currencies/types"
+
+	export let data: Entity[]
+
+	$: hasEntries = data.length > 0
+</script>
+
+<section>
+	<h2>Available Currencies</h2>
+	{#if hasEntries}
+		<p>
+			Below are the currencies that you have added on to your account.
+			They can be used to be associated to financial accounts.
+		</p>
+		{#each data as entity(entity.id)}
+			<article class="primary-container">
+				<h3>{entity.code}</h3>
+				<p>{entity.name}</p>
+			</article>
+		{/each}
+	{:else}
+		<p>
+			There are no available currencies at the momenet.
+			Create a currency to start.
+		</p>
+	{/if}
+</section>
+
+<style lang="scss">
+	@use "@/components/third-party/index";
+
+	h3 {
+		@extend h5;
+	}
+</style>
--- src/routes/currencies/+page.svelte
+++ src/routes/currencies/+page.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
-
+	import Collection from "%/currencies/collection.svelte"
 </script>
 
 <svelte:head>
 	<title>Currencies</title>
 </svelte:head>
+
+<article>
+	<h1>Your Currencies</h1>
+	<Collection data={[]}/>
+</article>
+
+<style lang="scss">
+	@use "@/components/third-party/index";
+
+	article {
+		@extend .medium-margin;
+		margin-left: 0em;
+		margin-right: 0em;
+	}
+</style>
