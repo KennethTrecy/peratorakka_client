8a5deba518d24bb8fe49ac264a2783c3aaf5ee9d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-31 17:16:24 +0800

    test(unit): ensure mechanism of password field

---
+++ src/components/public/form/password_field.spec.ts
@@ -0,0 +1,87 @@
+// @vitest-environment jsdom
+
+import { describe, it, expect } from "vitest"
+import userEvent from "@testing-library/user-event"
+import { act, render, cleanup } from "@testing-library/svelte"
+
+import Component from "./password_field.svelte"
+
+describe("Password field behavior", () => {
+	it("can render with no value", async () => {
+		const user = userEvent.setup()
+		const props = {
+			"fieldName": "",
+			"disabled": false,
+			"value": "",
+			"errors": [],
+		}
+		const { container, getByRole } = render(Component, props)
+
+		const textBox = getByRole("textbox")
+
+		expect(textBox.classList.contains("active")).toBeFalsy()
+
+		cleanup()
+	})
+
+	it("can render with new value", async () => {
+		const user = userEvent.setup()
+		const props = {
+			"fieldName": "",
+			"disabled": false,
+			"value": "",
+			"errors": [],
+		}
+		const { container, getByRole } = render(Component, props)
+
+		const textBox = getByRole("textbox")
+		await user.type(textBox, "Hello world")
+
+		expect(textBox.classList.contains("active")).toBeTruthy()
+
+		cleanup()
+	})
+
+	it("can render with no error", async () => {
+		const user = userEvent.setup()
+		const props = {
+			"fieldName": "hello",
+			"disabled": false,
+			"value": "",
+			"errors": [
+				{
+					"message": "world"
+				}
+			]
+		}
+		const { container } = render(Component, props)
+
+		const paragraph = container.querySelector("p")
+
+		expect(paragraph).toBeNull()
+
+		cleanup()
+	})
+
+	it("can render with error", async () => {
+		const user = userEvent.setup()
+		const props = {
+			"fieldName": "hello",
+			"disabled": false,
+			"value": "",
+			"errors": [
+				{
+					"field": "hello",
+					"message": "world"
+				}
+			]
+		}
+		const { container } = render(Component, props)
+
+		const paragraph = container.querySelector("p")
+
+		expect(paragraph).not.toBeNull()
+
+		cleanup()
+	})
+})
