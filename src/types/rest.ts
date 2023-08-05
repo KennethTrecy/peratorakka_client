1977b79cd21b3b945d3c2303122062d5e587a889
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-05 18:07:46 +0800

    internal(component): make types for requester

--- src/types/rest.ts
+++ src/types/rest.ts
@@ -1,3 +1,6 @@
+import type { Writable } from "svelte/store"
+import type { GeneralError } from "+/rest"
+
 export interface SimpleError {
 	message: string
 }
@@ -9,3 +12,21 @@ export interface FieldError extends SimpleError {
 export type GeneralError =
 	| SimpleError
 	| FieldError
+
+interface RequesterCase {
+	status: number,
+	action: () => void
+}
+
+export interface RequesterConstraints {
+	path: string,
+	defaultRequestConfiguration: Partial<Request>,
+	manualResponseHandlers: RequesterCase[],
+	expectedErrorStatusCodes: number[]
+}
+
+export interface RequesterInfo {
+	isConnecting: Writable<boolean>,
+	errors: Writable<GeneralError[]>,
+	send: (requestInfo: Partial<Request>) => void
+}
