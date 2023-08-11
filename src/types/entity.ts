63b866b875e30d2279612dd3a20c93b1905a8656
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-11 22:09:35 +0800

    internal(type): type the account entity

--- src/types/entity.ts
+++ src/types/entity.ts
@@ -1,7 +1,23 @@
+import { acceptableAccountKinds, accountKinds } from "#/entity"
+
 export interface Currency {
 	id: number
 	name: string
 	code: string
+	created_at: string
+	updated_at: string
+}
+
+type AccountKind = typeof accountKinds[number]
+
+export type AcceptableAccountKind = typeof acceptableAccountKinds[number]
+
+export interface Account {
+	id: number
+	currency_id: number
+	name: string
+	description: string
+	kind: AccountKind
 	created_at: string
 	updated_at: string
 }
