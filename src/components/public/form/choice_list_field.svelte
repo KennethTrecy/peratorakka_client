1147b43847aba91933793c9f50b0b06e41dc4a27
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-12 14:40:35 +0800

    internal(component): make choice list field

---
+++ src/components/public/form/choice_list_field.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import type { ChoiceInfo } from "+/component"
+	import type { FieldError, GeneralError } from "+/rest"
+
+	export let fieldName: string
+	export let disabled: boolean
+	export let value: string
+	export let IDPrefix: string = ""
+	export let errors: GeneralError[]
+	export let rawChoices: unknown[]
+	export let choiceConverter: (choice: unknown) => ChoiceInfo
+
+	$: normalizedFieldName = fieldName.replace(" ", "_").toLocaleLowerCase()
+	$: fieldID = (
+		IDPrefix === ""
+			? ""
+			: `${IDPrefix}_`
+	) + normalizedFieldName
+	$: message = errors.filter(
+		error => isFieldError(error) && error.field.endsWith(normalizedFieldName)
+	).map(error => error.message).join(" ")
+	$: choices = rawChoices.map(choiceConverter)
+
+	function isFieldError(error: any): error is FieldError {
+		return Object.keys(error).includes("field")
+	}
+</script>
+
+<div class="field label border no-margin">
+	<select
+		id={fieldID}
+		class="active"
+		{disabled}
+		bind:value={value}>
+		{#each choices as { label, data }(data)}
+			<option value={data}>
+				{label}
+			</option>
+		{/each}
+	</select>
+	<label class="active" for={fieldID}>{fieldName}</label>
+	{#if message !== ""}
+		<p class="error no-margin">{message}</p>
+	{/if}
+</div>
+
+<style lang="scss">
+	@use "@/components/third-party/index";
+
+	p {
+		@extend span;
+	}
+</style>
