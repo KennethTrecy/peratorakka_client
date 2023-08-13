7015432cf1c2e9852acbebdc0d9a238e059ebe20
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-13 16:31:43 +0800

    feat(modifier): specialize description of add form

--- src/components/private/modifiers/add_form.svelte
+++ src/components/private/modifiers/add_form.svelte
@@ -69,20 +69,24 @@
 <DescriptiveForm individualName="Financial Account" {mayShowForm}>
 	<svelte:fragment slot="description">
 		<JustifiedParagraph>
-			Financial accounts are some kind of label for the numerical values in a financial entry.
-			Some examples of these are capital, cash, or debt. They may be credited or debited
-			depending on their kind. They may be asset, liability, or equity to name a few.
+			Modifiers are premade actions to create financial entries. Currently, manual input is the
+			only kind of modifier available. Other kinds may added in the future to allow automated
+			modifications on different accounts.
 		</JustifiedParagraph>
 		<JustifiedParagraph>
-			To create a financial account to be used in the system, choose a currency and kind in order
-			for the system to calculate properly the associated numerical values. After that, fill out
-			other required info. Finally, press "Add" button.
+			Modifiers can also serve as references for financial entries. Everyone reading the entries
+			would know if a financial entry was a result of manual input or automated calculations.
+		</JustifiedParagraph>
+		<JustifiedParagraph>
+			To create a modifier to be used by the system, choose a debit account and a credit account
+			that the modifier would be responsible. After that, fill out other required info. Finally,
+			press "Add" button.
 		</JustifiedParagraph>
 	</svelte:fragment>
 	<svelte:fragment slot="requirement">
 		<JustifiedParagraph>
-			At least one currency must exist in the profile to show the form for creating financial
-			accounts.
+			At least two financial accounts must exist in the profile to show the form for creating
+			modifiers.
 		</JustifiedParagraph>
 	</svelte:fragment>
 	<BasicForm
