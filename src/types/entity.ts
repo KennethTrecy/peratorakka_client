e53a120045622ff1a51bcb685c9c6918c4468200
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-13 13:01:22 +0800

    refactor(type): generalize entity

--- src/types/entity.ts
+++ src/types/entity.ts
@@ -1,23 +1,23 @@
 import { acceptableAccountKinds, accountKinds } from "#/entity"
 
-export interface Currency {
+interface Entity {
 	id: number
-	name: string
-	code: string
 	created_at: string
 	updated_at: string
+}
+
+export interface Currency extends Entity {
+	name: string
+	code: string
 }
 
 type AccountKind = typeof accountKinds[number]
 
 export type AcceptableAccountKind = typeof acceptableAccountKinds[number]
 
-export interface Account {
-	id: number
+export interface Account extends Entity{
 	currency_id: number
 	name: string
 	description: string
 	kind: AccountKind
-	created_at: string
-	updated_at: string
 }
