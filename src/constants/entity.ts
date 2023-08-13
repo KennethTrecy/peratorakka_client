cde0cec63c705a29621e0ac5a872ac67f764a4cc
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-13 14:22:10 +0800

    internal(constant): add possible kinds of the modifiers

--- src/constants/entity.ts
+++ src/constants/entity.ts
@@ -10,3 +10,15 @@ export const accountKinds = [
 	"unknown",
 	...acceptableAccountKinds
 ] as const
+
+export const acceptableModifierKinds = [
+	// Below are the kinds not yet supported
+	// "reactive",
+	// "dependent",
+	"manual"
+] as const
+
+export const modifierKinds = [
+	"unknown",
+	...acceptableModifierKinds
+] as const
