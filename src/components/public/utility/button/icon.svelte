ce13c70fb968d618ac14a59e2d68bf077319a404
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-16 21:32:29 +0800

    feat(component): add ripple to icon button

--- src/components/public/utility/button/icon.svelte
+++ src/components/public/utility/button/icon.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { onMount } from "svelte"
+	import { MDCRipple } from "@material/ripple"
+
 	export let label: string
 	export let icon: string
 	let otherClasses: string
@@ -9,12 +12,21 @@
 		"mdc-icon-button",
 		...otherClasses.split(" ")
 	].filter(Boolean).join(" ")
+
+	let buttonElement: any
+	onMount(() => {
+		const button = new MDCRipple(buttonElement)
+		button.unbounded = true
+	})
 </script>
 
 <button
 	class={buttonClasses}
 	aria-label={label}
-	on:click>
+	on:click
+	bind:this={buttonElement}>
+	<div class="mdc-icon-button__ripple"></div>
+	<span class="mdc-icon-button__focus-ring"></span>
 	<i>{icon}</i>
 </button>
 
