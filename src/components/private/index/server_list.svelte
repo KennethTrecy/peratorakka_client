b417b60f2f9c4e71a5519506962783ad62a29627
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-19 13:16:32 +0800

    feat(home): redesign server list form

--- src/components/private/index/server_list.svelte
+++ src/components/private/index/server_list.svelte
@@ -1,22 +1,40 @@
 <script lang="ts">
+	import type { ChoiceInfo } from "+/component"
+
 	import { PUBLIC_PRODUCTION_SERVER_CHOICES } from "$env/static/public"
 
 	import { serverURL, CSRFToken, hasServer, hasToken, serverIcon } from "$/global_state"
 
+	import ServerDisplay from "$/utility/server_display.svelte"
+	import SingleForm from "$/form/single_form.svelte"
+	import TextCardButton from "$/button/card/text.svelte"
+	import TextField from "$/form/text_field.svelte"
+	import ChoiceListField from "$/form/choice_list_field.svelte"
+
 	const CUSTOM_KEY = "custom"
 
 	const rawServerChoices: string[] = PUBLIC_PRODUCTION_SERVER_CHOICES.split(",")
-	const domainNameRegex = /https?:\/\/(.+)/
-	const serverChoices = rawServerChoices.map(rawServer => ({
-		"URL": rawServer,
-		"name": (domainNameRegex.exec(rawServer) ?? [ null, rawServer ])[1]
-	}))
-	let selectedServer = serverChoices[0].URL ?? CUSTOM_KEY
+	let selectedServer = rawServerChoices[0] ?? CUSTOM_KEY
 	let customServer = ""
 	$: mustBeCustomServer = selectedServer === CUSTOM_KEY
 	$: resolvedSelectedServer = mustBeCustomServer
-		? customServer
+	? customServer
 		: selectedServer
+	$: serverChoices = [
+		...rawServerChoices,
+		CUSTOM_KEY
+	]
+	const domainNameRegex = /https?:\/\/(.+)/
+	function transformServer(server: string): ChoiceInfo {
+		let label = "Custom"
+		let data = CUSTOM_KEY
+		if (server !== CUSTOM_KEY) {
+			label = (domainNameRegex.exec(server) ?? [ null, server ])[1]
+			data = server
+		}
+
+		return { label, data }
+	}
 
 	let isConnecting = false
 	let didConnectionFail = false
@@ -49,95 +67,92 @@
 	}
 </script>
 
-<form
-	class="sever_list_form middle-align center-align grid large-margin"
-	on:submit={connect}>
-	<div class="s1 m2 l3"></div>
-	<div class="s10 m8 l6">
-		<div class="fill middle-align center-align">
-			<div class="center-align large-padding">
-				<i class="extra">{$serverIcon}</i>
-				{#if $hasToken}
-					<h1 class="connected">You are currently connected to <code>{$serverURL}</code></h1>
-					<p>You may change your current server</p>
-					<p><strong>Note</strong>: Doing so may log out any current account</p>
-				{:else if $hasServer}
-					<h1 class="disconnected">The client is trying to connect to the server</h1>
-					<p>Please wait for a moment</p>
-				{:else if didConnectionFail}
-					<h1 class="disconnected">The client cannot connect to the server</h1>
-					<p>Please look for another compatible server</p>
-				{:else}
-					<h1 class="disconnected">You are not yet connected to any server</h1>
-					<p>Choose or specify a server you want to connect</p>
-				{/if}
-				<div class="space"></div>
-				<fieldset class="no-space center-align">
-					<div class="field label suffix small">
-						<select
-							id="server_choices"
-							class="active"
-							disabled={isConnecting}
-							bind:value={selectedServer}>
-							{#each serverChoices as { URL, name }(URL)}
-								<option value={URL}>
-									{name}
-								</option>
-							{/each}
-							<option value={CUSTOM_KEY}>Custom</option>
-						</select>
-						<label for="server_choices" class="active">Servers</label>
-						<i>arrow_drop_down</i>
-					</div>
-				</fieldset>
-				{#if mustBeCustomServer}
-					<div class="space"></div>
-					<div class="no-space center-align">
-						<div class="field border">
-							<input type="text" bind:value={customServer} disabled={isConnecting}>
-							<span class="helper">Example: http://peratorakka.example.com</span>
-						</div>
-					</div>
-				{/if}
-				<div class="space"></div>
-				<button type="submit" disabled={isConnecting}>
-					{#if $hasToken}
-						Reconnect
-					{:else}
-						Connect
-					{/if}
-				</button>
+<SingleForm	on:submit={connect}>
+	<div class="mdc-typography" slot="description_layer">
+		{#if $hasToken}
+			<h1 class="mdc-typography--body2">
+				You are currently connected to <ServerDisplay address={$serverURL}/>.
+			</h1>
+			<p class="mdc-typography--body2">
+				You may change your current server.
+			</p>
+			<p class="mdc-typography--body2">
+				<strong>Note</strong>: Doing so may log out any current account.
+			</p>
+		{:else if $hasServer}
+			<h1 class="mdc-typography--headline6">The client is trying to connect to the server</h1>
+			<p class="mdc-typography--body2">
+				Please wait for a moment.
+			</p>
+		{:else if didConnectionFail}
+			<h1 class="mdc-typography--headline6">The client cannot connect to the server</h1>
+			<p class="mdc-typography--body2">
+				Please look for another compatible server.
+			</p>
+		{:else}
+			<h1 class="mdc-typography--headline6">You are not yet connected to any server</h1>
+			<p class="mdc-typography--body2">
+				Choose or specify a server you want to connect.
+			</p>
+		{/if}
+	</div>
+	<fieldset class="mdc-layout-grid" slot="field_layer">
+		<div class="mdc-layout-grid__inner">
+			<div class="mdc-layout-grid__cell mdc-layout-grid__cell--full">
+				<ChoiceListField
+					fieldName="Servers"
+					disabled={isConnecting}
+					bind:value={selectedServer}
+					rawChoices={serverChoices}
+					choiceConverter={transformServer}
+					IDPrefix="server_"
+					errors={[]}/>
 			</div>
+			{#if mustBeCustomServer}
+				<div class="mdc-layout-grid__cell mdc-layout-grid__cell--full">
+					<TextField
+						fieldName="Example: http://peratorakka.example.com"
+						disabled={isConnecting}
+						bind:value={customServer}
+						errors={[]}/>
+				</div>
+			{/if}
+		</div>
+	</fieldset>
+	<div class="mdc-card__actions" slot="action_layer">
+		<div class="mdc-card__action-buttons">
+			<TextCardButton
+				kind="submit"
+				disabled={isConnecting}
+				label={$hasToken ? "Reconnect" : "Connect"}/>
 		</div>
 	</div>
-</form>
+</SingleForm>
 
 <style lang="scss">
-	@use "@/components/third-party/index";
+	@use "@/components/third-party/new_index";
 
-	.sever_list_form {
-		width: 100%;
-		height: 100%;
-	}
+	@use "@material/card";
+	@use "@material/typography/mdc-typography";
+	@use "@material/layout-grid/mdc-layout-grid";
 
-	h1 {
-		&.disconnected {
-			@extend h5;
-		}
+	@include card.core-styles;
 
-		&.connected {
-			@extend p;
-			@extend .medium-text;
-			display: block;
+	.mdc-typography {
+		margin-top: 1rem;
+
+		> * {
+			margin-top: 1rem;
 		}
 	}
 
-	fieldset {
-		@extend nav;
+	.mdc-layout-grid__inner {
+		gap: 1em;
 	}
 
-	.fill {
-		height: max-content;
-		transition: height 250ms ease-in;
+	.mdc-layout-grid__cell--full {
+		@extend .mdc-layout-grid__cell--span-4-phone;
+		@extend .mdc-layout-grid__cell--span-8-tablet;
+		@extend .mdc-layout-grid__cell--span-12-desktop;
 	}
 </style>
