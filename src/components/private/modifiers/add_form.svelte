060569cbe02203349d5da36807f6ca57fb39133d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-19 21:26:06 +0800

    feat(modifier): redesign add form

--- src/components/private/modifiers/add_form.svelte
+++ src/components/private/modifiers/add_form.svelte
@@ -10,8 +10,9 @@
 
 	import BasicForm from "%/modifiers/basic_form.svelte"
 	import DescriptiveForm from "$/form/descriptive_form.svelte"
-	import MarginlessButton from "$/utility/marginless_button.svelte"
-	import JustifiedParagraph from "$/utility/justified_paragraph.svelte"
+	import ElementalParagraph from "$/typography/elemental_paragraph.svelte"
+	import TextCardButton from "$/button/card/text.svelte"
+	import TextContainer from "$/typography/text_container.svelte"
 
 	const dispatch = createEventDispatcher<{
 		"create": Modifier
@@ -68,27 +69,27 @@
 </script>
 
 <DescriptiveForm individualName="Financial Account" {mayShowForm}>
-	<svelte:fragment slot="description">
-		<JustifiedParagraph>
+	<TextContainer slot="description">
+		<ElementalParagraph>
 			Modifiers are premade actions to create financial entries. Currently, manual input is the
 			only kind of modifier available. Other kinds may added in the future to allow automated
 			modifications on different accounts.
-		</JustifiedParagraph>
-		<JustifiedParagraph>
+		</ElementalParagraph>
+		<ElementalParagraph>
 			Modifiers can also serve as references for financial entries. Everyone reading the entries
 			would know if a financial entry was a result of manual input or automated calculations.
-		</JustifiedParagraph>
-		<JustifiedParagraph>
+		</ElementalParagraph>
+		<ElementalParagraph>
 			To create a modifier to be used by the system, choose a debit account and a credit account
 			that the modifier would be responsible. After that, fill out other required info. Finally,
 			press "Add" button.
-		</JustifiedParagraph>
-	</svelte:fragment>
+		</ElementalParagraph>
+	</TextContainer>
 	<svelte:fragment slot="requirement">
-		<JustifiedParagraph>
+		<ElementalParagraph>
 			At least two financial accounts must exist in the profile to show the form for creating
 			modifiers.
-		</JustifiedParagraph>
+		</ElementalParagraph>
 	</svelte:fragment>
 	<BasicForm
 		slot="form"
@@ -102,10 +103,11 @@
 		{IDPrefix}
 		errors={$errors}
 		on:submit={createModifier}>
-		<svelte:fragment slot="buttonGroup">
-			<MarginlessButton kind="submit" disabled={$isConnecting}>
-				Add
-			</MarginlessButton>
+		<svelte:fragment slot="button_group">
+			<TextCardButton
+				kind="submit"
+				disabled={$isConnecting}
+				label="Add"/>
 		</svelte:fragment>
 	</BasicForm>
 </DescriptiveForm>
