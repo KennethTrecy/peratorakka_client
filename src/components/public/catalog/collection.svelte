423de33b03e63d36b67cac71e4586e5ac0c9bd9f
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-19 20:21:22 +0800

    feat(component): add progress bar

--- src/components/public/catalog/collection.svelte
+++ src/components/public/catalog/collection.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	import CurrencyCard from "%/currencies/currency_card.svelte"
+	import { onMount } from "svelte"
+	import { MDCLinearProgress } from "@material/linear-progress"
+
 	import ElementalParagraph from "$/typography/elemental_paragraph.svelte"
 	import GridCell from "$/layout/grid_cell.svelte"
 	import SecondaryHeading from "$/typography/secondary_heading.svelte"
@@ -9,15 +11,40 @@
 	export let data: unknown[]
 
 	$: hasData = data.length > 0
+	$: progressBarLabel = isConnecting
+		? `Loading ${collectiveName.toLocaleLowerCase()}...`
+		: `Finished attempt on loading ${collectiveName.toLocaleLowerCase()}.`
+	$: progressBarClasses = [
+		"mdc-linear-progress",
+		isConnecting ? "mdc-linear-progress--indeterminate" : "mdc-linear-progress--closed"
+	].join(" ")
+
+	let progressBar: MDCLinearProgress|null = null
+	let progressBarElement: any
+	onMount(() => {
+		progressBar = new MDCLinearProgress(progressBarElement)
+	})
 </script>
 
 <GridCell kind="full">
 	<SecondaryHeading>Available {collectiveName}</SecondaryHeading>
 </GridCell>
+<GridCell kind="full">
+	<div
+		role="progressbar"
+		class={progressBarClasses}
+		aria-label={progressBarLabel}
+		aria-valuemin={0}
+		aria-valuemax={1}
+		bind:this={progressBarElement}>
+		<div class="mdc-linear-progress__bar mdc-linear-progress__primary-bar">
+			<span class="mdc-linear-progress__bar-inner"></span>
+		</div>
+	</div>
+</GridCell>
 
 {#if isConnecting}
 	<GridCell kind="full">
-		<span class="loader large margin" role="progressbar"></span>
 		<ElementalParagraph>
 			Please wait while the client request the list from the server.
 		</ElementalParagraph>
@@ -38,4 +65,9 @@
 {/if}
 
 <style lang="scss">
+	@use "@/components/third-party/new_index";
+
+	@use "@material/linear-progress";
+
+	@include linear-progress.core-styles;
 </style>
