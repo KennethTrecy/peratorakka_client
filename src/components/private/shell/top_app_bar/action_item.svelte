e03c5be1d81fbba73538e6138b9f400dd6bb01bf
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-20 11:17:53 +0800

    refactor(component): globalize action item style

--- src/components/private/shell/top_app_bar/action_item.svelte
+++ src/components/private/shell/top_app_bar/action_item.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <IconButton
-	class="mdc-top-app-bar__action-item"
+	class="top_bar__action_item"
 	{label}
 	{icon}
 	on:click/>
@@ -15,4 +15,8 @@
 	@use "@/components/third-party/new_index";
 
 	@use "@material/top-app-bar/mdc-top-app-bar";
+
+	:global(.top_bar__action_item) {
+		@extend .mdc-top-app-bar__action-item;
+	}
 </style>
