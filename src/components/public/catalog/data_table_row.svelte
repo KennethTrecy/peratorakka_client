dea1cb4e52de68d8600dd1aa9bf19004d6831f1c
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-20 14:04:49 +0800

    internal(component): make table row component

---
+++ src/components/public/catalog/data_table_row.svelte
@@ -0,0 +1,12 @@
+<tr class="mdc-data-table__row">
+	<slot/>
+</tr>
+
+<style lang="scss">
+	@use "@/components/third-party/new_index";
+
+	@use "@material/data-table/data-table";
+
+	@include data-table.theme-baseline;
+	@include data-table.core-styles;
+</style>
