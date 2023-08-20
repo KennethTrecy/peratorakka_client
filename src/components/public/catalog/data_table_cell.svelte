47fecac5aae8f8060ee7ee61d37a6682f678d9c9
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-20 13:54:57 +0800

    internal(component): make table cell component

---
+++ src/components/public/catalog/data_table_cell.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { DataTableCellKind } from "+/component"
+
+	export let kind: DataTableCellKind = "normal"
+	export let doesRepresentRow: boolean = false
+
+	$: rowClasses = [
+		"mdc-data-table__cell",
+		kind === "numeric" ? "mdc-data-table__cell--numeric" : false
+	].filter(Boolean).join(" ")
+	$: scope = doesRepresentRow ? "row" : null
+</script>
+
+<td
+	class={rowClasses}
+	{scope}>
+	<slot/>
+</td>
+
+
+<style lang="scss">
+	@use "@/components/third-party/new_index";
+
+	@use "@material/data-table/data-table";
+
+	@include data-table.theme-baseline;
+	@include data-table.core-styles;
+</style>
