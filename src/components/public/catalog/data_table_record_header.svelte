1e0f0cc748610106dfd7d466fcd2bb29481b2fe9
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-20 14:21:13 +0800

    internal(component): make component for record headers

---
+++ src/components/public/catalog/data_table_record_header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import DataTableCell from "$/catalog/data_table_cell.svelte"
+</script>
+
+<DataTableCell scope="column">Name</DataTableCell>
+<DataTableCell scope="column">Debit Account</DataTableCell>
+<DataTableCell scope="column">Credit Account</DataTableCell>
+<slot name="special_cells"/>
+<DataTableCell scope="column">Actions</DataTableCell>
