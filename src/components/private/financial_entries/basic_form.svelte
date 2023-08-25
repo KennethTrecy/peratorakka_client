001d80ad2150876006911bb592f5ebbcdeac68a4
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-25 23:48:00 +0800

    internal(financial entry): make basic form

--- src/components/private/financial_entries/basic_form.svelte
+++ src/components/private/financial_entries/basic_form.svelte
@@ -4,85 +4,98 @@
 		AcceptableModifierKind,
 		AcceptableModifierAction,
 		Account,
-		Currency
+		Currency,
+		Modifier
 	} from "+/entity"
 
-	import { acceptableModifierKinds, acceptableModifierActions } from "#/entity"
-
 	import makeAccountTransformer from "$/form/choice_info_transformer/make_account_transformer"
-	import transformString from "$/form/choice_info_transformer/transform_string"
+	import transformModifier from "$/form/choice_info_transformer/transform_modifier"
 
 	import BasicForm from "$/form/basic_form.svelte"
 	import ChoiceListField from "$/form/choice_list_field.svelte"
 	import TextField from "$/form/text_field.svelte"
 
-	export const ACCEPTABLE_MODIFIER_KINDS = [ ...acceptableModifierKinds ]
-	export const ACCEPTABLE_MODIFIER_ACTIONS = [ ...acceptableModifierActions ]
-
 	export let IDPrefix: string
 	export let currencies: Currency[]
 	export let accounts: Account[]
+	export let modifiers: Modifier[]
 
-	export let debitAccountID: string
-	export let creditAccountID: string
-	export let name: string
-	export let description: string
-	export let kind: AcceptableModifierKind
-	export let action: AcceptableModifierAction
+	export let modifierID: string
+	export let transactedAt: string
+	export let debitAmount: string
+	export let creditAmount: string
+	export let remarks: string
 
 	export let isConnecting: boolean
 	export let errors: GeneralError[]
 	export let id: string|null = null
 
-	$: transformAccount = makeAccountTransformer(currencies)
+	$: chosenModifier = modifiers.find(
+		modifier => `${modifier.id}` === modifierID
+	)
+	$: debitAccount = chosenModifier && accounts.find(
+		account => account.id === chosenModifier?.debit_account_id
+	)
+	$: creditAccount = chosenModifier && accounts.find(
+		account => account.id === chosenModifier?.credit_account_id
+	)
+	$: debitCurrency = debitAccount && currencies.find(
+		currency => currency.id === debitAccount?.currency_id
+	)
+	$: creditCurrency = creditAccount && currencies.find(
+		currency => currency.id === creditAccount?.currency_id
+	)
+	$: hasSameCurrency = debitCurrency === creditCurrency
+	$: {
+		if (hasSameCurrency) creditAmount = debitAmount
+	}
 </script>
 
 <BasicForm {id} {isConnecting} {errors} on:submit>
 	<svelte:fragment slot="fields">
 		<ChoiceListField
-			fieldName="Debit Account"
-			errorFieldName="debit_account_id"
+			fieldName="Modifier"
+			errorFieldName="modifier_id"
 			disabled={isConnecting}
-			bind:value={debitAccountID}
-			rawChoices={accounts}
-			choiceConverter={transformAccount}
-			{IDPrefix}
-			{errors}/>
-		<ChoiceListField
-			fieldName="Credit Account"
-			errorFieldName="credit_account_id"
-			disabled={isConnecting}
-			bind:value={creditAccountID}
-			rawChoices={accounts}
-			choiceConverter={transformAccount}
-			{IDPrefix}
-			{errors}/>
-		<ChoiceListField
-			fieldName="Kind"
-			disabled={isConnecting}
-			bind:value={kind}
-			rawChoices={ACCEPTABLE_MODIFIER_KINDS}
-			choiceConverter={transformString}
-			{IDPrefix}
-			{errors}/>
-		<ChoiceListField
-			fieldName="Action"
-			disabled={isConnecting}
-			bind:value={action}
-			rawChoices={ACCEPTABLE_MODIFIER_ACTIONS}
-			choiceConverter={transformString}
+			bind:value={modifierID}
+			rawChoices={modifiers}
+			choiceConverter={transformModifier}
 			{IDPrefix}
 			{errors}/>
 		<TextField
-			fieldName="Name"
+			variant="date"
+			fieldName="Transaction Date"
+			errorFieldName="transacted_at"
 			disabled={isConnecting}
-			bind:value={name}
+			bind:value={transactedAt}
 			{IDPrefix}
 			{errors}/>
+		{#if hasSameCurrency}
+			<TextField
+				fieldName="Debit and Credit Amount"
+				errorFieldName="amount"
+				disabled={isConnecting}
+				bind:value={debitAmount}
+				{IDPrefix}
+				{errors}/>
+		{:else}
+			<TextField
+				fieldName="Debit Amount"
+				disabled={isConnecting}
+				bind:value={debitAmount}
+				{IDPrefix}
+				{errors}/>
+			<TextField
+				fieldName="Credit Amount"
+				disabled={isConnecting}
+				bind:value={creditAmount}
+				{IDPrefix}
+				{errors}/>
+		{/if}
 		<TextField
-			fieldName="Description"
+			fieldName="remarks"
 			disabled={isConnecting}
-			bind:value={description}
+			bind:value={remarks}
 			{IDPrefix}
 			{errors}/>
 	</svelte:fragment>
