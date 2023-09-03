4a8315e15f4e419d0495dd84d3af2a02a6903d5f
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-09-04 06:21:50 +0800

    refactor(frozen period): make customizable trial row

---
+++ src/components/private/frozen_periods/financial_statements/custom_trial_row.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import type { Currency } from "+/entity"
+
+	import formatAmount from "$/utility/format_amount"
+
+	import DataTableCell from "$/catalog/data_table_cell.svelte"
+	import DataTableHeader from "$/catalog/data_table_header.svelte"
+	import DataTableRow from "$/catalog/data_table_row.svelte"
+
+	export let rowName: string
+	export let currency: Currency|undefined
+	export let rawDebitAmount: string
+	export let rawCreditAmount: string
+
+	$: emptyAmount = formatAmount(
+		currency,
+		"0"
+	)
+	$: friendlyDebitAmount = formatAmount(
+		currency,
+		rawDebitAmount
+	)
+	$: friendlyCreditAmount = formatAmount(
+		currency,
+		rawCreditAmount
+	)
+	$: isPossiblyDebitAccount = friendlyDebitAmount !== emptyAmount
+	$: isPossiblyCreditAccount = friendlyCreditAmount !== emptyAmount
+	$: hasFilled = isPossiblyDebitAccount || isPossiblyCreditAccount
+
+	$: shownDebitAmount = isPossiblyDebitAccount ? friendlyDebitAmount : ""
+	$: shownCreditAmount = isPossiblyCreditAccount ? friendlyCreditAmount : ""
+</script>
+
+{#if hasFilled}
+	<DataTableRow>
+		<DataTableHeader scope="row">{rowName}</DataTableHeader>
+		<DataTableCell kind="numeric">{shownDebitAmount}</DataTableCell>
+		<DataTableCell kind="numeric">{shownCreditAmount}</DataTableCell>
+	</DataTableRow>
+{/if}
--- src/components/private/frozen_periods/financial_statements/trial_row.svelte
+++ src/components/private/frozen_periods/financial_statements/trial_row.svelte
@@ -4,11 +4,7 @@
 
 	import { UNKNOWN_ACCOUNT } from "#/component"
 
-	import formatAmount from "$/utility/format_amount"
-
-	import DataTableCell from "$/catalog/data_table_cell.svelte"
-	import DataTableHeader from "$/catalog/data_table_header.svelte"
-	import DataTableRow from "$/catalog/data_table_row.svelte"
+	import CustomTrialRow from "%/frozen_periods/financial_statements/custom_trial_row.svelte"
 
 	export let currency: Currency|undefined
 	export let accounts: Account[]
@@ -18,30 +14,16 @@
 	$: account = accounts.find(
 		account => account.id === data.account_id
 	) ?? UNKNOWN_ACCOUNT
-	$: emptyAmount = formatAmount(
-		currency,
-		"0"
-	)
-	$: friendlyDebitAmount = formatAmount(
-		currency,
-		kind === "unadjusted" ? data.unadjusted_debit_amount : data.adjusted_debit_amount
-	)
-	$: friendlyCreditAmount = formatAmount(
-		currency,
-		kind === "unadjusted" ? data.unadjusted_credit_amount : data.adjusted_credit_amount
-	)
-	$: isPossiblyDebitAccount = friendlyDebitAmount !== emptyAmount
-	$: isPossiblyCreditAccount = friendlyCreditAmount !== emptyAmount
-	$: hasFilled = isPossiblyDebitAccount || isPossiblyCreditAccount
-
-	$: shownDebitAmount = isPossiblyDebitAccount ? friendlyDebitAmount : ""
-	$: shownCreditAmount = isPossiblyCreditAccount ? friendlyCreditAmount : ""
+	$: rawDebitAmount = kind === "unadjusted"
+		? data.unadjusted_debit_amount
+		: data.adjusted_debit_amount
+	$: rawCreditAmount = kind === "unadjusted"
+		? data.unadjusted_credit_amount
+		: data.adjusted_credit_amount
 </script>
 
-{#if hasFilled}
-	<DataTableRow>
-		<DataTableHeader scope="row">{account.name}</DataTableHeader>
-		<DataTableCell kind="numeric">{shownDebitAmount}</DataTableCell>
-		<DataTableCell kind="numeric">{shownCreditAmount}</DataTableCell>
-	</DataTableRow>
-{/if}
+<CustomTrialRow
+	rowName={account.name}
+	{currency}
+	{rawDebitAmount}
+	{rawCreditAmount}/>
