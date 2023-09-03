5144bef0bb7234b4a02f9ebf92fbb90a0ed10e09
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-09-03 14:30:59 +0800

    refactor(component): allow data table to change name

---
+++ src/components/public/catalog/unnamed_data_table.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import ElementalParagraph from "$/typography/elemental_paragraph.svelte"
+	import GridCell from "$/layout/grid_cell.svelte"
+	import IndeterminateProgressBar from "$/utility/indeterminate_progress_bar.svelte"
+
+	export let isConnecting: boolean
+	export let collectiveName: string
+	export let data: unknown[]
+
+	$: hasData = data.length > 0
+	$: progressBarLabel = isConnecting
+		? `Loading ${collectiveName.toLocaleLowerCase()}...`
+		: `Finished attempt on loading ${collectiveName.toLocaleLowerCase()}.`
+</script>
+
+<GridCell kind="full">
+	<IndeterminateProgressBar
+		isLoading={isConnecting}
+		{progressBarLabel}/>
+	<slot name="name"/>
+</GridCell>
+
+{#if isConnecting}
+	<GridCell kind="full">
+		<ElementalParagraph>
+			Please wait while the client request the list from the server.
+		</ElementalParagraph>
+	</GridCell>
+{:else if hasData}
+	<GridCell kind="full">
+		<ElementalParagraph>
+			<slot name="filled_collection_description"/>
+		</ElementalParagraph>
+	</GridCell>
+	<GridCell kind="full">
+		<div class="mdc-data-table">
+			<div class="mdc-data-table__table-container">
+				<table class="mdc-data-table__table" aria-label="Dessert calories">
+					<thead>
+						<tr class="mdc-data-table__header-row">
+							<slot name="table_headers"/>
+						</tr>
+					</thead>
+					<tbody class="mdc-data-table__content">
+						<slot name="table_rows"/>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</GridCell>
+{:else}
+	<GridCell kind="full">
+		<ElementalParagraph>
+			<slot name="empty_collection_description"/>
+		</ElementalParagraph>
+	</GridCell>
+{/if}
+
+<style lang="scss">
+	@use "@/components/third-party/new_index";
+
+	@use "@material/data-table/data-table";
+
+	@include data-table.theme-baseline;
+	@include data-table.core-styles;
+
+	.mdc-data-table {
+		max-width: 100%;
+		overflow-x: scroll;
+	}
+</style>
