c681284106e4544952e9ce43c7a35a9a7b7dc70f
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-09-03 14:37:25 +0800

    internal(component): make tertiary heading

---
+++ src/components/public/typography/tertiary_heading.svelte
@@ -0,0 +1,13 @@
+<h3 class="mdc-typography--headline3">
+	<slot/>
+</h3>
+
+<style lang="scss">
+	@use "@/components/third-party/new_index";
+
+	@use "@material/typography/mdc-typography";
+
+	h3 {
+		text-align: center;
+	}
+</style>
