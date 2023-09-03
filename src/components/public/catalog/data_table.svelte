e1b93e4c30af5da80adfb74286dfb90bfa94d737
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-09-03 16:16:05 +0800

    refactor(component): make unit data table

--- src/components/public/catalog/data_table.svelte
+++ src/components/public/catalog/data_table.svelte
@@ -2,6 +2,7 @@
 	import CatalogBase from "$/catalog/base.svelte"
 	import GridCell from "$/layout/grid_cell.svelte"
 	import SecondaryHeading from "$/typography/secondary_heading.svelte"
+	import UnitDataTable from "$/catalog/unit_data_table.svelte"
 
 	export let isConnecting: boolean
 	export let collectiveName: string
@@ -16,19 +17,12 @@
 	<slot slot="filled_collection_description" name="filled_collection_description"/>
 	<slot slot="empty_collection_description" name="empty_collection_description"/>
 	<GridCell slot="available_content" kind="full">
-		<div class="mdc-data-table">
-			<div class="mdc-data-table__table-container">
-				<table class="mdc-data-table__table" aria-label="Dessert calories">
-					<thead>
-						<tr class="mdc-data-table__header-row">
-							<slot name="table_headers"/>
-						</tr>
-					</thead>
-					<tbody class="mdc-data-table__content">
-						<slot name="table_rows"/>
-					</tbody>
-				</table>
-			</div>
-		</div>
+		<UnitDataTable>
+			<slot slot="table_headers" name="table_headers"/>
+			<slot slot="table_rows" name="table_rows"/>
+			{#if $$slots.table_footer_cells}
+				<slot name="table_footer_cells"/>
+			{/if}
+		</UnitDataTable>
 	</GridCell>
 </CatalogBase>
---
+++ src/components/public/catalog/unit_data_table.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import DataTableRow from "$/catalog/data_table_row.svelte"
+</script>
+
+<div class="mdc-data-table">
+	<div class="mdc-data-table__table-container">
+		<table class="mdc-data-table__table" aria-label="Dessert calories">
+			<thead>
+				<tr class="mdc-data-table__header-row">
+					<slot name="table_headers"/>
+				</tr>
+			</thead>
+			<tbody class="mdc-data-table__content">
+				<slot name="table_rows"/>
+			</tbody>
+			{#if $$slots.table_footer_cells}
+				<tfoot>
+					<DataTableRow>
+						<slot name="table_footer_cells"/>
+					</DataTableRow>
+				</tfoot>
+			{/if}
+		</table>
+	</div>
+</div>
+
+<style lang="scss">
+	@use "@/components/third-party/new_index";
+
+	@use "@material/data-table/data-table";
+
+	@include data-table.theme-baseline;
+	@include data-table.core-styles;
+</style>
