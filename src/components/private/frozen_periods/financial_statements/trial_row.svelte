709afb16eadb61b9c3c1210883e0dec635a7b06a
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-09-03 14:59:41 +0800

    internal(frozen period): make trial row

---
+++ src/components/private/frozen_periods/financial_statements/trial_row.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import type { Currency, Account, SummaryCalculation } from "+/entity"
+
+	import { UNKNOWN_ACCOUNT } from "#/component"
+
+	import { formatAmount } from "!/index"
+
+	import DataTableCell from "$/catalog/data_table_cell.svelte"
+	import DataTableHeader from "$/catalog/data_table_header.svelte"
+	import DataTableRow from "$/catalog/data_table_row.svelte"
+
+	const minimumFractionDigits = 2
+	const maximumFractionDigits = 8
+
+	export let currencies: Currency[]
+	export let accounts: Account[]
+	export let data: Omit<SummaryCalculation, "frozen_period_id">
+	export let kind: "unadjusted"|"adjusted"
+
+	$: account = accounts.find(
+		account => account.id === data.account_id
+	) ?? UNKNOWN_ACCOUNT
+	$: currency = currencies.find(
+		currency => currency.id === account.currency_id
+	)
+	$: emptyAmount = formatAmount(
+		currency,
+		"0",
+		minimumFractionDigits,
+		maximumFractionDigits
+	)
+	$: friendlyDebitAmount = formatAmount(
+		currency,
+		kind === "unadjusted" ? data.unadjusted_debit_amount : data.adjusted_debit_amount,
+		minimumFractionDigits,
+		maximumFractionDigits
+	)
+	$: friendlyCreditAmount = formatAmount(
+		currency,
+		kind === "unadjusted" ? data.unadjusted_credit_amount : data.adjusted_credit_amount,
+		minimumFractionDigits,
+		maximumFractionDigits
+	)
+	$: isPossiblyDebitAccount = friendlyDebitAmount !== emptyAmount
+	$: isPossiblyCreditAccount = friendlyCreditAmount !== emptyAmount
+	$: hasFilled = isPossiblyDebitAccount || isPossiblyCreditAccount
+
+	$: shownDebitAmount = isPossiblyDebitAccount ? friendlyDebitAmount : ""
+	$: shownCreditAmount = isPossiblyCreditAccount ? friendlyCreditAmount : ""
+</script>
+
+{#if hasFilled}
+	<DataTableRow>
+		<DataTableHeader scope="row">{account.name}</DataTableHeader>
+		<DataTableCell>{shownDebitAmount}</DataTableCell>
+		<DataTableCell>{shownCreditAmount}</DataTableCell>
+	</DataTableRow>
+{/if}
