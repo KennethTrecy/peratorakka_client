a5281a0ffbe813027c92372875f77dd9c10fd94b
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-09-08 22:17:44 +0800

    feat(component): center the contents of data table

--- src/components/public/catalog/data_table.svelte
+++ src/components/public/catalog/data_table.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import CatalogBase from "$/catalog/base.svelte"
+	import Flex from "$/layout/flex.svelte"
 	import GridCell from "$/layout/grid_cell.svelte"
 	import SecondaryHeading from "$/typography/secondary_heading.svelte"
 	import UnitDataTable from "$/catalog/unit_data_table.svelte"
+	import TextContainer from "$/typography/text_container.svelte"
 
 	export let isConnecting: boolean
 	export let collectiveName: string
@@ -14,15 +16,25 @@
 	{collectiveName}
 	{data}>
 	<SecondaryHeading slot="name">Available {collectiveName}</SecondaryHeading>
-	<slot slot="filled_collection_description" name="filled_collection_description"/>
-	<slot slot="empty_collection_description" name="empty_collection_description"/>
+	<Flex slot="filled_collection_description" direction="column" mustPad={false}>
+		<TextContainer>
+			<slot name="filled_collection_description"/>
+		</TextContainer>
+	</Flex>
+	<Flex slot="empty_collection_description" direction="column" mustPad={false}>
+		<TextContainer>
+			<slot name="empty_collection_description"/>
+		</TextContainer>
+	</Flex>
 	<GridCell slot="available_content" kind="full">
-		<UnitDataTable>
-			<slot slot="table_headers" name="table_headers"/>
-			<slot slot="table_rows" name="table_rows"/>
-			{#if $$slots.table_footer_cells}
-				<slot name="table_footer_cells"/>
-			{/if}
-		</UnitDataTable>
+		<Flex direction="column" mustPad={false}>
+			<UnitDataTable>
+				<slot slot="table_headers" name="table_headers"/>
+				<slot slot="table_rows" name="table_rows"/>
+				{#if $$slots.table_footer_cells}
+					<slot name="table_footer_cells"/>
+				{/if}
+			</UnitDataTable>
+		</Flex>
 	</GridCell>
 </CatalogBase>
