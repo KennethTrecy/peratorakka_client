a7190723d91f4e111623bc3fa48293a7829dc78e
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-09-09 19:35:16 +0800

    internal(constant): add key for access token

--- src/constants/storage_keys.ts
+++ src/constants/storage_keys.ts
@@ -1,4 +1,5 @@
 export const THEME_MODE_KEY = "theme_mode"
 export const SERVER_URL_KEY = "server_url"
 export const CSRF_TOKEN_KEY = "csrf_token"
+export const ACCESS_TOKEN_KEY = "access_token"
 export const USER_EMAIL_KEY = "user_email_key"
