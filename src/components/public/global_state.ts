816d56e5a8cf5353c7d9dd070d27078814d2548b
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-09-09 20:08:43 +0800

    internal(component): add global state for access token metadata

--- src/components/public/global_state.ts
+++ src/components/public/global_state.ts
@@ -2,13 +2,20 @@ import type { Unsubscriber } from "svelte/store"
 
 import { derived, writable } from "svelte/store"
 
-import { SERVER_URL_KEY, CSRF_TOKEN_KEY, USER_EMAIL_KEY, ACCESS_TOKEN_KEY } from "#/storage_keys"
+import {
+	SERVER_URL_KEY,
+	CSRF_TOKEN_KEY,
+	USER_EMAIL_KEY,
+	ACCESS_TOKEN_KEY,
+	ACCESS_TOKEN_METADATA_KEY
+} from "#/storage_keys"
 
 export const hasLoadedGlobalStates = writable<boolean>(false)
 export const serverURL = writable<string>("")
 export const CSRFToken = writable<string>("")
 export const userEmail = writable<string>("")
 export const accessToken = writable<string>("")
+export const accessTokenMetadata = writable<Map<string, string>>(new Map())
 
 export const hasRequirements = writable<boolean>(false)
 export const mustHaveToken = writable<boolean>(false)
@@ -26,8 +33,14 @@ export const hasUser = derived(
 	([ hasTokenCurrently, currentUserEmail ]) => hasTokenCurrently && currentUserEmail !== ""
 )
 export const hasAccessToken = derived(
-	[ hasServer, accessToken ],
-	([ hasServerCurrently, currentAccessToken ]) => hasServerCurrently && currentAccessToken !== ""
+	[ hasServer, accessToken, accessTokenMetadata ],
+	([
+		hasServerCurrently,
+		currentAccessToken,
+		currentAccessTokenMetadata
+	]) => hasServerCurrently
+		&& currentAccessToken !== ""
+		&& currentAccessTokenMetadata.size > 0
 )
 
 export const serverIcon = derived<string>(
@@ -91,18 +104,21 @@ export const redirectPath = derived<string>(
 let stopStoringServerURL: Unsubscriber = () => null as void
 let stopStoringCSRFToken: Unsubscriber = () => null as void
 let stopStoringAccessToken: Unsubscriber = () => null as void
+let stopStoringAccessTokenMetadata: Unsubscriber = () => null as void
 let stopStoringUserEmail: Unsubscriber = () => null as void
 
 export function initializeGlobalStates() {
 	const storedServerURL = window.localStorage.getItem(SERVER_URL_KEY) ?? ""
 	const storedCSRFToken = window.localStorage.getItem(CSRF_TOKEN_KEY) ?? ""
 	const storedAccessToken = window.localStorage.getItem(ACCESS_TOKEN_KEY) ?? ""
+	const storedAccessTokenMetadata = window.localStorage.getItem(ACCESS_TOKEN_METADATA_KEY) ?? "[]"
 	const storedUserEmail = window.localStorage.getItem(USER_EMAIL_KEY) ?? ""
 
 	setTimeout(() => {
 		serverURL.set(storedServerURL)
 		CSRFToken.set(storedCSRFToken)
 		accessToken.set(storedAccessToken)
+		accessTokenMetadata.set(new Map(JSON.parse(storedAccessTokenMetadata)))
 		userEmail.set(storedUserEmail)
 		hasLoadedGlobalStates.set(true)
 	}, 250)
@@ -112,6 +128,7 @@ export function initializeGlobalStates() {
 			window.localStorage.setItem(SERVER_URL_KEY, newServerURL)
 			if (newServerURL === "") CSRFToken.set("")
 			if (newServerURL === "") accessToken.set("")
+			if (newServerURL === "") accessTokenMetadata.set(new Map())
 		}
 	})
 	stopStoringCSRFToken = CSRFToken.subscribe(newCSRFToken => {
@@ -126,6 +143,15 @@ export function initializeGlobalStates() {
 			if (newAccessToken === "") userEmail.set("")
 		}
 	})
+	stopStoringAccessTokenMetadata = accessTokenMetadata.subscribe(newAccessTokenMetadata => {
+		if (typeof window !== "undefined") {
+			window.localStorage.setItem(
+				ACCESS_TOKEN_METADATA_KEY,
+				JSON.stringify(Array.from(newAccessTokenMetadata.entries()))
+			)
+			if (newAccessTokenMetadata === "") accessToken.set("")
+		}
+	})
 	stopStoringUserEmail = userEmail.subscribe(newUserEmail => {
 		if (typeof window !== "undefined") {
 			window.localStorage.setItem(USER_EMAIL_KEY, newUserEmail)
@@ -138,6 +164,7 @@ export function unsubscribeWatchedGlobalStates() {
 		stopStoringServerURL()
 		stopStoringCSRFToken()
 		stopStoringAccessToken()
+		stopStoringAccessTokenMetadata()
 		stopStoringUserEmail()
 	}
 }
