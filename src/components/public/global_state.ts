aa87c540e19c8fff549ee41378aee53598d32144
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-09-09 19:42:48 +0800

    internal(component): add global state for access token

--- src/components/public/global_state.ts
+++ src/components/public/global_state.ts
@@ -2,26 +2,32 @@ import type { Unsubscriber } from "svelte/store"
 
 import { derived, writable } from "svelte/store"
 
-import { SERVER_URL_KEY, CSRF_TOKEN_KEY, USER_EMAIL_KEY } from "#/storage_keys"
+import { SERVER_URL_KEY, CSRF_TOKEN_KEY, USER_EMAIL_KEY, ACCESS_TOKEN_KEY } from "#/storage_keys"
 
 export const hasLoadedGlobalStates = writable<boolean>(false)
 export const serverURL = writable<string>("")
 export const CSRFToken = writable<string>("")
 export const userEmail = writable<string>("")
+export const accessToken = writable<string>("")
 
 export const hasRequirements = writable<boolean>(false)
 export const mustHaveToken = writable<boolean>(false)
+export const mustHaveAccessToken = writable<boolean>(false)
 export const mustBeGuest = writable<boolean>(false)
 export const mustBeAuthenticatedUser = writable<boolean>(false)
 
 export const hasServer = derived(serverURL, currentServerURL => currentServerURL !== "")
 export const hasToken = derived(
 	[ hasServer, CSRFToken ],
-	([ hasServerCurrently,currentCSRFToken ]) => hasServerCurrently && currentCSRFToken !== ""
+	([ hasServerCurrently, currentCSRFToken ]) => hasServerCurrently && currentCSRFToken !== ""
 )
 export const hasUser = derived(
 	[ hasToken, userEmail ],
 	([ hasTokenCurrently, currentUserEmail ]) => hasTokenCurrently && currentUserEmail !== ""
+)
+export const hasAccessToken = derived(
+	[ hasServer, accessToken ],
+	([ hasServerCurrently, currentAccessToken ]) => hasServerCurrently && currentAccessToken !== ""
 )
 
 export const serverIcon = derived<string>(
@@ -41,6 +47,8 @@ export const redirectPath = derived<string>(
 		hasRequirements,
 		mustHaveToken,
 		hasToken,
+		mustHaveAccessToken,
+		hasAccessToken,
 		mustBeGuest,
 		mustBeAuthenticatedUser,
 		hasUser
@@ -50,6 +58,8 @@ export const redirectPath = derived<string>(
 		doesHaveRequirements,
 		doesRequireToken,
 		isTokenPresent,
+		doesRequireAccessToken,
+		isAccessTokenPresent,
 		doesRequireGuest,
 		doesRequireAuthenticatedUser,
 		isAuthenticatedUserPresent
@@ -57,6 +67,10 @@ export const redirectPath = derived<string>(
 		if (wereGlobalStatesLoaded) {
 			if (doesHaveRequirements) {
 				if (doesRequireToken && !isTokenPresent) {
+					return "/"
+				}
+
+				if (doesRequireAccessToken && !isAccessTokenPresent) {
 					return "/"
 				}
 
@@ -76,16 +90,19 @@ export const redirectPath = derived<string>(
 
 let stopStoringServerURL: Unsubscriber = () => null as void
 let stopStoringCSRFToken: Unsubscriber = () => null as void
+let stopStoringAccessToken: Unsubscriber = () => null as void
 let stopStoringUserEmail: Unsubscriber = () => null as void
 
 export function initializeGlobalStates() {
 	const storedServerURL = window.localStorage.getItem(SERVER_URL_KEY) ?? ""
 	const storedCSRFToken = window.localStorage.getItem(CSRF_TOKEN_KEY) ?? ""
+	const storedAccessToken = window.localStorage.getItem(ACCESS_TOKEN_KEY) ?? ""
 	const storedUserEmail = window.localStorage.getItem(USER_EMAIL_KEY) ?? ""
 
 	setTimeout(() => {
 		serverURL.set(storedServerURL)
 		CSRFToken.set(storedCSRFToken)
+		accessToken.set(storedAccessToken)
 		userEmail.set(storedUserEmail)
 		hasLoadedGlobalStates.set(true)
 	}, 250)
@@ -94,12 +111,19 @@ export function initializeGlobalStates() {
 		if (typeof window !== "undefined") {
 			window.localStorage.setItem(SERVER_URL_KEY, newServerURL)
 			if (newServerURL === "") CSRFToken.set("")
+			if (newServerURL === "") accessToken.set("")
 		}
 	})
 	stopStoringCSRFToken = CSRFToken.subscribe(newCSRFToken => {
 		if (typeof window !== "undefined") {
 			window.localStorage.setItem(CSRF_TOKEN_KEY, newCSRFToken)
 			if (newCSRFToken === "") userEmail.set("")
+		}
+	})
+	stopStoringAccessToken = accessToken.subscribe(newAccessToken => {
+		if (typeof window !== "undefined") {
+			window.localStorage.setItem(ACCESS_TOKEN_KEY, newAccessToken)
+			if (newAccessToken === "") userEmail.set("")
 		}
 	})
 	stopStoringUserEmail = userEmail.subscribe(newUserEmail => {
@@ -113,6 +137,7 @@ export function unsubscribeWatchedGlobalStates() {
 	if (typeof window !== "undefined") {
 		stopStoringServerURL()
 		stopStoringCSRFToken()
+		stopStoringAccessToken()
 		stopStoringUserEmail()
 	}
 }
