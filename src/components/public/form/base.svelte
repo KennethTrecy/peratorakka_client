a55e4afe83256295460a544e01ce87f9c745498d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-09-10 13:46:25 +0800

    refactor(component): generalize base form

---
+++ src/components/public/form/base.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import type { GeneralError } from "+/rest"
+
+	import { isSimpleError } from "+/rest"
+
+	import IndeterminateProgressBar from "$/utility/indeterminate_progress_bar.svelte"
+	import ShortParagraph from "$/typography/short_paragraph.svelte";
+
+	export let errors: GeneralError[]
+	export let id: string|null
+	export let isConnecting: boolean
+
+	$: progressBarLabel = isConnecting
+		? "Waiting for server's response..."
+		: (
+			errors.length > 0
+			? "Failed to submit the details. Please check the errors."
+			: "Enter valid details only and submit the form."
+		)
+	$: simpleErrors = errors.filter(isSimpleError).map(error => error.message).join(" ")
+	$: hasSimpleErrors = simpleErrors.length > 0
+</script>
+
+<form class="mdc-card" {id} on:submit|preventDefault>
+	<IndeterminateProgressBar
+		isLoading={isConnecting}
+		{progressBarLabel}/>
+	{#if $$slots.lead_content}
+		<div class="mdc-card__content">
+			<slot name="lead_content"/>
+		</div>
+	{/if}
+	{#if hasSimpleErrors}
+		<div class="mdc-card__content">
+			<ShortParagraph>
+				{simpleErrors}
+			</ShortParagraph>
+		</div>
+	{/if}
+	<div class="mdc-card__content">
+		<slot name="field_content"/>
+	</div>
+	<div class="mdc-card__actions">
+		<div class="mdc-card__action-buttons">
+			<slot name="action_buttons"/>
+		</div>
+	</div>
+</form>
+
+<style lang="scss">
+	@use "@/components/third-party/index";
+
+	@use "@material/card";
+
+	@include card.core-styles;
+</style>
--- src/components/public/form/basic_form.svelte
+++ src/components/public/form/basic_form.svelte
@@ -1,54 +1,17 @@
 <script lang="ts">
 	import type { GeneralError } from "+/rest"
 
-	import { isSimpleError } from "+/rest"
-
 	import Flex from "$/layout/flex.svelte"
-	import IndeterminateProgressBar from "$/utility/indeterminate_progress_bar.svelte"
-	import ShortParagraph from "$/typography/short_paragraph.svelte";
+	import FormBase from "$/form/base.svelte"
 
 	export let errors: GeneralError[]
 	export let id: string|null
 	export let isConnecting: boolean
-
-	$: progressBarLabel = isConnecting
-		? "Waiting for server's response..."
-		: (
-			errors.length > 0
-			? "Failed to submit the details. Please check the errors."
-			: "Enter valid details only and submit the form."
-		)
-	$: simpleErrors = errors.filter(isSimpleError).map(error => error.message).join(" ")
-	$: hasSimpleErrors = simpleErrors.length > 0
 </script>
 
-<form class="mdc-card" {id} on:submit|preventDefault>
-	<IndeterminateProgressBar
-		isLoading={isConnecting}
-		{progressBarLabel}/>
-	{#if hasSimpleErrors}
-		<div class="mdc-card__content">
-			<ShortParagraph>
-				{simpleErrors}
-			</ShortParagraph>
-		</div>
-	{/if}
-	<div class="mdc-card__content">
-		<Flex>
-			<slot name="fields"/>
-		</Flex>
-	</div>
-	<div class="mdc-card__actions">
-		<div class="mdc-card__action-buttons">
-			<slot name="button_group"/>
-		</div>
-	</div>
-</form>
-
-<style lang="scss">
-	@use "@/components/third-party/index";
-
-	@use "@material/card";
-
-	@include card.core-styles;
-</style>
+<FormBase {isConnecting} {id} {errors} on:submit>
+	<Flex slot="field_content">
+		<slot name="fields"/>
+	</Flex>
+	<slot slot="action_buttons" name="button_group"/>
+</FormBase>
