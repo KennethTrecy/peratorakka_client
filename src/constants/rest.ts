60dfb21ab30bd1f06621c29b52c84019b07b3176
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-09-16 22:35:22 +0800

    internal(constant): prepare options for choosing mode

--- src/constants/rest.ts
+++ src/constants/rest.ts
@@ -1,3 +1,5 @@
+import type { ChoiceInfo } from "+/component"
+
 export const RECOMMENDED_API_VERSION = "0.2.0"
 
 export const MAINTENANCE_EXPIRATION_MECHANISM = "maintenance"
@@ -11,3 +13,18 @@ export const SEARCH_MODES = [
 	SEARCH_WITH_DELETED,
 	SEARCH_ONLY_DELETED
 ] as const
+
+export const SEARCH_NORMALLY_OPTION: ChoiceInfo = {
+	"label": "Present",
+	"data": SEARCH_NORMALLY
+}
+
+export const SEARCH_WITH_DELETED_OPTION: ChoiceInfo = {
+	"label": "All",
+	"data": SEARCH_WITH_DELETED
+}
+
+export const SEARCH_ONLY_DELETED_OPTION: ChoiceInfo = {
+	"label": "Archived",
+	"data": SEARCH_ONLY_DELETED
+}
