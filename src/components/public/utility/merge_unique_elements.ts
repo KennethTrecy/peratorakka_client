a54fb905b9e7040c092f2577acb079f11fd48c83
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-09-30 22:04:26 +0800

    internal(component): make function to merge unique elements

---
+++ src/components/public/utility/merge_unique_elements.ts
@@ -0,0 +1,23 @@
+export default function mergeUniqueElements<T>(
+	originalData: T[],
+	newData: T[],
+	generateKey: (element: T) => string|number
+): T {
+	const uniqueElements = [ ...originalData ]
+	const existingIDs = new Set<string|number>()
+
+	for (const element of originalData) {
+		existingIDs.add(generateKey(element))
+	}
+
+	for (const element of newData) {
+		const newKey = generateKey(element)
+
+		if (!existingIDs.has(newKey)) {
+			existingIDs.add(newKey)
+			uniqueElements.push(element)
+		}
+	}
+
+	return uniqueElements
+}
