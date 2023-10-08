a9e5e32f7e2dd1901ee75ed5fb151696bbff3026
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-10-08 14:00:56 +0800

    fix(component): pass the global context to JSON requester

--- src/components/public/rest/make_json_requester.ts
+++ src/components/public/rest/make_json_requester.ts
@@ -1,19 +1,23 @@
 import type { GeneralError, RequesterConstraints, RequesterInfo } from "+/rest"
+import type { ContextBundle } from "+/component"
 
-import { get, writable } from "svelte/store"
+import { getContext } from "svelte"
+import { writable } from "svelte/store"
 
-import { serverURL } from "$/global_state"
+import { GLOBAL_CONTEXT } from "#/contexts"
 
 import sendJSONRequest from "$/rest/send_json_request"
 
 export default function makeJSONRequester(constraints: RequesterConstraints): RequesterInfo {
+	const globalContext = getContext(GLOBAL_CONTEXT)
 	let isConnecting = writable(false)
 	let errors = writable<GeneralError[]>([])
 
 	async function send(specialRequestInfo: Partial<Request>) {
 		await sendJSONRequest(specialRequestInfo, constraints, {
 			isConnecting,
-			errors
+			errors,
+			globalContext
 		})
 	}
 	return {
