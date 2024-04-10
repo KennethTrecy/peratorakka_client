16653ff1df3204c0a183eed3f85224c164853480
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-04-10 22:27:14 +0900

    build: preprocess files to transpile scripts

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,12 @@
 import { resolve } from "path"
 import adapter from "@sveltejs/adapter-cloudflare"
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import("@sveltejs/kit").Config} */
 const configuration = {
+	"preprocess": [
+		vitePreprocess()
+	],
 	"kit": {
 		"adapter": adapter(),
 		"alias": {
