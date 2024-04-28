3b575603139b7eea1f1fd90647079c1f8af76588
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-04-28 15:36:30 +0900

    feat(component): add other configurations for smooth animation

--- src/constants/component.ts
+++ src/constants/component.ts
@@ -19,18 +19,74 @@ export const DEFAULT_MINIMUM_PROGRESS_RATE = 0.005
 export const DEFAULT_BUFFER_DELAY_CONFIGURATIONS: BufferDelayConfiguration[] = [
 	{
 		"rate": 0.05,
-		"delay": 150
+		"delay": 0
 	},
 	{
 		"rate": 0.1,
-		"delay": 250
+		"delay": 100
 	},
 	{
 		"rate": 0.15,
-		"delay": 500
+		"delay": 300
+	},
+	{
+		"rate": 0.165,
+		"delay": 400
+	},
+	{
+		"rate": 0.175,
+		"delay": 525
+	},
+	{
+		"rate": 0.185,
+		"delay": 600
 	},
 	{
 		"rate": 0.2,
 		"delay": 750
+	},
+	{
+		"rate": 0.235,
+		"delay": 875
+	},
+	{
+		"rate": 0.25,
+		"delay": 1000
+	},
+	{
+		"rate": 0.26,
+		"delay": 1075
+	},
+	{
+		"rate": 0.275,
+		"delay": 1125
+	},
+	{
+		"rate": 0.28,
+		"delay": 1150
+	},
+	{
+		"rate": 0.3,
+		"delay": 1250
+	},
+	{
+		"rate": 0.31,
+		"delay": 1550
+	},
+	{
+		"rate": 0.325,
+		"delay": 1750
+	},
+	{
+		"rate": 0.335,
+		"delay": 2000
+	},
+	{
+		"rate": 0.35,
+		"delay": 2500
+	},
+	{
+		"rate": 0.4,
+		"delay": 3000
 	}
 ]
