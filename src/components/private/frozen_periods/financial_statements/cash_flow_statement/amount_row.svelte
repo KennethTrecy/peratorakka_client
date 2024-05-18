c2a913b406a7a17163121cddfda1fbe36751fa84
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-05-18 22:18:27 +0800

    internal(frozen period): make amount row

---
+++ src/components/private/frozen_periods/financial_statements/cash_flow_statement/amount_row.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { Currency } from "+/entity"
+
+	import formatAmount from "$/utility/format_amount"
+
+	import DataTableCell from "$/catalog/data_table_cell.svelte"
+	import DataTableHeader from "$/catalog/data_table_header.svelte"
+	import DataTableRow from "$/catalog/data_table_row.svelte"
+
+	export let rowName: string
+	export let currency: Currency|undefined
+	export let rawAmount: string
+	export let hasEmptyTrailingRow: boolean = false
+
+	$: friendlyAmount = formatAmount(
+		currency,
+		rawAmount
+	)
+</script>
+
+<DataTableRow>
+	<DataTableHeader scope="row">{rowName}</DataTableHeader>
+	<DataTableCell kind="numeric">{friendlyAmount}</DataTableCell>
+</DataTableRow>
+{#if hasEmptyTrailingRow}
+	<DataTableRow/>
+{/if}
