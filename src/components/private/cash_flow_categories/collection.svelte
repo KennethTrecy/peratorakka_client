abfa231bf9acff038211f3a69dedcbf4d192c5a4
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-05-18 19:15:51 +0800

    internal(cash flow category): make collection

---
+++ src/components/private/cash_flow_categories/collection.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import type { CashFlowCategory } from "+/entity"
+	import type { GeneralError, SearchMode, SortOrder } from "+/rest"
+
+	import Collection from "$/catalog/collection.svelte"
+	import CashFlowCategoryCard from "%/cash_flow_categories/cash_flow_category_card.svelte"
+	import ListSpecifier from "$/form/list_specifier.svelte"
+
+	export let isConnecting: boolean
+	export let data: CashFlowCategory[]
+	export let searchMode: SearchMode
+	export let sortCriterion: string
+	export let sortOrder: SortOrder
+
+	export let listError: GeneralError[]
+
+	const availableSortCriteria = [
+		"name",
+		"created_at",
+		"updated_at",
+		"deleted_at"
+	]
+</script>
+
+<Collection collectiveName="Cash Flow Categories" {isConnecting} {data} progressRate={0}>
+	<svelte:fragment slot="filled_collection_description">
+		Below are the cash flow categories that you have added on to your profile.
+		They can be associated to financial accounts.
+	</svelte:fragment>
+	<ListSpecifier
+		slot="list_specifier"
+		bind:searchMode={searchMode}
+		bind:sortCriterion={sortCriterion}
+		bind:sortOrder={sortOrder}
+		{isConnecting}
+		{availableSortCriteria}
+		errors={listError}/>
+	<svelte:fragment slot="cards">
+		{#each data as entity(entity.id)}
+			<CashFlowCategoryCard bind:data={entity} on:delete/>
+		{/each}
+	</svelte:fragment>
+	<svelte:fragment slot="empty_collection_description">
+		There are no available cash flow categories at the moment.
+		Create a cash flow category to view.
+	</svelte:fragment>
+</Collection>
