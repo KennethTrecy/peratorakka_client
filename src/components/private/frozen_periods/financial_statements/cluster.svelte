f60795b85e63ac4aad84e027ed350428287a9c74
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-05-18 17:48:27 +0800

    fix(frozen period): make cash flow statement optional

--- src/components/private/frozen_periods/financial_statements/cluster.svelte
+++ src/components/private/frozen_periods/financial_statements/cluster.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+	import type { cashFlowCategoryKinds } from "#/entity"
+
 	import type { FinancialStatementGroup } from "+/rest"
 	import type { Currency, CashFlowCategory, Account, SummaryCalculation } from "+/entity"
 
 	import BalanceSheet from "%/frozen_periods/financial_statements/balance_sheet.svelte"
+	import CashFlowStatement from "%/frozen_periods/financial_statements/cash_flow_statement.svelte"
+	import ElementalParagraph from "$/typography/elemental_paragraph.svelte"
 	import Flex from "$/layout/flex.svelte"
 	import GridCell from "$/layout/grid_cell.svelte"
-	import CashFlowStatement from "%/frozen_periods/financial_statements/cash_flow_statement.svelte"
 	import IncomeStatement from "%/frozen_periods/financial_statements/income_statement.svelte"
 	import TrialBalance from "%/frozen_periods/financial_statements/trial_balance.svelte"
 
@@ -21,6 +24,9 @@
 	$: allowedCalculations = data.filter(
 		calculation => allowedAccountIDs.indexOf(calculation.account_id) > -1
 	)
+	$: hasAcceptableCashFlowCategories = cashFlowCategories.length > 1
+		&& cashFlowCategories.some(category => category.kind === acceptableCashFlowCategoryKinds[0])
+		&& cashFlowCategories.some(category => category.kind === acceptableCashFlowCategoryKinds[1])
 </script>
 
 <GridCell kind="triad">
@@ -45,12 +51,18 @@
 			{currency}
 			accounts={allowedAccounts}
 			data={allowedCalculations}/>
-		<CashFlowStatement
-			{statement}
-			{currency}
-			{cashFlowCategories}
-			accounts={allowedAccounts}
-			data={allowedCalculations}/>
+		{#if cashFlowCategories.length < 3}
+			<ElementalParagraph>
+				Note: There are no or few accounts that belongs to cash flows categories. At least one account must belong to liquid cash flow category and at least one account must belong to illiquid cash flow category.
+			</ElementalParagraph>
+		{:else}
+			<CashFlowStatement
+				{statement}
+				{currency}
+				{cashFlowCategories}
+				accounts={allowedAccounts}
+				data={allowedCalculations}/>
+		{/if}
 	</Flex>
 </GridCell>
 <GridCell kind="triad">
