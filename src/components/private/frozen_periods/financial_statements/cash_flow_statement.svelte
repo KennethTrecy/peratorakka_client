199b54aed562106b7e2121114ad56439a9f1e0ca
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-05-18 22:53:43 +0800

    feat(frozen period): show sections of categorized accounts

--- src/components/private/frozen_periods/financial_statements/cash_flow_statement.svelte
+++ src/components/private/frozen_periods/financial_statements/cash_flow_statement.svelte
@@ -2,10 +2,14 @@
 	import type { Currency, CashFlowCategory, Account, SummaryCalculation } from "+/entity"
 	import type { FinancialStatementGroup } from "+/rest"
 
+	import { acceptableCashFlowCategoryKinds } from "#/entity"
+
 	import formatAmount from "$/utility/format_amount"
 
 	import AmountRow
 		from "%/frozen_periods/financial_statements/cash_flow_statement/amount_row.svelte"
+	import CategorizedSection
+		from "%/frozen_periods/financial_statements/cash_flow_statement/categorized_section.svelte"
 	import DataTableCell from "$/catalog/data_table_cell.svelte"
 	import DataTableHeader from "$/catalog/data_table_header.svelte"
 	import QuarternaryHeading from "$/typography/quarternary_heading.svelte"
@@ -17,6 +21,12 @@
 	export let accounts: Account[]
 	export let data: Omit<SummaryCalculation, "frozen_period_id">[]
 
+	$: openedCashFlowCategories = cashFlowCategories.filter(
+		category => category.kind === acceptableCashFlowCategoryKinds[0]
+	)
+	$: closedCashFlowCategories = cashFlowCategories.filter(
+		category => category.kind === acceptableCashFlowCategoryKinds[1]
+	)
 	$: friendlyClosedLiquidAmount = formatAmount(
 		currency,
 		statement.cash_flow_statement.closed_liquid_amount
@@ -30,11 +40,27 @@
 		<DataTableHeader scope="column" kind="numeric">Amount</DataTableHeader>
 	</svelte:fragment>
 	<svelte:fragment slot="table_rows">
+		{#each openedCashFlowCategories as openedCashFlowCategory}
+			<CategorizedSection
+				cashFlowCategory={openedCashFlowCategory}
+				{currency}
+				{accounts}
+				{data}
+				hasEmptyTrailingRow={true}/>
+		{/each}
 		<AmountRow
 			rowName="Total Opened Liquid Amount"
 			currency={currency}
 			rawAmount={statement.cash_flow_statement.opened_liquid_amount}
 			hasEmptyTrailingRow={true}/>
+		{#each closedCashFlowCategories as closedCashFlowCategory}
+			<CategorizedSection
+				cashFlowCategory={closedCashFlowCategory}
+				{currency}
+				{accounts}
+				{data}
+				hasEmptyTrailingRow={true}/>
+		{/each}
 	</svelte:fragment>
 	<svelte:fragment slot="table_footer_cells">
 		<DataTableHeader scope="row">Total Closed Liquid Amount</DataTableHeader>
