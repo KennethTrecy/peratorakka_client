ef0d812626fbd5234e1b0c1799f61a28cd9a803e
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-05-26 18:10:48 +0800

    fix(cash flow activity): update add form

--- src/components/private/cash_flow_activities/add_form.svelte
+++ src/components/private/cash_flow_activities/add_form.svelte
@@ -1,27 +1,24 @@
 <script lang="ts">
-	import type { CashFlowCategory } from "+/entity"
+	import type { CashFlowActivity } from "+/entity"
 
 	import { createEventDispatcher } from "svelte"
 
-	import { acceptableCashFlowCategoryKinds } from "#/entity"
-
 	import makeJSONRequester from "$/rest/make_json_requester"
 
-	import BasicForm from "%/cash_flow_categories/basic_form.svelte"
+	import BasicForm from "%/cash_flow_activities/basic_form.svelte"
 	import DescriptiveForm from "$/form/descriptive_form.svelte"
 	import ElementalParagraph from "$/typography/elemental_paragraph.svelte"
 	import TextCardButton from "$/button/card/text.svelte"
 	import TextContainer from "$/typography/text_container.svelte"
 
 	const dispatch = createEventDispatcher<{
-		"create": CashFlowCategory
+		"create": CashFlowActivity
 	}>()
 	const IDPrefix = "new_"
 	let name = ""
 	let description = ""
-	let kind = acceptableCashFlowCategoryKinds[0]
 	let { isConnecting, errors, send } = makeJSONRequester({
-		"path": "/api/v1/cash_flow_categories",
+		"path": "/api/v1/cash_flow_activities",
 		"defaultRequestConfiguration": {
 			"method": "POST"
 		},
@@ -30,50 +27,47 @@
 				"statusCode": 201,
 				"action": async (response: Response) => {
 					const document = await response.json()
-					const { cash_flow_category } = document
+					const { cash_flow_activity } = document
 
 					name = ""
 					description = ""
-					kind = acceptableCashFlowCategoryKinds[0]
 					errors.set([])
-					dispatch("create", cash_flow_category)
+					dispatch("create", cash_flow_activity)
 				}
 			}
 		],
 		"expectedErrorStatusCodes": [ 400 ]
 	})
 
-	async function createCashFlowCategory() {
+	async function createCashFlowActivity() {
 		await send({
 			"body": JSON.stringify({
-				"cash_flow_category": {
+				"cash_flow_activity": {
 					name,
-					description,
-					kind
+					description
 				}
 			})
 		})
 	}
 </script>
 
-<DescriptiveForm individualName="Cash Flow Category" mayShowForm>
+<DescriptiveForm individualName="Cash Flow Activity" mayShowForm>
 	<TextContainer slot="description">
 		<ElementalParagraph>
-			Cash flow categories are used to classify changes in the current amount of accounts and determine the amount of available liquid cash and cash equivalents. Usual cash flow categories are cash and cash equivalent, operating activities, investment activities, and financing activities.
+			Cash flow activities are used to classify changes in the current amount of accounts and determine the amount of available liquid cash and cash equivalents. Usual cash flow activities are operating activities, investment activities, and financing activities.
 		</ElementalParagraph>
 		<ElementalParagraph>
-			A financial account in liquid category means that the account can be converted into cash immediately (e.g. bank deposits or physical cash in wallet) or an imaginary expense (e.g. depreciation expense). A financial account in illiquid category means that the account can be converted into cash after a long period or never at all such as land or equipments.
+			A liquid financial account is an account that can be converted into cash immediately (e.g. bank deposits or physical cash in wallet). Illiquid financial account means that the account can be converted into cash after a long period or never at all (e.g. land or equipments).
 		</ElementalParagraph>
 	</TextContainer>
 	<BasicForm
 		slot="form"
 		bind:name={name}
 		bind:description={description}
-		bind:kind={kind}
 		isConnecting={$isConnecting}
 		{IDPrefix}
 		errors={$errors}
-		on:submit={createCashFlowCategory}>
+		on:submit={createCashFlowActivity}>
 		<svelte:fragment slot="button_group">
 			<TextCardButton
 				kind="submit"
