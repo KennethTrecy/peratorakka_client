442a383b8ad23b247144a16c441fdb506f24165a
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-05-26 18:17:13 +0800

    fix(cash flow activity): update collection

--- src/components/private/cash_flow_activities/collection.svelte
+++ src/components/private/cash_flow_activities/collection.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	import type { CashFlowCategory } from "+/entity"
+	import type { CashFlowActivity } from "+/entity"
 	import type { GeneralError, SearchMode, SortOrder } from "+/rest"
 
 	import Collection from "$/catalog/collection.svelte"
-	import CashFlowCategoryCard from "%/cash_flow_categories/cash_flow_category_card.svelte"
+	import CashFlowActivityCard from "%/cash_flow_activities/cash_flow_activity_card.svelte"
 	import Flex from "$/layout/flex.svelte"
 	import ListSpecifier from "$/form/list_specifier.svelte"
 
 	export let isConnecting: boolean
-	export let data: CashFlowCategory[]
+	export let data: CashFlowActivity[]
 	export let searchMode: SearchMode
 	export let sortCriterion: string
 	export let sortOrder: SortOrder
@@ -23,10 +23,10 @@
 	]
 </script>
 
-<Collection collectiveName="Cash Flow Categories" {isConnecting} {data} progressRate={0}>
+<Collection collectiveName="Cash Flow Activities" {isConnecting} {data} progressRate={0}>
 	<svelte:fragment slot="filled_collection_description">
-		Below are the cash flow categories that you have added on to your profile.
-		They can be associated to financial accounts.
+		Below are the cash flow activity that you have added on to your profile.
+		They can be associated to modifiers.
 	</svelte:fragment>
 	<Flex slot="list_specifier" justifyContent="responsive_stretch" mustPad={false}>
 		<ListSpecifier
@@ -39,11 +39,11 @@
 	</Flex>
 	<svelte:fragment slot="cards">
 		{#each data as entity(entity.id)}
-			<CashFlowCategoryCard bind:data={entity} on:delete/>
+			<CashFlowActivityCard bind:data={entity} on:delete/>
 		{/each}
 	</svelte:fragment>
 	<svelte:fragment slot="empty_collection_description">
-		There are no available cash flow categories at the moment.
-		Create a cash flow category to view.
+		There are no available cash flow activity at the moment.
+		Create a cash flow activity to view.
 	</svelte:fragment>
 </Collection>
