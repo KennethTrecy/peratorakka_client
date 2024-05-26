0810950c5f012833fa0e4a477dc7788c33c68da5
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-05-26 16:21:26 +0800

    feat(account): remove cash flow activity in card

--- src/components/private/accounts/account_card.svelte
+++ src/components/private/accounts/account_card.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import type { GeneralError } from "+/rest"
-	import type { Currency, CashFlowCategory, Account, AcceptableAccountKind } from "+/entity"
+	import type { Currency, Account, AcceptableAccountKind } from "+/entity"
 
 	import { createEventDispatcher } from "svelte"
 	import { writable } from "svelte/store"
 
 	import { acceptableAccountKinds } from "#/entity"
-	import { NO_CASH_FLOW_CATEGORY } from "#/component"
 
 	import makeJSONRequester from "$/rest/make_json_requester"
 	import convertSnakeCaseToProperCase from "$/utility/convert_snake_case_to_proper_case"
@@ -17,16 +16,12 @@
 	import ShortParagraph from "$/typography/short_paragraph.svelte"
 
 	export let currencies: Currency[]
-	export let cashFlowCategories: CashFlowCategory[]
 	export let data: Account
 
 	const dispatch = createEventDispatcher<{
 		"delete": Account
 	}>()
 	let currencyID = `${data.currency_id}`
-	let cashFlowCategoryID = data.cash_flow_category_id === null
-		? `${NO_CASH_FLOW_CATEGORY.id}`
-		: `${data.cash_flow_category_id}`
 	let name = data.name
 	let description = data.description
 	let kind = fallbackToAceptableKind(data.kind)
@@ -40,10 +35,6 @@
 	$: associatedCurrency = currencies.find(
 		currency => currency.id === parseInt(currencyID)
 	) as Currency
-	$: hasCashFlowCategory = cashFlowCategoryID !== `${NO_CASH_FLOW_CATEGORY.id}`
-	$: associatedCashFlowCategory = hasCashFlowCategory ? cashFlowCategories.find(
-		cashFlowCategory => cashFlowCategory.id === parseInt(cashFlowCategoryID)
-	) as CashFlowCategory : null
 
 	let isConnectingToUpdate = writable<boolean>(false)
 	let updateErrors = writable<GeneralError[]>([])
@@ -53,8 +44,6 @@
 	let requestDelete = () => Promise.resolve()
 
 	$: friendlyKind = convertSnakeCaseToProperCase(data.kind).toLocaleLowerCase()
-	$: friendlyCategoryName = hasCashFlowCategory ? associatedCashFlowCategory?.name : null
-	$: friendlyCategoryKind = hasCashFlowCategory ? associatedCashFlowCategory?.kind : null
 	$: {
 		const requesterInfo = makeJSONRequester({
 			"path": `/api/v1/accounts/${data.id}`,
@@ -68,9 +57,6 @@
 						data = {
 							...data,
 							"currency_id": parseInt(currencyID),
-							"cash_flow_category_id": cashFlowCategoryID === ""
-								? null
-								: parseInt(cashFlowCategoryID),
 							name,
 							description,
 							kind
@@ -88,9 +74,6 @@
 				"body": JSON.stringify({
 					"account": {
 						"currency_id": parseInt(currencyID),
-						"cash_flow_category_id": cashFlowCategoryID === ""
-							? null
-							: parseInt(cashFlowCategoryID),
 						name,
 						description,
 						kind
@@ -123,9 +106,6 @@
 
 	function resetDraft() {
 		currencyID = `${data.currency_id}`
-		cashFlowCategoryID = data.cash_flow_category_id === null
-			? ""
-			: `${data.cash_flow_category_id}`
 		name = data.name
 		description = data.description
 		kind = fallbackToAceptableKind(data.kind)
@@ -154,14 +134,12 @@
 		let:cancelEdit
 		id={formID}
 		bind:currencyID={currencyID}
-		bind:cashFlowCategoryID={cashFlowCategoryID}
 		bind:name={name}
 		bind:description={description}
 		bind:kind={kind}
 		isConnecting={$isConnectingToUpdate}
 		{IDPrefix}
 		{currencies}
-		{cashFlowCategories}
 		{forceDisabledFields}
 		errors={$updateErrors}
 		on:submit={confirmEdit}>
@@ -182,9 +160,6 @@
 		<ShortParagraph>{data.description}</ShortParagraph>
 		<ShortParagraph>
 			This {friendlyKind} account uses {associatedCurrency.code} as its currency.
-			{#if hasCashFlowCategory}
-				It is considered as {friendlyCategoryKind} account under {friendlyCategoryName}.
-			{/if}
 		</ShortParagraph>
 	</svelte:fragment>
 </CollectionItem>
