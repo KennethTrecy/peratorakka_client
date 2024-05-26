fb47a77d7e576755c08649f369e1475b11f77e9d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-05-26 16:37:29 +0800

    internal(component): specify compatible type to merge

--- src/components/public/utility/merge_unique_resources.ts
+++ src/components/public/utility/merge_unique_resources.ts
@@ -1,6 +1,6 @@
 import mergeUniqueElements from "$/utility/merge_unique_elements"
 
-export default function mergeUniqueResources<T>(
+export default function mergeUniqueResources<T extends { id: number }>(
 	originalData: T[],
 	newData: T[]
 ): T[] {
