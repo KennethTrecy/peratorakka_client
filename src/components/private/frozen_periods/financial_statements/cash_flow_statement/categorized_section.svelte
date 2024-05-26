4773556ab895bf5442b1f59a8f8df954b7c68cc6
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-05-26 18:43:55 +0800

    fix(frozen period): show calculated flows in categorized section

--- src/components/private/frozen_periods/financial_statements/cash_flow_statement/categorized_section.svelte
+++ src/components/private/frozen_periods/financial_statements/cash_flow_statement/categorized_section.svelte
@@ -1,82 +1,64 @@
 <script lang="ts">
-	import type { Account, CashFlowCategory, Currency, SummaryCalculation } from "+/entity"
-	import type { LiquidBalance, IlliquidBalance } from "+/rest"
+	import type {
+		Currency,
+		CashFlowActivity,
+		Account,
+		FlowCalculation
+	} from "+/entity"
+	import type { CashFlowActivitySubtotal } from "+/rest"
 
-	import { acceptableAccountKinds, acceptableCashFlowCategoryKinds } from "#/entity"
-
-	import calculateFlowedAmount from "$/utility/calculate_flowed_amount"
-	import { isIlliquidBalance } from "+/rest"
+	import { UNKNOWN_ACCOUNT } from "#/component"
 
 	import AmountRow
 		from "%/frozen_periods/financial_statements/cash_flow_statement/amount_row.svelte"
 
 	export let currency: Currency|undefined
-	export let cashFlowCategory: CashFlowCategory
-	export let cashFlowSubtotals: (LiquidBalance|IlliquidBalance)[]
+	export let cashFlowActivity: CashFlowActivity
+	export let cashFlowSubtotals: CashFlowActivitySubtotal[]
 	export let accounts: Account[]
-	export let data: Omit<SummaryCalculation, "frozen_period_id">[]
+	export let flowCalculations: FlowCalculation[]
 
-	$: matchedAccounts = accounts.filter(
-		account => account.cash_flow_category_id === cashFlowCategory.id
-	)
 	$: matchedSubtotal = cashFlowSubtotals.find(
-		subtotal => subtotal.cash_flow_category_id === cashFlowCategory.id
-	) as LiquidBalance|IlliquidBalance
-	$: matchedAccountAmounts = matchedAccounts.map(account => {
-		const calculation = data.find(
-			data => data.account_id === account.id
-		)
-
-		if (typeof calculation === "undefined") return {
-			"accountName": account.name,
-			"rawAmount": "0"
-		}
-
-		const acceptableAccountKindIndex = ([ ...acceptableAccountKinds ] as string[])
-			.indexOf(account.kind as string)
-		const acceptableCashFlowCategoryKindIndex = (
-			[ ...acceptableCashFlowCategoryKinds ] as string[]
-		).indexOf(cashFlowCategory.kind as string)
-
-		const rawAmount = acceptableAccountKindIndex > -1 && acceptableCashFlowCategoryKindIndex > -1
-			? calculateFlowedAmount(
-				acceptableCashFlowCategoryKinds[acceptableCashFlowCategoryKindIndex],
-				acceptableAccountKinds[acceptableAccountKindIndex],
-				calculation as SummaryCalculation
-			)
-			: "0"
+		subtotal => subtotal.cash_flow_activity_id === cashFlowActivity.id
+	) as CashFlowActivitySubtotal
+	$: matchedFlowCalculations = flowCalculations.filter(
+		flowCalculation => flowCalculation.cash_flow_activity_id === cashFlowActivity.id
+	)
+	$: accountedFlowCalculations = matchedFlowCalculations.map(flowCalculation => {
+		const account = accounts.find(
+			account => account.id === flowCalculation.account_id
+		) ?? UNKNOWN_ACCOUNT
 
 		return {
-			"accountName": account.name,
-			rawAmount
+			...flowCalculation,
+			"@meta": {
+				account
+			}
 		}
 	})
-	$: changedAmountInfo = matchedAccountAmounts.filter(
-		amountInfo => amountInfo.rawAmount !== "0"
-	)
-	$: hasSubtotal = matchedSubtotal.subtotal !== "0"
-	$: rowCountBeforeAccounts = isIlliquidBalance(matchedSubtotal) && hasSubtotal ? 1 : 0
-	$: firstRowSpan = changedAmountInfo.length + 1 + rowCountBeforeAccounts
+	$: hasNetIncome = matchedSubtotal.net_income !== "0"
+	$: rowCountBeforeAccounts = hasNetIncome ? 1 : 0
+	$: firstRowSpan = matchedFlowCalculations.length + 1 + rowCountBeforeAccounts
 </script>
 
-{#if isIlliquidBalance(matchedSubtotal) && hasSubtotal}
+{#if hasNetIncome}
 	<AmountRow
-		categoryName={cashFlowCategory.name}
+		categoryName={cashFlowActivity.name}
 		categoryNameRowSpan={firstRowSpan}
 		currency={currency}
 		accountName="Net income"
 		rawAmount={matchedSubtotal.net_income}/>
 {/if}
-{#each changedAmountInfo as amountInfo, i}
+{#each accountedFlowCalculations as flowCalculation, i}
 	<AmountRow
-		categoryName={cashFlowCategory.name}
+		categoryName={cashFlowActivity.name}
 		categoryNameRowSpan={i + rowCountBeforeAccounts === 0 ? firstRowSpan : 0}
 		currency={currency}
-		accountName={amountInfo.accountName}
-		rawAmount={amountInfo.rawAmount}/>
+		accountName={flowCalculation["@meta"].account.name}
+		rawAmount={flowCalculation.net_amount}/>
 {/each}
 <AmountRow
-	categoryName={cashFlowCategory.name}
+	categoryName={cashFlowActivity.name}
 	categoryNameRowSpan={0}
 	currency={currency}
 	accountName="Balance"
