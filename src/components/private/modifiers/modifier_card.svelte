ce5fa6ec65ca1a9948852b4bea220e22b5333ee2
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-06-16 11:14:53 +0800

    fix(modifier): include account IDs in request

--- src/components/private/modifiers/modifier_card.svelte
+++ src/components/private/modifiers/modifier_card.svelte
@@ -134,6 +134,8 @@
 							creditCashFlowActivityID === `${NO_CASH_FLOW_ACTIVITY.id}`
 								? null
 								: parseInt(creditCashFlowActivityID),
+						"debit_account_id": parseInt(debitAccountID),
+						"credit_account_id": parseInt(creditAccountID),
 						name,
 						description
 					}
