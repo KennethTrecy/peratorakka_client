778a776848e1e16df0d1a265237cedee8828c3c5
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-08-04 12:16:26 +0800

    internal(component): finalize code to find lightest path

--- src/components/public/utility/derive_exchange_rate/find_lightest_path.ts
+++ src/components/public/utility/derive_exchange_rate/find_lightest_path.ts
@@ -1,8 +1,37 @@
 import type { ExchangeRateInfo } from "+/rest"
+import weighPath from "$/utility/derive_exchange_rate/weigh_path"
 
 export default function findLightestPath(
 	paths: ExchangeRateInfo[][],
 	currentTime: number
 ): ExchangeRateInfo[] {
-	return []
+	let lastWeight = Infinity
+	let lastPath: ExchangeRateInfo[] = []
+
+	for(const path of paths) {
+		const weight = weighPath(path, currentTime)
+		if (weight < lastWeight) {
+			lastWeight = weight
+			lastPath = path
+		}
+
+		if (weight === lastWeight) {
+			const timeStampsInLastPath = lastPath.map(
+				exchangeRate => new Date(exchangeRate.updated_at)
+			)
+			const timeStampsInCurrentPath = path.map(
+				exchangeRate => new Date(exchangeRate.updated_at)
+			)
+
+			timeStampsInLastPath.sort().reverse()
+			timeStampsInCurrentPath.sort().reverse()
+
+			// Get path with latest timestamp
+			if (timeStampsInCurrentPath[0] > timeStampsInLastPath[0]) {
+				lastPath = path
+			}
+		}
+	}
+
+	return lastPath
 }
