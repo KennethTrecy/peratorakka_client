626a8fa7ff24b7bb6acf294b7bb09a984385c05c
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-08-04 10:29:14 +0800

    test(unit): watch for exchange rate loops

---
+++ src/components/public/utility/find_exchange_rate_path.spec.ts
@@ -0,0 +1,214 @@
+import type { Currency } from "+/entity"
+import type { ExchangeRateInfo } from "+/rest"
+
+import { describe, it, expect } from "vitest"
+
+import topicFunction from "./find_exchange_rate_path"
+
+describe("Find exchange rate path function behavior", () => {
+	it("can find direct rates", async () => {
+		const destinationCurrency: Currency = {
+			"id": 3,
+			"name": "Currency C",
+			"code": "C"
+		}
+
+		const exchangeRates: ExchangeRateInfo[] = [
+			{
+				"source": {
+					"id": 1,
+					"value": "2"
+				},
+				"destination": {
+					"id": 2,
+					"value": "3"
+				},
+				"updated_at": (new Date()).toDateString()
+			},
+			{
+				"source": {
+					"id": 2,
+					"value": "2"
+				},
+				"destination": {
+					"id": 3,
+					"value": "3"
+				},
+				"updated_at": (new Date()).toDateString()
+			}
+		]
+
+		const currentPath: ExchangeRateInfo = [ exchangeRates[0] ]
+
+		const result = topicFunction(destinationCurrency, exchangeRates, currentPath)
+
+		expect(result).toBe([ [ exchangeRates[0] ] ])
+	})
+
+	it("can find indirect rates", async () => {
+		const destinationCurrency: Currency = {
+			"id": 2,
+			"name": "Currency B",
+			"code": "B"
+		}
+
+		const exchangeRates: ExchangeRateInfo[] = [
+			{
+				"source": {
+					"id": 1,
+					"value": "2"
+				},
+				"destination": {
+					"id": 3,
+					"value": "3"
+				},
+				"updated_at": (new Date()).toDateString()
+			},
+			{
+				"source": {
+					"id": 3,
+					"value": "2"
+				},
+				"destination": {
+					"id": 2,
+					"value": "3"
+				},
+				"updated_at": (new Date()).toDateString()
+			},
+			{
+				"source": {
+					"id": 3,
+					"value": "2"
+				},
+				"destination": {
+					"id": 4,
+					"value": "3"
+				},
+				"updated_at": (new Date()).toDateString()
+			},
+			{
+				"source": {
+					"id": 4,
+					"value": "2"
+				},
+				"destination": {
+					"id": 2,
+					"value": "3"
+				},
+				"updated_at": (new Date()).toDateString()
+			}
+		]
+
+		const currentPath: ExchangeRateInfo = [ exchangeRates[0] ]
+
+		const result = topicFunction(destinationCurrency, exchangeRates, currentPath)
+
+		expect(result).toBe([
+			[ exchangeRates[0], exchangeRates[1] ],
+			[ exchangeRates[0], exchangeRates[2], exchangeRates[3] ]
+		])
+	})
+
+	it("can stop early", async () => {
+		const destinationCurrency: Currency = {
+			"id": 2,
+			"name": "Currency B",
+			"code": "B"
+		}
+
+		const exchangeRates: ExchangeRateInfo[] = [
+			{
+				"source": {
+					"id": 1,
+					"value": "2"
+				},
+				"destination": {
+					"id": 2,
+					"value": "3"
+				},
+				"updated_at": (new Date()).toDateString()
+			}
+		]
+
+		const currentPath: ExchangeRateInfo = [ exchangeRates[0] ]
+
+		const result = topicFunction(destinationCurrency, exchangeRates, currentPath)
+
+		expect(result).toBe([ [ exchangeRates[0] ] ])
+	})
+
+	it("can find skip loops", async () => {
+		const destinationCurrency: Currency = {
+			"id": 2,
+			"name": "Currency B",
+			"code": "B"
+		}
+
+		const exchangeRates: ExchangeRateInfo[] = [
+			{
+				"source": {
+					"id": 1,
+					"value": "2"
+				},
+				"destination": {
+					"id": 3,
+					"value": "3"
+				},
+				"updated_at": (new Date()).toDateString()
+			},
+			{
+				"source": {
+					"id": 3,
+					"value": "2"
+				},
+				"destination": {
+					"id": 2,
+					"value": "3"
+				},
+				"updated_at": (new Date()).toDateString()
+			},
+			{
+				"source": {
+					"id": 3,
+					"value": "2"
+				},
+				"destination": {
+					"id": 4,
+					"value": "3"
+				},
+				"updated_at": (new Date()).toDateString()
+			},
+			{
+				"source": {
+					"id": 4,
+					"value": "2"
+				},
+				"destination": {
+					"id": 2,
+					"value": "3"
+				},
+				"updated_at": (new Date()).toDateString()
+			},
+			{
+				"source": {
+					"id": 4,
+					"value": "2"
+				},
+				"destination": {
+					"id": 3,
+					"value": "3"
+				},
+				"updated_at": (new Date()).toDateString()
+			}
+		]
+
+		const currentPath: ExchangeRateInfo = [ exchangeRates[0] ]
+
+		const result = topicFunction(destinationCurrency, exchangeRates, currentPath)
+
+		expect(result).toBe([
+			[ exchangeRates[0], exchangeRates[1] ],
+			[ exchangeRates[0], exchangeRates[2], exchangeRates[3] ]
+		])
+	})
+})
