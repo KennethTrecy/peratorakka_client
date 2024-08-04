b16ae42c7f6506a9c9c493b6bcb43e2559bca125
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-08-04 11:44:59 +0800

    internal(component): finalize code to weigh paths

--- src/components/public/utility/derive_exchange_rate/weigh_path.ts
+++ src/components/public/utility/derive_exchange_rate/weigh_path.ts
@@ -1,5 +1,13 @@
 import type { ExchangeRateInfo } from "+/rest"
 
-export default function weighPath(path: ExchangeRateInfo[]): number {
-	return 0
+export default function weighPath(path: ExchangeRateInfo[], currentTime: number): number {
+	let totalWeight = 0
+
+	for (const exchangeRate of path) {
+		const lastUpdate = new Date(exchangeRate.updated_at)
+		const edgeWeight = currentTime - lastUpdate.getTime()
+		totalWeight += edgeWeight
+	}
+
+	return totalWeight
 }
