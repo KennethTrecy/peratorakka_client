9a5af5bf729137c75143aa61c12a095540957876
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-08-04 11:04:02 +0800

    internal(component): make the main process to find exchange path

--- src/components/public/utility/find_exchange_rate_path.ts
+++ src/components/public/utility/find_exchange_rate_path.ts
@@ -6,5 +6,38 @@ export default function findExchangeRatePaths(
 	exchangeRates: ExchangeRateInfo[],
 	currentPath: ExchangeRateInfo[]
 ): ExchangeRateInfo[][] {
-	return []
+	const lastPathIndex = currentPath.length - 1;
+	const lastExchangeRate = currentPath[lastPathIndex]
+	if (lastExchangeRate.destination.currency_id === destinationCurrency.id) {
+		return [
+			currentPath
+		]
+	}
+
+	const lastFoundCurrencyIDs = new Set(
+		currentPath.reduce((previousIDs, exchangeRate) => {
+			return [
+				...previousIDs,
+				exchangeRate.source.currency_id,
+				exchangeRate.destination.currency_id
+			]
+		}, [])
+	)
+
+	let foundPaths: ExchangeRateInfo[][] = []
+
+	for (const exchangeRate of exchangeRates) {
+		if (
+			exchangeRate.source.currency_id === lastExchangeRate.destination.currency_id
+			&& !lastFoundCurrencyIDs.has(exchangeRate.destination.currency_id)
+		) {
+			const newPath = [ ...currentPath, exchangeRate ];
+			foundPaths = [
+				...foundPaths,
+				...findExchangeRatePaths(destinationCurrency, exchangeRates, newPath)
+			]
+		}
+	}
+
+	return foundPaths
 }
