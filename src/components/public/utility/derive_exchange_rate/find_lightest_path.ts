881c5de0167d2a5a05922ba1272554b0458e87e4
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-08-04 11:48:37 +0800

    internal(component): make initial code to find lightest path

---
+++ src/components/public/utility/derive_exchange_rate/find_lightest_path.ts
@@ -0,0 +1,8 @@
+import type { ExchangeRateInfo } from "+/rest"
+
+export default function findLightestPath(
+	paths: ExchangeRateInfo[][],
+	currentTime: number
+): ExchangeRateInfo[] {
+	return []
+}
