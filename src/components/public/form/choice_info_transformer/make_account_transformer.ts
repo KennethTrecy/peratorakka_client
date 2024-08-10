d1d134a572f75ee7cfc838f1237d260ec9e28e84
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-08-10 14:52:49 +0800

    internal(component): customize choice info for filler data

--- src/components/public/form/choice_info_transformer/make_account_transformer.ts
+++ src/components/public/form/choice_info_transformer/make_account_transformer.ts
@@ -1,11 +1,21 @@
 import type { ChoiceInfo } from "+/component"
 import type { Account, Currency } from "+/entity"
 
+import { ANY_ACCOUNT } from "#/component"
+
 import convertSnakeCaseToProperCase from "$/utility/convert_snake_case_to_proper_case"
 
 export default function makeAccountTransformer(currencies: Currency[]) {
 	return (account: Account): ChoiceInfo => {
 		const data = `${account.id}`
+
+		if (account.id === ANY_ACCOUNT.id) {
+			return {
+				"label": account.name,
+				data
+			}
+		}
+
 		const kind = convertSnakeCaseToProperCase(account.kind)
 		const currencyInfo = currencies.find(currency => currency.id === account.currency_id)
 		const currencyCode = currencyInfo?.code ?? "Unknown"
--- src/components/public/form/choice_info_transformer/transform_currency.ts
+++ src/components/public/form/choice_info_transformer/transform_currency.ts
@@ -1,8 +1,18 @@
 import type { ChoiceInfo } from "+/component"
 import type { Currency } from "+/entity"
 
+import { ANY_CURRENCY } from "#/component"
+
 export default function transformCurrency(currency: Currency): ChoiceInfo {
 	const data = `${currency.id}`
+
+	if (currency.id === ANY_CURRENCY.id) {
+		return {
+			"label": currency.name,
+			data
+		}
+	}
+
 	const label = `${currency.code} (${currency.name})`
 
 	return {
