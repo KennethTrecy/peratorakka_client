afe93ff8988f9bb875119c34a236791995304a23
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-11-23 21:53:25 +0800

    feat(shell): update the navigation button

---
+++ src/components/private/shell/top_app_bar/navigation_button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+export let label: string
+export let icon: string
+</script>
+
+<a
+	href="#!"
+	data-target="mobile-navigator"
+	class="sidenav-trigger"
+	aria-label={label}>
+	<i class="material-icons">{icon}</i>
+</a>
+
+<style lang="scss">
+@use "@/components/third-party/index";
+</style>
