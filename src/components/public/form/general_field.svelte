b673b00f3c6ccce68948aac6c5c4a7282b6b2edd
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-11-24 12:42:42 +0800

    fix(component): correct colors for disabled fields

--- src/components/public/form/general_field.svelte
+++ src/components/public/form/general_field.svelte
@@ -36,4 +36,11 @@ $: message = errors.filter(
 
 <style lang="scss">
 @use "@/components/third-party/index";
+
+:global(.input-field:has([disabled])) {
+	--input-color: var(--md-ref-palette-neutral60);
+	--md-sys-color-on-surface: var(--input-color);
+	--md-sys-color-primary: var(--input-color);
+	color: var(--input-color);
+}
 </style>
