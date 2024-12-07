f438970e6606ca542bd29dc927222c85431444ca
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-12-08 00:25:06 +0800

    internal(component): allow deletion on data table record

--- src/components/public/catalog/data_table_record.svelte
+++ src/components/public/catalog/data_table_record.svelte
@@ -1,145 +1,217 @@
 <script lang="ts">
-	import type { GeneralError } from "+/rest"
-	import type { CardStatus, DataTableCellStatus } from "+/component"
-	import type { Writable } from "svelte/store"
-	import type { Account } from "+/entity"
+import type {
+	CardStatus,
+	DataTableCellKind,
+	DataTableCellStatus,
+	RestorableItemOptions
+} from "+/component"
 
-	import { createEventDispatcher } from "svelte"
-	import { get } from "svelte/store"
+import { createEventDispatcher } from "svelte"
+import { get } from "svelte/store"
 
-	import DataTableAccountCell from "$/catalog/data_table_account_cell.svelte"
-	import DataTableCell from "$/catalog/data_table_cell.svelte"
-	import DataTableHeader from "$/catalog/data_table_header.svelte"
-	import DataTableRow from "$/catalog/data_table_row.svelte"
-	import Flex from "$/layout/flex.svelte"
-	import ShortParagraph from "$/typography/short_paragraph.svelte"
-	import TextButton from "$/button/text.svelte"
-	import WeakenedTertiaryHeading from "$/typography/weakened_tertiary_heading.svelte"
+import DataTableCell from "$/catalog/data_table_cell.svelte"
+import DataTableHeader from "$/catalog/data_table_header.svelte"
+import DataTableRow from "$/catalog/data_table_row.svelte"
+import Flex from "$/layout/flex.svelte"
+import ShortParagraph from "$/typography/short_paragraph.svelte"
+import TextButton from "$/button/text.svelte"
+import TextCardButton from "$/button/card/text.svelte"
+import WeakenedTertiaryHeading from "$/typography/weakened_tertiary_heading.svelte"
 
-	export let label: string
-	export let debitAccount: Account
-	export let creditAccount: Account
-	export let updateErrors: Writable<GeneralError[]>
-	export let requestUpdate: () => Promise<void>
-	export let isConnectingToDelete: boolean
-	export let deleteErrors: Writable<GeneralError[]>
-	export let requestDelete: () => Promise<void>
-	export let headerStatus: DataTableCellStatus = "present"
+export let label: string
+export let isArchived: boolean
+export let headerStatus: DataTableCellStatus = "present"
+export let options: RestorableItemOptions
+$: isConnectingToUpdate = options.isConnectingToUpdate
+$: updateErrors = options.updateErrors
+$: requestUpdate = options.requestUpdate
+$: isConnectingToDelete = options.isConnectingToDelete
+$: deleteErrors = options.deleteErrors
+$: requestDelete = options.requestDelete
+$: isConnectingToRestore = options.isConnectingToRestore
+$: restoreErrors = options.restoreErrors
+$: requestRestore = options.requestRestore
+$: isConnectingToForceDelete = options.isConnectingToForceDelete
+$: forceDeleteErrors = options.forceDeleteErrors
+$: requestForceDelete = options.requestForceDelete
 
-	const dispatch = createEventDispatcher<{
-		"delete": void
-		"resetDraft": void
-	}>()
+const dispatch = createEventDispatcher<{
+	"resetDraft": void
+}>()
 
-	let status: CardStatus = "reading"
-	$: isEditing = status === "editing"
-	$: isConfirmingDeletion = status === "confirming_deletion"
-	$: debitAccountName = debitAccount.name
-	$: creditAccountName = creditAccount.name
-	$: debitExistence = debitAccount.deleted_at === null
-	$: creditExistence = creditAccount.deleted_at === null
+let status: CardStatus = "reading"
+$: isEditing = status === "editing"
+$: isConfirmingDeletion = status === "confirming_deletion"
+$: isConfirmingRestoration = status === "confirming_restoration"
+$: isConfirmingForceDeletion = status === "confirming_nullification"
 
-	function startReading() {
-		status = "reading"
-	}
+function startReading() {
+	status = "reading"
+}
 
-	function startEditing() {
-		status = "editing"
-	}
+function startEditing() {
+	status = "editing"
+}
 
-	function confirmDeletion() {
-		status = "confirming_deletion"
-	}
+function confirmDeletion() {
+	status = "confirming_deletion"
+}
 
-	async function confirmEdit(event: SubmitEvent) {
-		event.preventDefault()
+function confirmRestoration() {
+	status = "confirming_restoration"
+}
 
-		await requestUpdate()
-		if (get(updateErrors).length === 0) {
-			updateErrors.set([])
-			startReading()
-		}
-	}
+function confirmForceDeletion() {
+	status = "confirming_nullification"
+}
 
-	async function confirmDelete() {
-		await requestDelete()
-	}
+async function confirmEdit(event: SubmitEvent) {
+	event.preventDefault()
 
-	function cancelEdit() {
+	await requestUpdate()
+	if (get(updateErrors).length === 0) {
+		updateErrors.set([])
 		startReading()
-		dispatch("resetDraft")
 	}
+}
+
+async function confirmDelete() {
+	deleteErrors.set([])
+	await requestDelete()
+}
+
+async function confirmRestore() {
+	restoreErrors.set([])
+	await requestRestore()
+}
+
+async function confirmForceDelete() {
+	forceDeleteErrors.set([])
+	await requestForceDelete()
+}
+
+function cancelEdit() {
+	startReading()
+	dispatch("resetDraft")
+}
+
+$: isConfirming = isEditing
+	? isConnectingToUpdate
+	: isConfirmingDeletion
+		? isConnectingToDelete
+		: isConfirmingRestoration
+			? isConnectingToRestore
+			: isConnectingToForceDelete
+
+$: confirmAction = isConfirmingDeletion
+	? confirmDelete
+	: isConfirmingRestoration
+		? confirmRestore
+		: confirmForceDelete
+
+$: actionCellKind = (
+		isEditing
+		|| isConfirmingDeletion
+		|| isConfirmingRestoration
+		|| isConfirmingForceDeletion
+		? "descriptive"
+		: "representative"
+	) as DataTableCellKind
 </script>
 
 <DataTableRow>
 	<slot name="leading_cells"/>
-	<DataTableHeader scope="row" kind="representative" status={headerStatus}>
-		{label}
-	</DataTableHeader>
-	<DataTableAccountCell
-		rawDebitExistence={[ debitExistence ]}
-		rawDebit={[ debitAccountName ]}
-		rawCreditExistence={[ creditExistence ]}
-		rawCredit={[ creditAccountName ]}/>
+	{#if $$slots.main_cell}
+		<slot name="main_cell" {label} {headerStatus}/>
+	{:else}
+		<DataTableHeader scope="row" kind="representative" status={headerStatus}>
+			{label}
+		</DataTableHeader>
+	{/if}
 	<slot name="trailing_cells"/>
-	<DataTableCell>
-		{#if isConfirmingDeletion}
-			<article class="mdc-card">
-				<div class="mdc-card__content">
-					<Flex>
-						<WeakenedTertiaryHeading>
-							Delete {label}?
-						</WeakenedTertiaryHeading>
-						<slot name="delete_confirmation_message"/>
-						{#each $deleteErrors as error}
-							<ShortParagraph>
-								{error.message}
-							</ShortParagraph>
-						{/each}
+	<DataTableCell kind={actionCellKind}>
+		{#if isConfirmingDeletion || isConfirmingRestoration || isConfirmingForceDeletion}
+			<article class="card">
+				<div class="card-content">
+					<Flex mustPad={false}>
+						{#if isConfirmingDeletion}
+							<WeakenedTertiaryHeading>
+								Delete {label}?
+							</WeakenedTertiaryHeading>
+							<slot name="delete_confirmation_message"/>
+							{#each $deleteErrors as error}
+								<ShortParagraph>
+									{error.message}
+								</ShortParagraph>
+							{/each}
+						{:else if isConfirmingRestoration}
+							<WeakenedTertiaryHeading>
+								Restore {label}?
+							</WeakenedTertiaryHeading>
+							<slot name="restore_confirmation_message"/>
+							{#each $restoreErrors as error}
+								<ShortParagraph>
+									{error.message}
+								</ShortParagraph>
+							{/each}
+						{:else if isConfirmingForceDeletion}
+							<WeakenedTertiaryHeading>
+								Force delete {label}?
+							</WeakenedTertiaryHeading>
+							<slot name="force_delete_confirmation_message"/>
+							{#each $forceDeleteErrors as error}
+								<ShortParagraph>
+									{error.message}
+								</ShortParagraph>
+							{/each}
+						{/if}
 					</Flex>
 				</div>
-				<div class="mdc-card__actions">
-					<div class="mdc-card__action-buttons">
-						<TextButton
-							class="mdc-button--raised"
-							kind="button"
-							disabled={isConnectingToDelete}
-							label="Confirm"
-							on:click={confirmDelete}/>
-						<TextButton
-							class="mdc-button--outlined"
-							kind="button"
-							disabled={isConnectingToDelete}
-							label="Cancel"
-							on:click={startReading}/>
-					</div>
+				<div class="card_action">
+					<TextCardButton
+						kind="button"
+						disabled={$isConfirming}
+						label="Confirm"
+						on:click={confirmAction}/>
+					<TextCardButton
+						kind="button"
+						label="Cancel"
+						on:click={startReading}/>
 				</div>
 			</article>
 		{:else if isEditing}
 			<slot
 				name="edit_form"
 				{confirmEdit}
-				{cancelEdit}/>
+				{cancelEdit}
+				isConnecting={$isConfirming}
+				errors={$updateErrors}/>
 		{:else}
-			<TextButton
-				class="mdc-button--raised"
-				kind="button"
-				label="Edit"
-				on:click={startEditing}/>
-			<TextButton
-				class="mdc-button--outlined"
-				kind="button"
-				label="Delete"
-				on:click={confirmDeletion}/>
+			<slot name="other_info"/>
+			{#if isArchived}
+				<TextButton
+					kind="submit"
+					label="Restore"
+					on:click={confirmRestoration}/>
+				<TextButton
+					kind="button"
+					label="Force Delete"
+					on:click={confirmForceDeletion}/>
+			{:else}
+				<TextButton
+					kind="button"
+					label="Edit"
+					on:click={startEditing}/>
+				<TextButton
+					kind="button"
+					label="Delete"
+					on:click={confirmDeletion}/>
+			{/if}
 			<slot name="additional_buttons"/>
 		{/if}
 	</DataTableCell>
 </DataTableRow>
 
 <style lang="scss">
-	@use "@/components/third-party/index";
-
-	@use "@material/card";
-
-	@include card.core-styles;
+@use "@/components/third-party/index";
 </style>
