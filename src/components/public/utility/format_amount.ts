bc5077b9476155b8254a879d15a848804c2f116a
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-12-08 03:03:38 +0800

    feat(component): base formatting precision on currency if possible

--- src/components/public/utility/format_amount.ts
+++ src/components/public/utility/format_amount.ts
@@ -5,8 +5,13 @@ import { DEFAULT_MINIMUM_FRACTION_DIGITS, DEFAULT_MAXIMUM_FRACTION_DIGITS } from
 import { formatAmount as formatAmountStrictly } from "!/index"
 
 export default function formatAmount(currency: Currency | undefined, amount: string): string {
-	const minimumFractionDigits = DEFAULT_MINIMUM_FRACTION_DIGITS
-	const maximumFractionDigits = DEFAULT_MAXIMUM_FRACTION_DIGITS
+	const minimumFractionDigits = typeof currency === "undefined"
+		? DEFAULT_MINIMUM_FRACTION_DIGITS
+		: currency.presentational_precision
+	const maximumFractionDigits = typeof currency === "undefined"
+		? DEFAULT_MAXIMUM_FRACTION_DIGITS
+		: currency.presentational_precision
+
 	const isNegative = amount.startsWith("-")
 	const cleanAmount = isNegative ? amount.slice(1): amount
 
