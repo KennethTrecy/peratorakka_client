9b2ec507789ca5e35ada01cdf941d2b3310793b3
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-12-08 00:15:38 +0800

    feat(component): update table account cells

--- src/components/public/catalog/data_table_account_cell.svelte
+++ src/components/public/catalog/data_table_account_cell.svelte
@@ -1,24 +1,28 @@
 <script lang="ts">
-	import type { DataTableCellKind } from "+/component"
+import type { DataTableCellKind } from "+/component"
 
-	import DataTableCell from "$/catalog/data_table_cell.svelte"
+import DataTableCell from "$/catalog/data_table_cell.svelte"
 
-	export let kind: DataTableCellKind = "normal"
-	export let rawDebitExistence: boolean[]
-	export let rawDebit: any[]
-	export let rawCreditExistence: boolean[]
-	export let rawCredit: any[]
+export let kind: DataTableCellKind = "normal"
+export let rawDebitExistence: boolean[]
+export let rawDebit: any[]
+export let rawCreditExistence: boolean[]
+export let rawCredit: any[]
 
-	$: debits = kind === "normal" ? (rawDebit as string[]) : (rawDebit as number[])
-	$: credits = kind === "normal" ? (rawCredit as string[]) : (rawCredit as number[])
-	$: debitClasses = [
-		"debit",
-		kind
-	].join(" ")
-	$: creditClasses = [
-		"credit",
-		kind
-	].join(" ")
+$: debits = kind === "normal" ? (rawDebit as string[]) : (rawDebit as number[])
+$: credits = kind === "normal" ? (rawCredit as string[]) : (rawCredit as number[])
+$: debitClasses = [
+	"browser-default",
+	"debit",
+	"p-0",
+	kind
+].join(" ")
+$: creditClasses = [
+	"browser-default",
+	"credit",
+	"p-0",
+	kind
+].join(" ")
 </script>
 
 <DataTableCell {kind}>
@@ -39,39 +43,37 @@
 </DataTableCell>
 
 <style lang="scss">
-	ul {
-		list-style: none;
+ul {
+	list-style: none;
+}
+
+.debit {
+	text-align: left;
+
+	li[data-exists=false] {
+		text-decoration: line-through;
 	}
+}
 
-	.debit {
-		text-align: left;
+.credit {
+	text-align: right;
 
-		li[data-exists=false] {
-			text-decoration: line-through;
-		}
+	li[data-exists=false] {
+		text-decoration: line-through;
 	}
+}
 
-	.credit {
-		text-align: right;
+.debit.normal, .debit.numeric {
+	margin-right: 1em;
+}
 
-		li[data-exists=false] {
-			text-decoration: line-through;
-		}
-	}
+.credit.normal, .credit.numeric {
+	margin-left: 1em;
+}
 
-	.debit.normal {
-		margin-right: 1em;
-	}
-
-	.credit.normal {
-		margin-left: 1em;
-	}
-
-	.debit.numeric {
-		margin-right: 3em;
-	}
-
-	.credit.numeric {
-		margin-left: 3em;
-	}
+.numeric {
+	overflow: clip;
+	text-overflow: clip;
+	white-space: nowrap;
+}
 </style>
