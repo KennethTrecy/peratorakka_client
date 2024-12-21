45014ec392b45536a64058e332003fcdda29d268
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-12-21 15:59:31 +0800

    internal(component): make field for checkboxes

---
+++ src/components/public/form/checkbox_field.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+import type { GeneralError } from "+/rest"
+
+import GeneralContent from "$/form/general_content.svelte"
+
+export let fieldName: string
+export let disabled: boolean
+export let value: boolean
+export let errorFieldID = ""
+export let IDPrefix: string = ""
+export let errors: GeneralError[]
+</script>
+
+<div>
+	<GeneralContent
+		{fieldName}
+		{errorFieldID}
+		{IDPrefix}
+		{errors}
+		let:fieldID
+		let:labelID
+		let:helperID>
+		<label for={fieldID} id={labelID}>
+			<input
+				id={fieldID}
+				type="checkbox"
+				class="filled-in"
+				bind:value={value}
+				disabled={disabled}
+				aria-labelledby={labelID}
+				aria-controls={helperID}
+				aria-describedby={helperID}/>
+			<span>{fieldName}</span>
+		</label>
+	</GeneralContent>
+</div>
+
+<style lang="scss">
+@use "@/components/third-party/index";
+
+input[type="checkbox"].filled-in:checked + span:not(.lever)::before {
+	--md-sys-color-on-primary: var(--md-ref-palette-neutral80);
+}
+</style>
