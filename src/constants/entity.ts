7d9e58fcedcf909d37a0cc516f4167f65caa1fbd
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-12-21 16:53:36 +0800

    internal(constant): make constants for usage of numerical tools

--- src/constants/entity.ts
+++ src/constants/entity.ts
@@ -85,3 +85,69 @@ export const exchangeRateBases = [
 	UNKNOWN_EXCHANGE_RATE_BASIS,
 	...acceptableExchangeRateBases
 ] as const
+
+const UNKNOWN_NUMERICAL_TOOL_KIND = "unknown"
+const NUMBER_NUMERICAL_TOOL_KIND = "number"
+const TABLE_NUMERICAL_TOOL_KIND = "table"
+const PIE_NUMERICAL_TOOL_KIND = "pie"
+const LINE_NUMERICAL_TOOL_KIND = "line"
+
+export const acceptableNumericalToolKinds = [
+	NUMBER_NUMERICAL_TOOL_KIND,
+	TABLE_NUMERICAL_TOOL_KIND,
+	PIE_NUMERICAL_TOOL_KIND,
+	LINE_NUMERICAL_TOOL_KIND
+] as const
+
+export const numericalToolKinds = [
+	UNKNOWN_NUMERICAL_TOOL_KIND,
+	...acceptableNumericalToolKinds
+] as const
+
+const UNKNOWN_NUMERICAL_TOOL_RECURRENCE_PERIOD = "unknown"
+export const PERIODIC_NUMERICAL_TOOL_RECURRENCE_PERIOD = "periodic"
+export const YEARLY_NUMERICAL_TOOL_RECURRENCE_PERIOD = "yearly"
+
+export const acceptableNumericalToolRecurrencePeriods = [
+	PERIODIC_NUMERICAL_TOOL_RECURRENCE_PERIOD,
+	YEARLY_NUMERICAL_TOOL_RECURRENCE_PERIOD
+] as const
+
+export const numericalToolRecurrencePeriods = [
+	UNKNOWN_NUMERICAL_TOOL_RECURRENCE_PERIOD,
+	...acceptableNumericalToolRecurrencePeriods
+] as const
+
+const UNKNOWN_AMOUNT_STAGE_BASIS = "unknown"
+const OPENED_AMOUNT_STAGE_BASIS = "opened"
+const UNADJUSTED_AMOUNT_STAGE_BASIS = "unadjusted"
+const CLOSED_AMOUNT_STAGE_BASIS = "closed"
+
+export const acceptableAmountStageBases = [
+	OPENED_AMOUNT_STAGE_BASIS,
+	UNADJUSTED_AMOUNT_STAGE_BASIS,
+	CLOSED_AMOUNT_STAGE_BASIS
+] as const
+
+export const amountStageBases = [
+	UNKNOWN_AMOUNT_STAGE_BASIS,
+	...acceptableAmountStageBases
+] as const
+
+const UNKNOWN_AMOUNT_SIDE_BASIS = "unknown"
+const DEBIT_AMOUNT_SIDE_BASIS = "debit"
+const CREDIT_AMOUNT_SIDE_BASIS = "credit"
+const NET_DEBIT_AMOUNT_SIDE_BASIS = "net_debit"
+const NET_CREDIT_AMOUNT_SIDE_BASIS = "net_credit"
+
+export const acceptableAmountSideBases = [
+	DEBIT_AMOUNT_SIDE_BASIS,
+	CREDIT_AMOUNT_SIDE_BASIS,
+	NET_DEBIT_AMOUNT_SIDE_BASIS,
+	NET_CREDIT_AMOUNT_SIDE_BASIS
+] as const
+
+export const amountSideBases = [
+	UNKNOWN_AMOUNT_SIDE_BASIS,
+	...acceptableAmountSideBases
+] as const
