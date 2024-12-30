570f371d74155a40c0526573c243af334bf788d9
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-12-30 12:10:28 +0800

    internal(type): allow other classes for grid cell

--- src/components/public/layout/grid_cell.svelte
+++ src/components/public/layout/grid_cell.svelte
@@ -1,28 +1,31 @@
 <script lang="ts">
-	import type { GridCellKind } from "+/component"
+import type { GridCellKind } from "+/component"
 
-	export let kind: GridCellKind
+export let kind: GridCellKind
+let otherClasses: string = ""
 
-	$: cellClasses =  (
-		kind === "wide"
-		? [ "s6", "m6", "l6" ]
-		: kind === "narrow"
+export { otherClasses as class }
+
+$: cellClasses =  ((
+	kind === "wide"
+	? [ "s6", "m6", "l6" ]
+	: kind === "narrow"
+		? [ "s3", "m3", "l3" ]
+		: kind === "padder"
 			? [ "s3", "m3", "l3" ]
-			: kind === "padder"
-				? [ "s3", "m3", "l3" ]
-				: kind === "full"
-					? [ "s12" ]
-					: kind === "majority"
-						? [ "s12", "m7", "l8" ]
-						: kind === "minority"
-							? [ "s12", "m5", "l4" ]
-							: kind === "triad"
-								? [ "s12", "m4", "l4" ]
-								: kind === "pair"
-									? [ "s12", "m6", "l6" ]
-									: []
-		).join(" ")
-	$: role = kind === "padder" ? "presentation" : null
+			: kind === "full"
+				? [ "s12" ]
+				: kind === "majority"
+					? [ "s12", "m7", "l8" ]
+					: kind === "minority"
+						? [ "s12", "m5", "l4" ]
+						: kind === "triad"
+							? [ "s12", "m4", "l4" ]
+							: kind === "pair"
+								? [ "s12", "m6", "l6" ]
+								: []
+	).join(" ")+" "+otherClasses).trim()
+$: role = kind === "padder" ? "presentation" : null
 </script>
 
 <div class={cellClasses} {role}>
