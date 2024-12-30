16493ffc3768a8b6a8cc3beb5152e341e59bcd89
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-12-30 14:10:16 +0800

    internal(dashboard): make line display

--- src/components/private/dashboard/dashboard_card.svelte
+++ src/components/private/dashboard/dashboard_card.svelte
@@ -7,6 +7,7 @@ import { createEventDispatcher, onMount } from "svelte"
 import {
 	NUMBER_NUMERICAL_TOOL_KIND,
 	PIE_NUMERICAL_TOOL_KIND,
+	LINE_NUMERICAL_TOOL_KIND,
 	CURRENCY_FORMULA_OUTPUT_FORMAT,
 	RAW_FORMULA_OUTPUT_FORMAT
 } from "#/entity"
@@ -15,6 +16,7 @@ import { fallbackToAcceptableFormulaOutputFormat } from "+/entity"
 
 import NumberVisualizer from "%/dashboard/dashboard_card/number.svelte"
 import PieVisualizer from "%/dashboard/dashboard_card/pie.svelte"
+import LineVisualizer from "%/dashboard/dashboard_card/line.svelte"
 
 export let data: NumericalTool
 export let formulae: Formula[]
@@ -47,6 +49,12 @@ $: outputFormat = data.configuration.sources[0].type === "collection"
 			{conclusion}/>
 	{:else if data.kind === PIE_NUMERICAL_TOOL_KIND}
 		<PieVisualizer
+			numericalTool={data}
+			{currency}
+			{outputFormat}
+			{conclusion}/>
+	{:else if data.kind === LINE_NUMERICAL_TOOL_KIND}
+		<LineVisualizer
 			numericalTool={data}
 			{currency}
 			{outputFormat}
---
+++ src/components/private/dashboard/dashboard_card/line.svelte
@@ -0,0 +1,103 @@
+<script lang="ts">
+import type { Currency, NumericalTool, AcceptableFormulaOutputFormat } from "+/entity"
+import type { NumericalToolConclusion, AcceptableConstellationKind, Constellation } from "+/rest"
+
+import { Line } from "svelte-chartjs"
+import {
+	Chart as ChartJS,
+	Title,
+	Tooltip,
+	Legend,
+	LineElement,
+	LinearScale,
+	PointElement,
+	CategoryScale
+} from "chart.js"
+
+
+import formatStar from "$/utility/format_star"
+import generateColors from "$/utility/generate_colors"
+
+import Flex from "$/layout/flex.svelte"
+import GridCell from "$/layout/grid_cell.svelte"
+import ShortParagraph from "$/typography/short_paragraph.svelte"
+import WeakenedTertiaryHeading from "$/typography/weakened_tertiary_heading.svelte"
+
+export let numericalTool: NumericalTool
+export let currency: Currency|null
+export let outputFormat: AcceptableFormulaOutputFormat
+export let conclusion: NumericalToolConclusion
+
+$: timeTags = conclusion.time_tags
+$: timeTagCount = timeTags.length
+$: constellations = conclusion.constellations
+$: hasMultipleTimes = timeTags.length > 1
+
+$: reducedConstellations = constellations.filter(
+	constellation => constellation.stars.some(star => star.numerical_value !== 0)
+)
+$: [ backgroundColor, hoverBackgroundColor ] = generateColors(reducedConstellations.length)
+$: constellationInfo = {
+	"labels": timeTags,
+	"datasets": reducedConstellations.map((constellation, i) => ({
+		"label": constellation.name,
+		"data": constellation.stars.map(star => star.numerical_value),
+		"backgroundColor": backgroundColor[i],
+		"pointBackgroundColor": backgroundColor[i],
+		"pointHoverBackgroundColor": hoverBackgroundColor[i],
+		"fill": true,
+		"lineTension": 0.2
+	}))
+}
+$: options = {
+	"responsive": true,
+	"plugins": {
+		"tooltip": {
+			"callbacks": {
+				"title": function(context: any) {
+					const i = context[0].datasetIndex
+					const j = context[0].dataIndex
+
+					return `${reducedConstellations[i].name} (${timeTags[j]})`
+				},
+				"label": function(context: any) {
+					const i = context.datasetIndex
+					const j = context.dataIndex
+
+					return formatStar(
+						outputFormat,
+						currency,
+						reducedConstellations[i].stars[j]
+					)
+				}
+			}
+		}
+	}
+}
+
+ChartJS.register(Title, Tooltip, Legend, LineElement, LinearScale, PointElement, CategoryScale)
+</script>
+
+<GridCell kind="pair" class="line">
+	<article class="card">
+		<div class="card-content">
+			<Flex mustPad={false} justifyContent="center">
+				<WeakenedTertiaryHeading>
+					{numericalTool.name} Line Chart
+				</WeakenedTertiaryHeading>
+				<Line data={constellationInfo} {options}/>
+				<ShortParagraph>
+					The line chart above shows the from {timeTags[0]}{#if hasMultipleTimes}&nbsp; to {timeTags[timeTagCount - 1]}{/if}.
+				</ShortParagraph>
+			</Flex>
+		</div>
+	</article>
+</GridCell>
+
+<style lang="scss">
+@use "@/components/third-party/index";
+
+:global(.line) {
+	grid-row: span 3;
+}
+</style>
