c192ac75d0cd003927e2f5d23ace9585d27bff08
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-12-30 12:24:05 +0800

    internal(dashboard): make pie display

---
+++ src/components/private/dashboard/dashboard_card/pie.svelte
@@ -0,0 +1,123 @@
+<script lang="ts">
+import type { Currency, NumericalTool, AcceptableFormulaOutputFormat } from "+/entity"
+import type { NumericalToolConclusion, AcceptableConstellationKind, Constellation } from "+/rest"
+
+import { Pie } from "svelte-chartjs"
+import {
+	Chart as ChartJS,
+	Title,
+	Tooltip,
+	Legend,
+	ArcElement,
+	CategoryScale
+} from "chart.js"
+
+import { ACCEPTABLE_CONSTELLATION_KINDS } from "#/rest"
+
+import convertSnakeCaseToProperCase from "$/utility/convert_snake_case_to_proper_case"
+import formatStar from "$/utility/format_star"
+import generateColors from "$/utility/generate_colors"
+
+import Flex from "$/layout/flex.svelte"
+import GridCell from "$/layout/grid_cell.svelte"
+import ShortParagraph from "$/typography/short_paragraph.svelte"
+import WeakenedTertiaryHeading from "$/typography/weakened_tertiary_heading.svelte"
+
+export let numericalTool: NumericalTool
+export let currency: Currency|null
+export let outputFormat: AcceptableFormulaOutputFormat
+export let conclusion: NumericalToolConclusion
+
+$: timeTags = conclusion.time_tags
+$: timeTagCount = timeTags.length
+$: constellations = conclusion.constellations
+$: hasMultipleTimes = timeTags.length > 1
+
+$: reducedConstellations = constellations.filter(
+	constellation => constellation.stars.some(star => star.numerical_value !== 0)
+)
+$: constellationGroups = reducedConstellations.reduce((groups, constellation) => ({
+	...groups,
+	[constellation.kind]: typeof groups[constellation.kind] === "undefined" ? [
+		constellation
+	] : [
+		...groups[constellation.kind],
+		constellation
+	]
+}), {} as Record<AcceptableConstellationKind, Constellation[]>)
+$: constellationCollection = ACCEPTABLE_CONSTELLATION_KINDS.map(kind => ({
+	kind,
+	"group": constellationGroups[kind] ?? []
+})).filter(collection => collection.group.length > 0)
+$: constellationInfo = constellationCollection.map(collection => {
+	const [ backgroundColor, hoverBackgroundColor ] = generateColors(collection.group.length)
+	const labels = collection.group.map(constellation => constellation.name)
+	return {
+		"name": collection.kind,
+		"chartInfo": {
+			labels,
+			"datasets": timeTags.map((tag, i) => ({
+				"data": collection.group.map(
+					constellation => constellation.stars[timeTagCount - i - 1].numerical_value
+				),
+				backgroundColor,
+				hoverBackgroundColor
+			}))
+		},
+		"options": {
+			"responsive": true,
+			"plugins": {
+				"tooltip": {
+					"callbacks": {
+						"title": function(context: any) {
+							const i = context[0].datasetIndex
+							const j = context[0].dataIndex
+
+							return `${labels[j]} (${timeTags[timeTagCount - i - 1]})`
+						},
+						"label": function(context: any) {
+							const i = context.datasetIndex
+							const j = context.dataIndex
+
+							return formatStar(
+								outputFormat,
+								currency,
+								collection.group[j].stars[timeTagCount - i - 1]
+							)
+						}
+					}
+				}
+			}
+		}
+	}
+})
+
+ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale)
+</script>
+
+{#each constellationInfo as constellationDatum}
+	<GridCell kind="pair" class="pie">
+		<article class="card">
+			<div class="card-content">
+				<Flex mustPad={false} justifyContent="center">
+					<WeakenedTertiaryHeading>
+						Composition of {convertSnakeCaseToProperCase(constellationDatum.name)}s
+						(instructed by {numericalTool.name})
+					</WeakenedTertiaryHeading>
+					<Pie data={constellationDatum.chartInfo} options={constellationDatum.options}/>
+					<ShortParagraph>
+						The chart above shows the from {timeTags[0]}{#if hasMultipleTimes}&nbsp;(inner pie) to {timeTags[timeTagCount - 1]} (outer pie){/if}.
+					</ShortParagraph>
+				</Flex>
+			</div>
+		</article>
+	</GridCell>
+{/each}
+
+<style lang="scss">
+@use "@/components/third-party/index";
+
+:global(.pie) {
+	grid-row: span 4;
+}
+</style>
