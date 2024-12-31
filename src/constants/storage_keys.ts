17ec9aa2274153760464db3a4e6adf2ca41df0b6
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-12-31 19:20:13 +0800

    internal(constant): add new storage key for theme name

--- src/constants/storage_keys.ts
+++ src/constants/storage_keys.ts
@@ -4,6 +4,7 @@ export const CSRF_TOKEN_KEY = "csrf_token"
 export const ACCESS_TOKEN_KEY = "access_token"
 export const ACCESS_TOKEN_METADATA_KEY = "access_token_metadata"
 export const USER_EMAIL_KEY = "user_email"
+export const THEME_NAME_KEY = "theme_name"
 
 // @deprecated To be removed on any later version beyond v0.3.0
 export const USER_EMAIL_KEY_v0_2_1 = "user_email_key"
