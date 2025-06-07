032aea360977a235cbc8f4de748bf35c221ff168
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-06-07 23:22:56 +0800

    feat(currency): consider precision formats in the basic form

--- src/components/private/currencies/basic_form.svelte
+++ src/components/private/currencies/basic_form.svelte
@@ -1,23 +1,42 @@
 <script lang="ts">
 import type { GeneralError } from "+/rest"
+import type { PrecisionFormat } from "+/entity"
+
+import transformPrecisionFormat from "$/form/choice_info_transformer/transform_precision_format"
 
 import BasicForm from "$/form/basic_form.svelte"
+import ChoiceListField from "$/form/choice_list_field.svelte"
+import ElementalParagraph from "$/typography/elemental_paragraph.svelte"
+import TextContainer from "$/typography/text_container.svelte"
 import TextField from "$/form/text_field.svelte"
-import NumberField from "$/form/number_field.svelte"
 
 export let IDPrefix: string
+export let precisionFormats: PrecisionFormat[]
 
+export let precisionFormatID: string
 export let code: string
 export let name: string
-export let presentationalPrecision: number
 
 export let isConnecting: boolean
 export let errors: GeneralError[]
 export let id: string|null = null
+
+$: precisionFormat = precisionFormats.find(
+	precisionFormat => `${precisionFormat.id}` === precisionFormatID
+) as PrecisionFormat
 </script>
 
 <BasicForm {id} {isConnecting} {errors} on:submit>
 	<svelte:fragment slot="fields">
+		<ChoiceListField
+			fieldName="Precision Format"
+			errorFieldID="precision_format_id"
+			disabled={isConnecting}
+			bind:value={precisionFormatID}
+			rawChoices={precisionFormats}
+			choiceConverter={transformPrecisionFormat}
+			{IDPrefix}
+			{errors}/>
 		<TextField
 			fieldName="Code"
 			disabled={isConnecting}
@@ -30,15 +49,19 @@ export let id: string|null = null
 			bind:value={name}
 			{IDPrefix}
 			{errors}/>
-		<NumberField
-			fieldName="Presentational Precision"
-			min={0}
-			max={12}
-			step={1}
-			disabled={isConnecting}
-			bind:value={presentationalPrecision}
-			{IDPrefix}
-			{errors}/>
+		<TextContainer>
+			<ElementalParagraph>
+				Selected precision format may
+				{#if (
+					precisionFormat.minimum_presentational_precision === 0
+					&& precisionFormat.maximum_presentational_precision === 0
+				)}
+					not shown any decimal places.
+				{:else}
+					show {precisionFormat.minimum_presentational_precision === 0 ? "" : `at least ${precisionFormat.minimum_presentational_precision} decimal places and `} {precisionFormat.maximum_presentational_precision} decimal places at most.
+				{/if}
+			</ElementalParagraph>
+		</TextContainer>
 	</svelte:fragment>
 	<slot slot="button_group" name="button_group"/>
 </BasicForm>
