7f7d908192444619320ac0a30b7a53153a8dd4b8
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-06 07:36:49 +0800

    fix(component): migrate general content for form fields

--- src/components/public/form/general_content.svelte
+++ src/components/public/form/general_content.svelte
@@ -1,39 +1,46 @@
 <script lang="ts">
+import type { Snippet } from "svelte"
 import type { GeneralError } from "+/rest"
 
 import { isFieldError } from "+/rest"
 
-export let fieldName: string
-export let supportText: string = ""
-export let errorFieldID: string
-export let IDPrefix: string
-export let errors: GeneralError[]
+let {
+	fieldName,
+	supportText = "",
+	errorFieldID,
+	IDPrefix,
+	errors,
+	children
+}: {
+	fieldName: string
+	supportText?: string
+	errorFieldID: string
+	IDPrefix: string
+	errors: GeneralError[]
+	children: Snippet<[ { fieldID: string, labelID: string, helperID: string } ]>;
+} = $props()
 
-$: normalizedFieldName = fieldName.replaceAll(" ", "_", ).toLocaleLowerCase()
-$: fieldID = fieldName === ""
+let normalizedFieldName = $derived(fieldName.replaceAll(" ", "_", ).toLocaleLowerCase())
+let fieldID = $derived(fieldName === ""
 	? errorFieldID
 	: (
 		IDPrefix === ""
 			? ""
 			: `${IDPrefix}_`
-	) + normalizedFieldName
-$: resolvedErrorFieldID = errorFieldID === ""
+	) + normalizedFieldName)
+let resolvedErrorFieldID = $derived(errorFieldID === ""
 	? normalizedFieldName
-	: errorFieldID
-$: labelID = `${fieldID}_label`
-$: helperID = `${fieldID}_helper`
-$: message = errors.filter(
+	: errorFieldID)
+let labelID = $derived(`${fieldID}_label`)
+let helperID = $derived(`${fieldID}_helper`)
+let message = $derived(errors.filter(
 	error => isFieldError(error) && error.field.endsWith(resolvedErrorFieldID)
-).map(error => error.message).join(" ")
+).map(error => error.message).join(" "))
 </script>
 
-<slot {fieldID} {labelID} {helperID}></slot>
+{@render children({ fieldID, labelID, helperID, })}
 {#if message !== ""}
 	<div class="supporting-text" id={helperID}>{message}</div>
 {:else if supportText !== ""}
 	<div class="supporting-text" id={helperID}>{supportText}</div>
 {/if}
-
-<style lang="scss">
-@use "@/components/third-party/index";
-</style>
