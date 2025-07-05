947c0907954391ce115e4b1643ab0fc9759c184c
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-06 07:59:05 +0800

    fix(component): migrate form base

--- src/components/public/form/base.svelte
+++ src/components/public/form/base.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+import type { Snippet } from "svelte"
 import type { GeneralError } from "+/rest"
 
 import { isSimpleError } from "+/rest"
@@ -6,45 +7,57 @@ import { isSimpleError } from "+/rest"
 import Flex from "$/layout/flex.svelte"
 import ReactiveProgressBar from "$/utility/reactive_progress_bar.svelte"
 import ShortParagraph from "$/typography/short_paragraph.svelte"
-import TextContainer from "$/typography/text_container.svelte"
 
-export let errors: GeneralError[]
-export let id: string|null
-export let isConnecting: boolean
+let {
+	errors,
+	id,
+	isConnecting,
+	onsubmit,
+	lead_content,
+	field_content,
+	action_buttons
+}: {
+	errors: GeneralError[]
+	id: string|null
+	isConnecting: boolean
+	onsubmit: (event: SubmitEvent) => void
+	lead_content?: Snippet
+	field_content: Snippet
+	action_buttons: Snippet
+} = $props()
 
-$: progressBarLabel = isConnecting
-	? "Waiting for server's response..."
-	: (
-		errors.length > 0
-		? "Failed to submit the details. Please check the errors."
-		: "Enter valid details only and submit the form."
-	)
-$: simpleErrors = errors.filter(isSimpleError).map(error => error.message).join(" ")
-$: hasSimpleErrors = simpleErrors.length > 0
+let progressBarLabel = $derived(
+	isConnecting
+		? "Waiting for server's response..."
+		: (
+			errors.length > 0
+			? "Failed to submit the details. Please check the errors."
+			: "Enter valid details only and submit the form."
+		)
+)
+let simpleErrors = $derived(errors.filter(isSimpleError).map(error => error.message).join(" "))
+let hasSimpleErrors = $derived(simpleErrors.length > 0)
+
+function submit(event: SubmitEvent) {
+	event.preventDefault()
+	onsubmit(event)
+}
 </script>
 
-<form class="card" {id} on:submit|preventDefault>
+<form class="card" {id} onsubmit={submit}>
 	<ReactiveProgressBar {progressBarLabel} isLoading={isConnecting}/>
 	<div class="card-content">
-		{#if $$slots.lead_content}
-			<slot name="lead_content"/>
-		{/if}
+		{@render lead_content?.()}
+		{@render field_content()}
 		{#if hasSimpleErrors}
-			<Flex justifyContent="start">
-				<TextContainer>
-					<ShortParagraph>
-						{simpleErrors}
-					</ShortParagraph>
-				</TextContainer>
+			<Flex justifyContent="start" mustPad={false}>
+				<ShortParagraph>
+					{simpleErrors}
+				</ShortParagraph>
 			</Flex>
 		{/if}
-		<slot name="field_content"/>
 	</div>
 	<div class="card-action">
-		<slot name="action_buttons"/>
+		{@render action_buttons()}
 	</div>
 </form>
-
-<style lang="scss">
-@use "@/components/third-party/index";
-</style>
