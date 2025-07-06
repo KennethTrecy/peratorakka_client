ed2546cde47e8f02be35f7a55915678e13be67ad
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-06 12:27:39 +0800

    fix(cash flow activity): migrate page

--- src/routes/cash_flow_activities/+page.svelte
+++ src/routes/cash_flow_activities/+page.svelte
@@ -1,25 +1,19 @@
 <script lang="ts">
 import type { ContextBundle } from "+/component"
 import type { CashFlowActivity } from "+/entity"
-import type { SearchMode, SortOrder } from "+/rest"
 
-import { get, writable } from "svelte/store"
-import { onMount, getContext } from "svelte"
+import { getContext } from "svelte"
 import { afterNavigate, beforeNavigate, goto } from "$app/navigation"
 
 import { GLOBAL_CONTEXT } from "#/contexts"
-import { SEARCH_NORMALLY, ASCENDING_ORDER, MAXIMUM_PAGINATED_LIST_LENGTH } from "#/rest"
 
 import assertAuthentication from "$/page_requirement/assert_authentication"
-import makeJSONRequester from "$/rest/make_json_requester"
 
-import AddForm from "%/cash_flow_activities/add_form.svelte"
-import ArticleGrid from "$/layout/article_grid.svelte"
-import Collection from "%/cash_flow_activities/collection.svelte"
-import ExtraResourceLoader from "$/catalog/extra_resource_loader.svelte"
-import GridCell from "$/layout/grid_cell.svelte"
-import InnerGrid from "$/layout/inner_grid.svelte"
-import PrimaryHeading from "$/typography/primary_heading.svelte"
+import BasicForm from "%/cash_flow_activities/basic_form.svelte"
+import CompleteResourcePage from "$/layout/complete_resource_page.svelte"
+import ElementalParagraph from "$/typography/elemental_paragraph.svelte"
+import Card from "%/cash_flow_activities/cash_flow_activity_card.svelte"
+import TextContainer from "$/typography/text_container.svelte"
 
 const globalContext = getContext(GLOBAL_CONTEXT) as ContextBundle
 
@@ -29,125 +23,94 @@ assertAuthentication(globalContext, {
 	goto
 })
 
-let cashFlowActivities: CashFlowActivity[] = []
-
-let searchMode: SearchMode = SEARCH_NORMALLY
-let sortCriterion: string = "name"
-let sortOrder: SortOrder = ASCENDING_ORDER
-let lastOffset: number = 0
-
-const collectiveName = "cash_flow_activities"
-const partialPath = `/api/v1/${collectiveName}`
-let parameters: [string, string][] = [
-	[ "filter[search_mode]", searchMode as string ],
-	[ "sort[0][0]", sortCriterion ],
-	[ "sort[0][1]", sortOrder as string ]
-]
-let completePath = writable(partialPath)
-$: {
-	parameters = [
-		[ "filter[search_mode]", searchMode as string ],
-		[ "sort[0][0]", sortCriterion ],
-		[ "sort[0][1]", sortOrder as string ],
-	]
-
-	completePath.set(`${partialPath}?${
-		new URLSearchParams([
-			...parameters,
-			[ "page[offset]", `${lastOffset}` ],
-			[ "page[limit]", MAXIMUM_PAGINATED_LIST_LENGTH ]
-		]).toString()
-	}`)
+function deriveID(resource: unknown): string {
+	return `${(resource as CashFlowActivity).id}`
 }
 
-let { isConnecting, errors, send } = makeJSONRequester({
-	"path": completePath,
-	"defaultRequestConfiguration": {
-		"method": "GET"
-	},
-	"manualResponseHandlers": [
-		{
-			"statusCode": 200,
-			"action": async (response: Response) => {
-				let responseDocument = await response.json()
-				errors.set([])
-				cashFlowActivities = responseDocument[collectiveName]
-				lastOffset = Math.max(0, responseDocument[collectiveName].length - 1)
-			}
+let name = $state("")
+let description = $state("")
+
+function makeNewResourceObject(): Record<string, unknown> {
+	return {
+		"cash_flow_activity": {
+			name,
+			description
 		}
-	],
-	"expectedErrorStatusCodes": [ 401 ]
-})
-
-async function reloadCashFlowActivities() {
-	cashFlowActivities = []
-	await send({})
-}
-
-async function loadList() {
-	const currentServerURL = get(globalContext.serverURL as any)
-
-	if (currentServerURL === "") {
-		setTimeout(loadList, 1000)
-		return
 	}
-
-	await reloadCashFlowActivities()
 }
 
-onMount(loadList)
+function processCreatedResourceObject(document: Record<string, unknown>): unknown {
+	name = ""
+	description = ""
 
-function addCashFlowActivity(event: CustomEvent<CashFlowActivity>) {
-	if (searchMode === "only_deleted") return
-
-	const newCashFlowActivities = event.detail
-	cashFlowActivities = [
-		newCashFlowActivities,
-		...cashFlowActivities
-	]
-}
-
-function addCashFlowActivities(event: CustomEvent<unknown[]>) {
-	const newCashFlowActivities = event.detail as CashFlowActivity[]
-	cashFlowActivities = [
-		...cashFlowActivities,
-		...newCashFlowActivities
-	]
-}
-
-function removeCashFlowActivities(event: CustomEvent<CashFlowActivity>) {
-	const oldCashFlowActivity = event.detail
-	cashFlowActivities = cashFlowActivities.filter(
-		cashFlowActivity => cashFlowActivity.id !== oldCashFlowActivity.id
-	)
+	const { "cash_flow_activity": cashFlowActivity } = document
+	return cashFlowActivity
 }
 </script>
 
-<svelte:head>
-	<title>Cash Flow Activities</title>
-</svelte:head>
-
-<ArticleGrid>
-	<InnerGrid>
-		<GridCell kind="full">
-			<PrimaryHeading>Cash Flow Activities</PrimaryHeading>
-		</GridCell>
-		<AddForm on:create={addCashFlowActivity}/>
-		<Collection
-			data={cashFlowActivities}
-			bind:searchMode={searchMode}
-			bind:sortCriterion={sortCriterion}
-			bind:sortOrder={sortOrder}
-			isConnecting={$isConnecting}
-			listErrors={$errors}
-			on:remove={removeCashFlowActivities}/>
-		<ExtraResourceLoader
-			isConnectingForInitialList={$isConnecting}
-			{partialPath}
-			{parameters}
-			{collectiveName}
-			bind:lastOffset={lastOffset}
-			on:reloadFully={reloadCashFlowActivities}
-			on:addResources={addCashFlowActivities}/>
-	</InnerGrid>
-</ArticleGrid>
+{#snippet general_description()}
+<TextContainer>
+	<ElementalParagraph>
+		Cash flow activities are used to classify changes in the current amount of accounts and
+		determine the amount of available liquid cash and cash equivalents. Usual cash flow
+		activities are operating activities, investment activities, and financing activities.
+	</ElementalParagraph>
+	<ElementalParagraph>
+		A liquid financial account is an account that can be converted into cash immediately
+		(e.g. bank deposits or physical cash in wallet). These are the accounts that do not need
+		to be associated with cash flow activity in modifiers.
+	</ElementalParagraph>
+	<ElementalParagraph>
+		Meanwhile, an illiquid financial account means that the account can be converted into
+		cash after a long period or never at all (e.g. land or equipments). These accounts are
+		required to be associated with a cash flow activity in modifiers.
+	</ElementalParagraph>
+</TextContainer>
+{/snippet}
+<CompleteResourcePage
+	pageTitle="Cash Flow Activities"
+	createTitle="Add Cash Flow Activity"
+	listTitle="Available Cash Flow Activities"
+	collectiveName="cash_flow_activities"
+	defaultSortCriterion="name"
+	availableSortCriteria={[
+		"name",
+		"created_at",
+		"updated_at",
+		"deleted_at"
+	]}
+	{deriveID}
+	{makeNewResourceObject}
+	{processCreatedResourceObject}
+	description={general_description}>
+	{#snippet form({ IDPrefix, isConnecting, errors, onsubmit, button_group })}
+		<BasicForm
+			bind:name={name}
+			bind:description={description}
+			{isConnecting}
+			{IDPrefix}
+			{errors}
+			{onsubmit}
+			{button_group}>
+		</BasicForm>
+	{/snippet}
+	{#snippet filled_collection_description()}
+		Below are the cash flow activity that you have added on to your profile.
+		They can be associated to modifiers.
+	{/snippet}
+	{#snippet empty_collection_description({ isPresent, isPresentAndArchived, isArchived })}
+		There are no available cash flow activity at the moment.
+		{#if isPresent}Create{/if}{#if isPresentAndArchived}/{/if}{#if isArchived}Delete{/if}
+		a cash flow activity to view.
+	{/snippet}
+	{#snippet collection_items({ resources, updateResource, removeResource })}
+		{#each resources as entity, i((entity as CashFlowActivity).id)}
+			<Card
+				bind:data={
+					() => entity as CashFlowActivity,
+					updatedResource => updateResource(updatedResource, i)
+				}
+				remove={removeResource}/>
+		{/each}
+	{/snippet}
+</CompleteResourcePage>
