30e6ce20a74469f0a9fe3413bfb4ae669e70632d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-06 08:21:50 +0800

    fix(component): migrate basic form

--- src/components/public/form/basic_form.svelte
+++ src/components/public/form/basic_form.svelte
@@ -1,17 +1,31 @@
 <script lang="ts">
-	import type { GeneralError } from "+/rest"
+import type { Snippet } from "svelte"
+import type { GeneralError } from "+/rest"
 
-	import Flex from "$/layout/flex.svelte"
-	import FormBase from "$/form/base.svelte"
+import Flex from "$/layout/flex.svelte"
+import FormBase from "$/form/base.svelte"
 
-	export let errors: GeneralError[]
-	export let id: string|null
-	export let isConnecting: boolean
+let {
+	errors,
+	id,
+	isConnecting,
+	onsubmit,
+	fields,
+	button_group
+}: {
+	errors: GeneralError[]
+	id: string|null
+	isConnecting: boolean
+	onsubmit: (event: SubmitEvent) => void
+	fields: Snippet
+	button_group: Snippet
+} = $props()
 </script>
 
-<FormBase {isConnecting} {id} {errors} on:submit>
-	<Flex slot="field_content" justifyContent="stretch" mustPad={false}>
-		<slot name="fields"/>
-	</Flex>
-	<slot slot="action_buttons" name="button_group"/>
+<FormBase {isConnecting} {id} {errors} {onsubmit} action_buttons={button_group}>
+	{#snippet field_content()}
+		<Flex justifyContent="stretch" mustPad={false}>
+			{@render fields()}
+		</Flex>
+	{/snippet}
 </FormBase>
