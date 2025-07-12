0c902c8aba0f10e238e46a0d64584265f4183bf1
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-12 22:29:47 +0800

    feat(financial entry): make atom container

---
+++ src/components/private/financial_entries/basic_form/atom_container.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+import type { GeneralError } from "+/rest"
+import type { ChoiceInfo } from "+/component"
+import type { Account, CompleteFinancialEntryAtomInput } from "+/entity"
+
+import { REAL_DEBIT_MODIFIER_ATOM_KIND } from "#/entity"
+
+import TextField from "$/form/text_field.svelte"
+import GeneralFieldContainer from "$/form/general_field_container.svelte"
+
+let {
+	atom = $bindable(),
+	IDPrefix,
+	disabled,
+	isConnecting,
+	errors,
+	transformAccount
+}: {
+	atom: CompleteFinancialEntryAtomInput
+	IDPrefix: string
+	disabled: boolean
+	isConnecting: boolean
+	errors: GeneralError[]
+	transformAccount: (account: Account) => ChoiceInfo
+} = $props()
+
+let label = $derived(`${
+	atom.modifier_atom.kind === REAL_DEBIT_MODIFIER_ATOM_KIND
+		? "Debit Amount"
+		: "Credit Amount"
+} for ${atom.account.name} (${atom.currency.code})`)
+</script>
+
+<GeneralFieldContainer tag="fieldset">
+	<TextField
+		fieldName={label}
+		errorFieldID="financial_entry_atoms"
+		disabled={isConnecting || disabled}
+		bind:value={
+			() => atom.input.numerical_value,
+			value => atom = {
+				...atom,
+				"input": {
+					...atom.input,
+					"numerical_value": value
+				}
+			}}
+		{IDPrefix}
+		{errors}/>
+</GeneralFieldContainer>
