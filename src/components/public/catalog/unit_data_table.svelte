7c1d13e443923f447c1ba6f057eb3a33cd3f29d0
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-13 15:27:58 +0800

    fix(component): migrate unit data table

--- src/components/public/catalog/unit_data_table.svelte
+++ src/components/public/catalog/unit_data_table.svelte
@@ -1,28 +1,34 @@
 <script lang="ts">
-	import DataTableRow from "$/catalog/data_table_row.svelte"
+import type { Snippet } from "svelte"
+
+import DataTableRow from "$/catalog/data_table_row.svelte"
+
+let { table_headers, table_rows, table_footer_cells }: {
+	table_headers: Snippet
+	table_rows: Snippet
+	table_footer_cells: Snippet
+} = $props()
 </script>
 
 <table class="body-normal">
 	<thead>
 		<DataTableRow>
-			<slot name="table_headers"/>
+			{@render table_headers?.()}
 		</DataTableRow>
 	</thead>
 	<tbody>
-		<slot name="table_rows"/>
+		{@render table_rows?.()}
 	</tbody>
-	{#if $$slots.table_footer_cells}
+	{#if table_footer_cells}
 		<tfoot>
 			<DataTableRow>
-				<slot name="table_footer_cells"/>
+				{@render table_footer_cells?.()}
 			</DataTableRow>
 		</tfoot>
 	{/if}
 </table>
 
 <style lang="scss">
-@use "@/components/third-party/index";
-
 table {
 	white-space: wrap;
 	max-width: 100%;
