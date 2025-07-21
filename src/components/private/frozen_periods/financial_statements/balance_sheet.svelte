6149f4a6ead7e46cfab9fd75f6b5977f5a370d2a
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-21 15:21:02 +0800

    fix(frozen period): migrate balance sheet

--- src/components/private/frozen_periods/financial_statements/balance_sheet.svelte
+++ src/components/private/frozen_periods/financial_statements/balance_sheet.svelte
@@ -1,122 +1,133 @@
 <script lang="ts">
-	import type { Currency, Account, SummaryCalculation } from "+/entity"
-	import type { FinancialStatementGroup, ExchangeRateInfo } from "+/rest"
+import type { PrecisionFormat, Currency } from "+/entity"
+import type { FinancialStatementGroup, ExchangeRateInfo } from "+/rest"
+import type { SimplifiedSummaryCalculation } from "+/component"
 
-	import { EMPTY_AMOUNT } from "#/component"
-	import {
-		DEPRECIATIVE_ASSET_ACCOUNT_KIND,
-		EQUITY_ACCOUNT_KIND,
-		GENERAL_ASSET_ACCOUNT_KIND,
-		LIABILITY_ACCOUNT_KIND,
-		LIQUID_ASSET_ACCOUNT_KIND
-	} from "#/entity"
+import { assetAccountKinds, EQUITY_ACCOUNT_KIND, LIABILITY_ACCOUNT_KIND } from "#/entity"
 
-	import deriveExchangeRateQuickly from "$/utility/derive_exchange_rate_quickly"
+import makeCleanShownAmount from "$/utility/make_clean_shown_amount"
 
-	import CustomTrialRow from "%/frozen_periods/financial_statements/custom_trial_row.svelte"
-	import DataTableHeader from "$/catalog/data_table_header.svelte"
-	import QuarternaryHeading from "$/typography/quarternary_heading.svelte"
-	import TrialRow from "%/frozen_periods/financial_statements/trial_row.svelte"
-	import UnitDataTable from "$/catalog/unit_data_table.svelte"
+import CustomTrialRow from "%/frozen_periods/financial_statements/custom_trial_row.svelte"
+import DataTableHeader from "$/catalog/data_table_header.svelte"
+import QuarternaryHeading from "$/typography/quarternary_heading.svelte"
+import TrialRow from "%/frozen_periods/financial_statements/trial_row.svelte"
+import UnitDataTable from "$/catalog/unit_data_table.svelte"
 
-	export let statement: FinancialStatementGroup
-	export let exchangeRates: ExchangeRateInfo[]
-	export let viewedCurrency: Currency
-	export let currencies: Currency[]
-	export let accounts: Account[]
-	export let data: Omit<SummaryCalculation, "frozen_period_id">[]
+let {
+	statement,
+	statementExchangeRate,
+	statementCurrency,
+	viewedCurrency,
+	precisionFormats,
+	currencies,
+	data
+}: {
+	statement: FinancialStatementGroup
+	statementExchangeRate: ExchangeRateInfo
+	statementCurrency: Currency
+	viewedCurrency: Currency
+	precisionFormats: PrecisionFormat[]
+	currencies: Currency[]
+	data: SimplifiedSummaryCalculation[]
+} = $props()
 
-	$: assetAccounts = accounts.filter(account => [
-		GENERAL_ASSET_ACCOUNT_KIND,
-		LIQUID_ASSET_ACCOUNT_KIND,
-		DEPRECIATIVE_ASSET_ACCOUNT_KIND
-	].indexOf(account.kind) > -1)
-	$: assetAccountIDs = assetAccounts.map(account => account.id)
-	$: liabilityAccounts = accounts.filter(account => account.kind === LIABILITY_ACCOUNT_KIND)
-	$: liabilityAccountIDs = liabilityAccounts.map(account => account.id)
-	$: equityAccounts = accounts.filter(account => account.kind === EQUITY_ACCOUNT_KIND)
-	$: equityAccountIDs = equityAccounts.map(account => account.id)
-
-	$: assetCalculations = data.filter(
-		calculation => assetAccountIDs.indexOf(calculation.account_id) > -1
-	)
-	$: liabilityCalculations = data.filter(
-		calculation => liabilityAccountIDs.indexOf(calculation.account_id) > -1
-	)
-	$: equityCalculations = data.filter(
-		calculation => equityAccountIDs.indexOf(calculation.account_id) > -1
-	)
-
-	$: exchangeRate = deriveExchangeRateQuickly(
-		statement.currency_id,
-		viewedCurrency.id,
-		currencies,
-		exchangeRates
-	)
+let assetCalculations = $derived(data.filter(
+	calculation => assetAccountKinds.indexOf(calculation.account.kind) > -1
+))
+let liabilityCalculations = $derived(data.filter(
+	calculation => calculation.account.kind === LIABILITY_ACCOUNT_KIND
+))
+let equityCalculations = $derived(data.filter(
+	calculation => calculation.account.kind === EQUITY_ACCOUNT_KIND
+))
+let friendlyTotalAssetInfo = $derived(makeCleanShownAmount(
+	precisionFormats,
+	currencies,
+	statementExchangeRate,
+	statementCurrency,
+	viewedCurrency,
+	statement.balance_sheet.total_assets
+))
+let friendlyDebitedTotalAsset = $derived(
+	friendlyTotalAssetInfo[0] ? "" : friendlyTotalAssetInfo[1]
+)
+let friendlyCreditedTotalAsset = $derived(
+	friendlyTotalAssetInfo[0] ? friendlyTotalAssetInfo[1] : ""
+)
+let friendlyTotalLiabilities = $derived(makeCleanShownAmount(
+	precisionFormats,
+	currencies,
+	statementExchangeRate,
+	statementCurrency,
+	viewedCurrency,
+	statement.balance_sheet.total_liabilities
+))
+let friendlyDebitedTotalLiabilities = $derived(
+	friendlyTotalLiabilities[0] ? friendlyTotalLiabilities[1] : ""
+)
+let friendlyCreditedTotalLiabilities = $derived(
+	friendlyTotalLiabilities[0] ? "" : friendlyTotalLiabilities[1]
+)
+let friendlyTotalEquities = $derived(makeCleanShownAmount(
+	precisionFormats,
+	currencies,
+	statementExchangeRate,
+	statementCurrency,
+	viewedCurrency,
+	statement.balance_sheet.total_equities
+))
+let friendlyDebitedTotalEquities = $derived(
+	friendlyTotalEquities[0] ? friendlyTotalEquities[1] : ""
+)
+let friendlyCreditedTotalEquities = $derived(
+	friendlyTotalEquities[0] ? "" : friendlyTotalEquities[1]
+)
+let friendlyNetAmountInfo = $derived(makeCleanShownAmount(
+	precisionFormats,
+	currencies,
+	statementExchangeRate,
+	statementCurrency,
+	viewedCurrency,
+	statement.income_statement.net_total
+))
+let friendlyDebitedNetAmount = $derived(friendlyNetAmountInfo[0] ? friendlyNetAmountInfo[1] : "")
+let friendlyCreditedNetAmount = $derived(friendlyNetAmountInfo[0] ? "" : friendlyNetAmountInfo[1])
 </script>
 
 <QuarternaryHeading>Balance Sheet</QuarternaryHeading>
 <UnitDataTable>
-	<svelte:fragment slot="table_headers">
+	{#snippet table_headers()}
 		<DataTableHeader scope="column">Permanent Account</DataTableHeader>
 		<DataTableHeader scope="column" kind="numeric">Debit Amount</DataTableHeader>
 		<DataTableHeader scope="column" kind="numeric">Credit Amount</DataTableHeader>
-	</svelte:fragment>
-	<svelte:fragment slot="table_rows">
-		{#each assetCalculations as calculation(calculation.account_id)}
-			<TrialRow
-				{viewedCurrency}
-				{exchangeRates}
-				{currencies}
-				{accounts}
-				data={calculation}
-				kind="unadjusted"/>
+	{/snippet}
+	{#snippet table_rows()}
+		{#each assetCalculations as calculation(calculation.account.id)}
+			<TrialRow data={calculation}/>
 		{/each}
 		<CustomTrialRow
 			rowName="Total Assets"
-			currency={viewedCurrency}
-			{exchangeRate}
-			rawDebitAmount={statement.balance_sheet.total_assets}
-			rawCreditAmount={EMPTY_AMOUNT}
+			shownDebitAmount={friendlyDebitedTotalAsset}
+			shownCreditAmount={friendlyCreditedTotalAsset}
 			hasEmptyTrailingRow={true}/>
-
-		{#each liabilityCalculations as calculation(calculation.account_id)}
-			<TrialRow
-				{viewedCurrency}
-				{exchangeRates}
-				{currencies}
-				{accounts}
-				data={calculation}
-				kind="unadjusted"/>
+		{#each liabilityCalculations as calculation(calculation.account.id)}
+			<TrialRow data={calculation}/>
 		{/each}
 		<CustomTrialRow
 			rowName="Total Liabilities"
-			currency={viewedCurrency}
-			{exchangeRate}
-			rawDebitAmount={EMPTY_AMOUNT}
-			rawCreditAmount={statement.balance_sheet.total_liabilities}
+			shownDebitAmount={friendlyDebitedTotalLiabilities}
+			shownCreditAmount={friendlyCreditedTotalLiabilities}
 			hasEmptyTrailingRow={true}/>
-
-		{#each equityCalculations as calculation(calculation.account_id)}
-			<TrialRow
-				{viewedCurrency}
-				{exchangeRates}
-				{currencies}
-				{accounts}
-				data={calculation}
-				kind="unadjusted"/>
+		{#each equityCalculations as calculation(calculation.account.id)}
+			<TrialRow data={calculation}/>
 		{/each}
 		<CustomTrialRow
 			rowName="Net Income"
-			currency={viewedCurrency}
-			{exchangeRate}
-			rawDebitAmount={EMPTY_AMOUNT}
-			rawCreditAmount={statement.income_statement.net_total}/>
+			shownDebitAmount={friendlyDebitedNetAmount}
+			shownCreditAmount={friendlyCreditedNetAmount}/>
 		<CustomTrialRow
 			rowName="Total Equities"
-			currency={viewedCurrency}
-			{exchangeRate}
-			rawDebitAmount={EMPTY_AMOUNT}
-			rawCreditAmount={statement.balance_sheet.total_equities}/>
-	</svelte:fragment>
+			shownDebitAmount={friendlyDebitedTotalEquities}
+			shownCreditAmount={friendlyCreditedTotalEquities}/>
+	{/snippet}
 </UnitDataTable>
