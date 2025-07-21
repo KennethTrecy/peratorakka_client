8d65cd0dc287d33536695bbdc3038facdd1bb46b
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-21 17:08:30 +0800

    fix(collection): migrate account card

--- src/components/private/collections/account_card.svelte
+++ src/components/private/collections/account_card.svelte
@@ -1,45 +1,58 @@
 <script lang="ts">
 import type { Account, AccountCollection, Currency } from "+/entity"
 
-import { createEventDispatcher } from "svelte"
-
 import { UNKNOWN_ACCOUNT } from "#/component"
 
+import convertSnakeCaseToProperCase from "$/utility/convert_snake_case_to_proper_case"
 import makeRestorableItemOptions from "$/rest/make_restorable_item_options"
 
-import CollectionItem from "$/catalog/collection_item.svelte"
+import CardItem from "$/catalog/card_item.svelte"
 import ShortParagraph from "$/typography/short_paragraph.svelte"
 
-export let data: AccountCollection
-export let accounts: Account[]
-export let currencies: Currency[]
+let {
+	data,
+	accounts,
+	currencies,
+	remove
+}: {
+	data: AccountCollection
+	accounts: Account[]
+	currencies: Currency[]
+	remove: (resource: AccountCollection) => void
+} = $props()
 
-$: account = accounts.find(account => account.id === data.account_id) ?? UNKNOWN_ACCOUNT
-// $: currency = currencies.find(currency => currency.id === account.currency_id) as Currency
-const dispatch = createEventDispatcher<{
-	"remove": AccountCollection
-}>()
+let account = $derived(accounts.find(account => account.id === data.account_id) ?? UNKNOWN_ACCOUNT)
+let associatedCurrency = $derived(currencies.find(
+	currency => currency.id === account.currency_id
+))
+let friendlyKind = $derived(convertSnakeCaseToProperCase(account.kind).toLocaleLowerCase())
 
-$: restorableItemOptions = makeRestorableItemOptions(
-	`/api/v1/account_collections/${data.id}`,
+let restorableItemOptions = $derived(makeRestorableItemOptions(
+	`/api/v2/account_collections/${data.id}`,
 	{
 		"updateCacheData": () => {},
-		"removeCacheData": () => dispatch("remove", data),
+		"removeCacheData": () => remove(data),
 		"makeUpdatedBody": () => data as unknown as Record<string, unknown>
 	}
-)
+))
 </script>
 
-<CollectionItem
+<CardItem
 	label={account.name}
 	isArchived={false}
 	options={restorableItemOptions}>
-	<ShortParagraph slot="force_delete_confirmation_message">
-		Deleting this collection may prevent related data from being shown permanently.
-	</ShortParagraph>
-	<svelte:fragment slot="resource_info">
+	{#snippet force_delete_confirmation_message()}
+		<ShortParagraph >
+			Deleting this collection may prevent related data from being shown permanently.
+		</ShortParagraph>
+	{/snippet}
+	{#snippet resource_info()}
 		<ShortParagraph>
 			{account.description}
 		</ShortParagraph>
-	</svelte:fragment>
-</CollectionItem>
+		<ShortParagraph>
+			This {friendlyKind} account uses {associatedCurrency?.code ?? "???"} as its currency. The
+			account ID is {data.id}.
+		</ShortParagraph>
+	{/snippet}
+</CardItem>
