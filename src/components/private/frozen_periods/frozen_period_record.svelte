fb3377b8799f96a56c490b0c84636386b5423c82
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-21 15:29:45 +0800

    fix(frozen period): migrate record

--- src/components/private/frozen_periods/frozen_period_record.svelte
+++ src/components/private/frozen_periods/frozen_period_record.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
-import type { GeneralError } from "+/rest"
 import type { FrozenPeriod } from "+/entity"
-
-import { createEventDispatcher } from "svelte"
-import { writable } from "svelte/store"
 
 import makeJSONRequester from "$/rest/make_json_requester"
 
@@ -15,42 +11,37 @@ import Flex from "$/layout/flex.svelte"
 import ShortParagraph from "$/typography/short_paragraph.svelte"
 import WeakenedTertiaryHeading from "$/typography/weakened_tertiary_heading.svelte"
 
-export let data: FrozenPeriod
+let {
+	data,
+	remove,
+	check
+}: {
+	data: FrozenPeriod
+	remove: (period: FrozenPeriod) => void
+	check: (period: FrozenPeriod) => void
+} = $props()
 
-let isConfirmingDeletion = false
-let isConnectingToDelete = writable<boolean>(false)
-let deleteErrors = writable<GeneralError[]>([])
-let requestDelete = () => Promise.resolve()
+let isConfirmingDeletion = $state(false)
 
-const dispatch = createEventDispatcher<{
-	"remove": FrozenPeriod
-	"check": FrozenPeriod
-}>()
 let id = `#${data.id}`
 let startedAt = data.started_at.slice(0, "YYYY-MM-DD".length)
 let finishedAt = data.finished_at.slice(0, "YYYY-MM-DD".length)
 
-$: {
-	const requesterInfo = makeJSONRequester({
-		"path": `/api/v1/frozen_periods/${data.id}/force`,
-		"defaultRequestConfiguration": {
-			"method": "DELETE",
-		},
-		"manualResponseHandlers": [
-			{
-				"statusCode": 204,
-				"action": async (_response: Response) => {
-					dispatch("remove", data)
-				}
+const { isConnecting, errors, send } = makeJSONRequester({
+	"path": `/api/v2/frozen_periods/${data.id}/force`,
+	"defaultRequestConfiguration": {
+		"method": "DELETE",
+	},
+	"manualResponseHandlers": [
+		{
+			"statusCode": 204,
+			"action": async (_response: Response) => {
+				remove(data)
 			}
-		],
-		"expectedErrorStatusCodes": [ 400 ]
-	})
-
-	isConnectingToDelete = requesterInfo.isConnecting
-	deleteErrors = requesterInfo.errors
-	requestDelete = async () => await requesterInfo.send({})
-}
+		}
+	],
+	"expectedErrorStatusCodes": [ 400 ]
+})
 
 function startReading() {
 	isConfirmingDeletion = false
@@ -61,11 +52,11 @@ function confirmDeletion() {
 }
 
 async function confirmDelete() {
-	await requestDelete()
+	await send({})
 }
 
 function checkCalculations() {
-		dispatch("check", data)
+	check(data)
 }
 </script>
 
@@ -90,34 +81,35 @@ function checkCalculations() {
 						<ShortParagraph>
 							Are you sure to delete the frozen period?
 						</ShortParagraph>
+						{#each $errors as error}
+							<ShortParagraph>
+								{error.message}
+							</ShortParagraph>
+						{/each}
 					</Flex>
 				</div>
 				<div class="card-action">
 					<TextButton
 						kind="button"
-						disabled={$isConnectingToDelete}
+						disabled={$isConnecting}
 						label="Confirm"
-						on:click={confirmDelete}/>
+						onclick={confirmDelete}/>
 					<TextButton
 						kind="button"
-						disabled={$isConnectingToDelete}
+						disabled={$isConnecting}
 						label="Cancel"
-						on:click={startReading}/>
+						onclick={startReading}/>
 				</div>
 			</article>
 		{:else}
 			<TextButton
 				kind="button"
 				label="Check"
-				on:click={checkCalculations}/>
+				onclick={checkCalculations}/>
 			<TextButton
 				kind="button"
 				label="Force Delete"
-				on:click={confirmDeletion}/>
+				onclick={confirmDeletion}/>
 		{/if}
 	</DataTableCell>
 </DataTableRow>
-
-<style lang="scss">
-@use "@/components/third-party/index";
-</style>
