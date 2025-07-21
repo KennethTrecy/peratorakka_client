f844171548487d9110da74d3b03a90c7fc977fb4
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-21 15:10:14 +0800

    fix(frozen period): migrate total amount row

--- src/components/private/frozen_periods/financial_statements/cash_flow_statement/total_amount_row.svelte
+++ src/components/private/frozen_periods/financial_statements/cash_flow_statement/total_amount_row.svelte
@@ -1,30 +1,22 @@
 <script lang="ts">
-	import type { ExchangeRateInfo } from "+/rest"
-	import type { Currency } from "+/entity"
+import DataTableCell from "$/catalog/data_table_cell.svelte"
+import DataTableHeader from "$/catalog/data_table_header.svelte"
+import DataTableRow from "$/catalog/data_table_row.svelte"
 
-	import formatAmount from "$/utility/format_amount"
-	import convertAmount from "$/utility/convert_amount"
-
-	import DataTableCell from "$/catalog/data_table_cell.svelte"
-	import DataTableHeader from "$/catalog/data_table_header.svelte"
-	import DataTableRow from "$/catalog/data_table_row.svelte"
-
-	export let rowName: string
-	export let exchangeRate: ExchangeRateInfo
-	export let currency: Currency|undefined
-	export let rawAmount: string
-	export let hasEmptyTrailingRow: boolean = false
-
-	$: convertedAmount = convertAmount(rawAmount, exchangeRate)
-	$: friendlyAmount = formatAmount(
-		currency,
-		convertedAmount
-	)
+let {
+	rowName,
+	shownAmount,
+	hasEmptyTrailingRow = false
+}: {
+	rowName: string
+	shownAmount: string
+	hasEmptyTrailingRow?: boolean
+} = $props()
 </script>
 
 <DataTableRow>
 	<DataTableHeader scope="row" columnSpan={2}>{rowName}</DataTableHeader>
-	<DataTableCell kind="numeric">{friendlyAmount}</DataTableCell>
+	<DataTableCell kind="numeric">{shownAmount}</DataTableCell>
 </DataTableRow>
 {#if hasEmptyTrailingRow}
 	<DataTableRow/>
