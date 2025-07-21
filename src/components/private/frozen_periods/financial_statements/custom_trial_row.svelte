263a7f799fdcfe485416ef47efd8b60ed0c0c6fa
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-21 15:16:52 +0800

    fix(frozen period): migrate custom trial row

--- src/components/private/frozen_periods/financial_statements/custom_trial_row.svelte
+++ src/components/private/frozen_periods/financial_statements/custom_trial_row.svelte
@@ -1,40 +1,23 @@
 <script lang="ts">
-import type { ExchangeRateInfo } from "+/rest"
-import type { Currency } from "+/entity"
-
-import { EMPTY_AMOUNT } from "#/component"
-
-import formatAmount from "$/utility/format_amount"
-import convertAmount from "$/utility/convert_amount"
-
 import DataTableCell from "$/catalog/data_table_cell.svelte"
 import DataTableHeader from "$/catalog/data_table_header.svelte"
 import DataTableRow from "$/catalog/data_table_row.svelte"
 
-export let rowName: string
-export let currency: Currency|undefined
-export let exchangeRate: ExchangeRateInfo
-export let rawDebitAmount: string
-export let rawCreditAmount: string
-export let hasEmptyTrailingRow: boolean = false
+let {
+	rowName,
+	shownDebitAmount,
+	shownCreditAmount,
+	hasEmptyTrailingRow = false
+}: {
+	rowName: string
+	shownDebitAmount: string
+	shownCreditAmount: string
+	hasEmptyTrailingRow?: boolean
+} = $props()
 
-$: emptyAmount = formatAmount(currency, EMPTY_AMOUNT)
-$: convertedDebitAmount = convertAmount(rawDebitAmount, exchangeRate)
-$: friendlyDebitAmount = formatAmount(
-	currency,
-	convertedDebitAmount
-)
-$: convertedCreditAmount = convertAmount(rawCreditAmount, exchangeRate)
-$: friendlyCreditAmount = formatAmount(
-	currency,
-	convertedCreditAmount
-)
-$: isPossiblyDebitAccount = friendlyDebitAmount !== emptyAmount
-$: isPossiblyCreditAccount = friendlyCreditAmount !== emptyAmount
-$: hasFilled = isPossiblyDebitAccount || isPossiblyCreditAccount
-
-$: shownDebitAmount = isPossiblyDebitAccount ? friendlyDebitAmount : ""
-$: shownCreditAmount = isPossiblyCreditAccount ? friendlyCreditAmount : ""
+let isPossiblyDebitAccount = $derived(shownDebitAmount !== "")
+let isPossiblyCreditAccount = $derived(shownCreditAmount !== "")
+let hasFilled = $derived(isPossiblyDebitAccount || isPossiblyCreditAccount)
 </script>
 
 {#if hasFilled}
@@ -45,7 +28,7 @@ $: shownCreditAmount = isPossiblyCreditAccount ? friendlyCreditAmount : ""
 	</DataTableRow>
 	{#if hasEmptyTrailingRow}
 		<DataTableRow>
-			<DataTableCell> </DataTableCell>
+			<DataTableCell></DataTableCell>
 		</DataTableRow>
 	{/if}
 {/if}
