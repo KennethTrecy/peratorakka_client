98aa17acea5c8170f6ad829c120e482a3643b949
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-25 22:30:27 +0800

    fix(collection): migrate card

--- src/components/private/collections/collection_card.svelte
+++ src/components/private/collections/collection_card.svelte
@@ -1,31 +1,33 @@
 <script lang="ts">
 import type { Collection } from "+/entity"
 
-import { createEventDispatcher } from "svelte"
-
 import checkArchivedState from "$/utility/check_archived_state"
 import makeRestorableItemOptions from "$/rest/make_restorable_item_options"
 
 import BasicForm from "%/collections/basic_form.svelte"
-import CollectionItem from "$/catalog/collection_item.svelte"
+import CardItem from "$/catalog/card_item.svelte"
 import EditActionCardButtonPair from "$/button/card/edit_action_pair.svelte"
 import TextCardButton from "$/button/card/text.svelte"
 import ShortParagraph from "$/typography/short_paragraph.svelte"
 
-export let data: Collection
+let {
+	data = $bindable(),
+	remove,
+	view
+}: {
+	data: Collection
+	remove: (resource: Collection) => void
+	view: (resource: Collection) => void
+} = $props()
 
-const dispatch = createEventDispatcher<{
-	"remove": Collection
-	"view": Collection
-}>()
-let name = data.name
-let description = data.description
+let name = $state(data.name)
+let description = $state(data.description)
 
-$: isArchived = checkArchivedState(data)
-$: IDPrefix = `old_collection_${data.id}`
-$: formID = `${IDPrefix}_update_form`
-$: restorableItemOptions = makeRestorableItemOptions(
-	`/api/v1/collections/${data.id}`,
+let isArchived = $derived(checkArchivedState(data))
+let IDPrefix = $derived(`old_collection_${data.id}`)
+let formID = $derived(`${IDPrefix}_update_form`)
+let restorableItemOptions = $derived(makeRestorableItemOptions(
+	`/api/v2/collections/${data.id}`,
 	{
 		"updateCacheData": () => {
 			data = {
@@ -34,7 +36,7 @@ $: restorableItemOptions = makeRestorableItemOptions(
 				name
 			}
 		},
-		"removeCacheData": () => dispatch("remove", data),
+		"removeCacheData": () => remove(data),
 		"makeUpdatedBody": () => ({
 			"collection": {
 				description,
@@ -42,7 +44,7 @@ $: restorableItemOptions = makeRestorableItemOptions(
 			}
 		})
 	}
-)
+))
 
 function resetDraft() {
 	name = data.name
@@ -50,53 +52,58 @@ function resetDraft() {
 }
 
 function viewDetail() {
-	dispatch("view", data)
+	view(data)
 }
 </script>
 
-<CollectionItem
+<CardItem
 	label={data.name}
 	{isArchived}
 	options={restorableItemOptions}
-	on:resetDraft={resetDraft}>
-	<BasicForm
-		slot="edit_form"
-		let:confirmEdit
-		let:cancelEdit
-		let:isConnecting
-		let:errors
-		id={formID}
-		bind:name={name}
-		bind:description={description}
-		{IDPrefix}
-		{isConnecting}
-		{errors}
-		on:submit={confirmEdit}>
-		<EditActionCardButtonPair
-			slot="button_group"
-			disabled={isConnecting}
-			on:cancelEdit={cancelEdit}/>
-	</BasicForm>
-	<ShortParagraph slot="delete_confirmation_message">
-		Deleting this collection may prevent related data from being shown temporarily.
-	</ShortParagraph>
-	<ShortParagraph slot="restore_confirmation_message">
-		Restoring this collection may show related data.
-	</ShortParagraph>
-	<ShortParagraph slot="force_delete_confirmation_message">
-		Deleting this collection may prevent related data from being shown permanently.
-	</ShortParagraph>
-	<svelte:fragment slot="resource_info">
+	{resetDraft}>
+	{#snippet edit_form({ confirmEdit, cancelEdit, isConnecting, errors })}
+		<BasicForm
+			id={formID}
+			bind:name={name}
+			bind:description={description}
+			{IDPrefix}
+			{isConnecting}
+			{errors}
+			onsubmit={confirmEdit}>
+			{#snippet button_group()}
+				<EditActionCardButtonPair
+					disabled={isConnecting}
+					cancelEdit={cancelEdit}/>
+			{/snippet}
+		</BasicForm>
+	{/snippet}
+	{#snippet delete_confirmation_message()}
+		<ShortParagraph >
+			Deleting this collection may prevent related data from being shown temporarily.
+		</ShortParagraph>
+	{/snippet}
+	{#snippet restore_confirmation_message()}
+		<ShortParagraph >
+			Restoring this collection may show related data.
+		</ShortParagraph>
+	{/snippet}
+	{#snippet force_delete_confirmation_message()}
+		<ShortParagraph >
+			Deleting this collection may prevent related data from being shown permanently.
+		</ShortParagraph>
+	{/snippet}
+	{#snippet resource_info()}
 		<ShortParagraph>
 			{data.description}
 		</ShortParagraph>
 		<ShortParagraph>
 			The collection ID is {data.id}.
 		</ShortParagraph>
-	</svelte:fragment>
-	<TextCardButton
-		slot="extra_buttons"
-		kind="button"
-		label="View"
-		on:click={viewDetail}/>
-</CollectionItem>
+	{/snippet}
+	{#snippet extra_buttons()}
+		<TextCardButton
+			kind="button"
+			label="View"
+			onclick={viewDetail}/>
+	{/snippet}
+</CardItem>
