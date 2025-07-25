47d5ae2a5be957a8c4b0aa40c6cc07df0e9305d6
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-25 22:23:40 +0800

    fix(component): migrate data table

--- src/components/public/catalog/data_table.svelte
+++ src/components/public/catalog/data_table.svelte
@@ -5,10 +5,37 @@ import GridCell from "$/layout/grid_cell.svelte"
 import SecondaryHeading from "$/typography/secondary_heading.svelte"
 import UnitDataTable from "$/catalog/unit_data_table.svelte"
 
-export let isConnecting: boolean
-export let progressRate: number
-export let collectiveName: string
-export let data: unknown[]
+	interface Props {
+		isConnecting: boolean
+		progressRate: number
+		collectiveName: string
+		data: unknown[];
+		filled_collection_description?: Snippet
+		empty_collection_description?: Snippet
+		list_specifier?: Snippet
+		table_footer_cells?: Snippet
+		table_headers?: Snippet
+		table_rows?: Snippet
+	}
+
+	let {
+		isConnecting,
+		progressRate,
+		collectiveName,
+		data,
+		filled_collection_description,
+		empty_collection_description,
+		list_specifier,
+		table_footer_cells,
+		table_headers,
+		table_rows
+	}: Props = $props()
+
+	const filled_collection_description_render = $derived(filled_collection_description);
+	const empty_collection_description_render = $derived(empty_collection_description);
+	const table_headers_render = $derived(table_headers);
+	const table_rows_render = $derived(table_rows);
+	const table_footer_cells_render = $derived(table_footer_cells);
 </script>
 
 <CatalogBase
@@ -16,32 +43,52 @@ export let data: unknown[]
 	{progressRate}
 	{collectiveName}
 	{data}>
-	<SecondaryHeading slot="name">Available {collectiveName}</SecondaryHeading>
-	<slot slot="filled_collection_description" name="filled_collection_description"/>
-	<slot slot="empty_collection_description" name="empty_collection_description"/>
-	<svelte:fragment slot="bare_list_specifier">
-		{#if $$slots.list_specifier}
-			<GridCell kind="full">
-				<Flex direction="row" justifyContent="responsive_stretch" mustPad={false}>
-					<slot name="list_specifier"/>
-				</Flex>
-			</GridCell>
-		{/if}
-	</svelte:fragment>
-	<GridCell slot="available_content" kind="full">
-		<Flex direction="column" mustPad={false}>
-			{#if $$slots.table_footer_cells}
-				<UnitDataTable>
-					<slot slot="table_headers" name="table_headers"/>
-					<slot slot="table_rows" name="table_rows"/>
-					<slot slot="table_footer_cells" name="table_footer_cells"/>
-				</UnitDataTable>
-			{:else}
-				<UnitDataTable>
-					<slot slot="table_headers" name="table_headers"/>
-					<slot slot="table_rows" name="table_rows"/>
-				</UnitDataTable>
+	{#snippet name()}
+		<SecondaryHeading >Available {collectiveName}</SecondaryHeading>
+	{/snippet}
+	{#snippet filled_collection_description()}
+		{@render filled_collection_description_render?.()}
+	{/snippet}
+	{#snippet empty_collection_description()}
+		{@render empty_collection_description_render?.()}
+	{/snippet}
+	{#snippet bare_list_specifier()}
+
+			{#if list_specifier}
+				<GridCell kind="full">
+					<Flex direction="row" justifyContent="responsive_stretch" mustPad={false}>
+						{@render list_specifier?.()}
+					</Flex>
+				</GridCell>
 			{/if}
-		</Flex>
-	</GridCell>
+
+	{/snippet}
+	{#snippet available_content()}
+		<GridCell  kind="full">
+			<Flex direction="column" mustPad={false}>
+				{#if table_footer_cells}
+					<UnitDataTable>
+						{#snippet table_headers()}
+										{@render table_headers_render?.()}
+									{/snippet}
+						{#snippet table_rows()}
+										{@render table_rows_render?.()}
+									{/snippet}
+						{#snippet table_footer_cells()}
+										{@render table_footer_cells_render?.()}
+									{/snippet}
+					</UnitDataTable>
+				{:else}
+					<UnitDataTable>
+						{#snippet table_headers()}
+										{@render table_headers_render?.()}
+									{/snippet}
+						{#snippet table_rows()}
+										{@render table_rows_render?.()}
+									{/snippet}
+					</UnitDataTable>
+				{/if}
+			</Flex>
+		</GridCell>
+	{/snippet}
 </CatalogBase>
