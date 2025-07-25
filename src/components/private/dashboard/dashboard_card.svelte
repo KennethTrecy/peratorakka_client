755dd1cea56f91446b53f9ba87cb6391ef41895c
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-25 23:10:03 +0800

    fix(dashboard): migrate card

--- src/components/private/dashboard/dashboard_card.svelte
+++ src/components/private/dashboard/dashboard_card.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
-import type { Currency, Formula, NumericalTool } from "+/entity"
+import type {
+	PrecisionFormat,
+	Currency,
+	Formula,
+	NumericalTool,
+	NumericalToolConfiguration
+} from "+/entity"
 import type { NumericalToolConclusion } from "+/rest"
 
+import debounce from "lodash.debounce"
+import { writable } from "svelte/store"
+
+import { UNKNOWN_CURRENCY } from "#/component"
 import {
 	NUMBER_NUMERICAL_TOOL_KIND,
 	PIE_NUMERICAL_TOOL_KIND,
 	LINE_NUMERICAL_TOOL_KIND,
-	TABLE_NUMERICAL_TOOL_KIND,
-	CURRENCY_FORMULA_OUTPUT_FORMAT,
-	RAW_FORMULA_OUTPUT_FORMAT
+	TABLE_NUMERICAL_TOOL_KIND
 } from "#/entity"
 
-import { fallbackToAcceptableFormulaOutputFormat } from "+/entity"
+import makeJSONRequester from "$/rest/make_json_requester"
+import makeDateFieldValue from "$/utility/make_date_field_value"
 
 import GridCell from "$/layout/grid_cell.svelte"
 import LineVisualizer from "%/dashboard/dashboard_card/line.svelte"
@@ -19,24 +28,101 @@ import NumberVisualizer from "%/dashboard/dashboard_card/number.svelte"
 import PieVisualizer from "%/dashboard/dashboard_card/pie.svelte"
 import TableVisualizer from "%/dashboard/dashboard_card/table.svelte"
 
-export let data: NumericalTool
-export let formulae: Formula[]
-export let currencies: Currency[]
-export let conclusion: NumericalToolConclusion|null
+let {
+	data,
+	formulae,
+	currencies,
+	precisionFormats,
+	workingIndex,
+	assignedIndex,
+	finish,
+	skip
+}: {
+	data: NumericalTool
+	formulae: Formula[]
+	currencies: Currency[]
+	precisionFormats: PrecisionFormat[]
+	workingIndex: number
+	assignedIndex: number
+	finish: () => void
+	skip: () => void
+} = $props()
 
-$: hasLoaded = conclusion !== null
-$: currency = hasLoaded ? (currencies[0] ?? null) : null
+let referenceDate = $state(makeDateFieldValue(new Date()))
 
-$: outputFormat = data.configuration.sources[0].type === "collection"
-	? CURRENCY_FORMULA_OUTPUT_FORMAT
-	: data.configuration.sources[0].type === "formula"
-		? fallbackToAcceptableFormulaOutputFormat(
-			formulae.find(
-				formula =>
-					data.configuration.sources[0].type === "formula"
-					&& formula.id === data.configuration.sources[0].formula_id
-			)?.output_format ?? RAW_FORMULA_OUTPUT_FORMAT
-		) : RAW_FORMULA_OUTPUT_FORMAT
+let IDPrefix = $derived(`old_numerical_tool_${data.id}`)
+let formID = $derived(`${IDPrefix}_time_travel_form`)
+
+let conclusion = $state<NumericalToolConclusion|null>(null)
+const completeCalculationPath = writable(`/api/v2/numerical_tools/calculate/${data.id}`)
+
+const { isConnecting, errors, send } = makeJSONRequester({
+	"path": completeCalculationPath,
+	"defaultRequestConfiguration": {
+		"method": "GET"
+	},
+	"manualResponseHandlers": [
+		{
+			"statusCode": 200,
+			"action": async (response: Response) => {
+				const responseDocument = await response.json()
+
+				conclusion = responseDocument["@meta"] as NumericalToolConclusion
+			}
+		}
+	],
+	"expectedErrorStatusCodes": [ 401 ]
+})
+
+let abortController: AbortController|null = null
+const loadConclusionsFully = debounce(() => loadConclusions(), 1000)
+async function loadConclusions() {
+	if (conclusion === null) {
+		completeCalculationPath.set(
+			`/api/v2/numerical_tools/calculate/${data.id}?reference_date=${referenceDate}`
+		)
+	} else {
+		return
+	}
+
+	if (abortController !== null) abortController.abort()
+
+	abortController = new AbortController()
+
+	await send({
+		"signal": abortController.signal
+	})
+
+	if (conclusion === null) {
+		skip()
+	} else {
+		finish()
+	}
+}
+
+$effect(() => {
+	if (workingIndex === assignedIndex) {
+		loadConclusionsFully()
+	}
+})
+
+let configuration = $derived(JSON.parse(data.configuration) as NumericalToolConfiguration)
+let associatedCurrency = $derived(currencies.find(
+	currency => currency.id === data.currency_id
+) ?? UNKNOWN_CURRENCY)
+let associatedCurrencyPrecisionFormat = $derived(precisionFormats.find(
+	precisionFormat => precisionFormat.id === associatedCurrency.precision_format_id
+) ?? null)
+
+async function view(newReferenceDate: string): Promise<void> {
+	referenceDate = newReferenceDate
+	await refresh()
+}
+
+async function refresh() {
+	conclusion = null
+	await loadConclusions()
+}
 </script>
 
 {#if conclusion === null}
@@ -46,27 +132,67 @@ $: outputFormat = data.configuration.sources[0].type === "collection"
 {:else}
 	{#if data.kind === NUMBER_NUMERICAL_TOOL_KIND}
 		<NumberVisualizer
-			numericalTool={data}
-			{currency}
-			{outputFormat}
-			{conclusion}/>
+			currencyPrecisionFormat={associatedCurrencyPrecisionFormat}
+			currency={associatedCurrency}
+			{formulae}
+			{precisionFormats}
+			name={data.name}
+			{configuration}
+			{referenceDate}
+			{conclusion}
+			{IDPrefix}
+			{formID}
+			{isConnecting}
+			{errors}
+			{view}
+			{refresh}/>
 	{:else if data.kind === PIE_NUMERICAL_TOOL_KIND}
 		<PieVisualizer
-			numericalTool={data}
-			{currency}
-			{outputFormat}
-			{conclusion}/>
+			currencyPrecisionFormat={associatedCurrencyPrecisionFormat}
+			currency={associatedCurrency}
+			{formulae}
+			{precisionFormats}
+			name={data.name}
+			{configuration}
+			{referenceDate}
+			{conclusion}
+			{IDPrefix}
+			{formID}
+			{isConnecting}
+			{errors}
+			{view}
+			{refresh}/>
 	{:else if data.kind === LINE_NUMERICAL_TOOL_KIND}
 		<LineVisualizer
-			numericalTool={data}
-			{currency}
-			{outputFormat}
-			{conclusion}/>
+			currencyPrecisionFormat={associatedCurrencyPrecisionFormat}
+			currency={associatedCurrency}
+			{formulae}
+			{precisionFormats}
+			name={data.name}
+			{configuration}
+			{referenceDate}
+			{conclusion}
+			{IDPrefix}
+			{formID}
+			{isConnecting}
+			{errors}
+			{view}
+			{refresh}/>
 	{:else if data.kind === TABLE_NUMERICAL_TOOL_KIND}
 		<TableVisualizer
-			numericalTool={data}
-			{currency}
-			{outputFormat}
-			{conclusion}/>
+			currencyPrecisionFormat={associatedCurrencyPrecisionFormat}
+			currency={associatedCurrency}
+			{formulae}
+			{precisionFormats}
+			name={data.name}
+			{configuration}
+			{referenceDate}
+			{conclusion}
+			{IDPrefix}
+			{formID}
+			{isConnecting}
+			{errors}
+			{view}
+			{refresh}/>
 	{/if}
 {/if}
