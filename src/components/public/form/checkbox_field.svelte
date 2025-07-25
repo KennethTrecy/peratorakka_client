9551f68e05ad2e144a961a0a70f7113c4a6843a0
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-25 22:38:12 +0800

    fix(component): migrate checkbox field

--- src/components/public/form/checkbox_field.svelte
+++ src/components/public/form/checkbox_field.svelte
@@ -3,12 +3,21 @@ import type { GeneralError } from "+/rest"
 
 import GeneralContent from "$/form/general_content.svelte"
 
-export let fieldName: string
-export let disabled: boolean
-export let value: boolean
-export let errorFieldID = ""
-export let IDPrefix: string = ""
-export let errors: GeneralError[]
+let {
+	fieldName,
+	disabled,
+	value = $bindable(),
+	errorFieldID = "",
+	IDPrefix = "",
+	errors
+}: {
+	fieldName: string
+	disabled: boolean
+	value: boolean
+	errorFieldID?: string
+	IDPrefix?: string
+	errors: GeneralError[]
+} = $props()
 </script>
 
 <div>
@@ -16,28 +25,25 @@ export let errors: GeneralError[]
 		{fieldName}
 		{errorFieldID}
 		{IDPrefix}
-		{errors}
-		let:fieldID
-		let:labelID
-		let:helperID>
-		<label for={fieldID} id={labelID}>
-			<input
-				id={fieldID}
-				type="checkbox"
-				class="filled-in"
-				bind:checked={value}
-				disabled={disabled}
-				aria-labelledby={labelID}
-				aria-controls={helperID}
-				aria-describedby={helperID}/>
-			<span>{fieldName}</span>
-		</label>
+		{errors}>
+		{#snippet children({ fieldID, labelID, helperID })}
+			<label for={fieldID} id={labelID}>
+				<input
+					id={fieldID}
+					type="checkbox"
+					class="filled-in"
+					bind:checked={value}
+					disabled={disabled}
+					aria-labelledby={labelID}
+					aria-controls={helperID}
+					aria-describedby={helperID}/>
+				<span>{fieldName}</span>
+			</label>
+		{/snippet}
 	</GeneralContent>
 </div>
 
 <style lang="scss">
-@use "@/components/third-party/index";
-
 input[type="checkbox"].filled-in:checked + span:not(.lever)::before {
 	--md-sys-color-on-primary: var(--md-ref-palette-neutral80);
 }
