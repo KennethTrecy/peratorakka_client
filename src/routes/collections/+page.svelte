c99e27efb876debafc0732df6be02561b6f69d06
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-25 22:34:34 +0800

    fix(collection): migrate page

--- src/routes/collections/+page.svelte
+++ src/routes/collections/+page.svelte
@@ -1,28 +1,21 @@
 <script lang="ts">
 import type { ContextBundle } from "+/component"
 import type { Account, AccountCollection, Currency, Collection } from "+/entity"
-import type { GeneralError, SearchMode, SortOrder } from "+/rest"
 
-import { get, writable, derived } from "svelte/store"
-import { onMount, getContext } from "svelte"
+import { getContext } from "svelte"
 import { afterNavigate, beforeNavigate, goto } from "$app/navigation"
 
 import { GLOBAL_CONTEXT } from "#/contexts"
-import { SEARCH_NORMALLY, ASCENDING_ORDER, MAXIMUM_PAGINATED_LIST_LENGTH } from "#/rest"
 
 import assertAuthentication from "$/page_requirement/assert_authentication"
-import loadAllDependencies from "$/rest/load_all_dependencies"
-import makeJSONRequester from "$/rest/make_json_requester"
 
-import AddForm from "%/collections/add_form.svelte"
 import AddLinkForm from "%/collections/add_link_form.svelte"
-import ArticleGrid from "$/layout/article_grid.svelte"
-import CollectionCollection from "%/collections/collection.svelte"
-import ExtraResourceLoader from "$/catalog/extra_resource_loader.svelte"
-import GridCell from "$/layout/grid_cell.svelte"
-import InnerGrid from "$/layout/inner_grid.svelte"
 import LinkCollection from "%/collections/link_collection.svelte"
-import PrimaryHeading from "$/typography/primary_heading.svelte"
+import BasicForm from "%/collections/basic_form.svelte"
+import CompleteResourcePage from "$/layout/complete_resource_page.svelte"
+import ElementalParagraph from "$/typography/elemental_paragraph.svelte"
+import Card from "%/collections/collection_card.svelte"
+import TextContainer from "$/typography/text_container.svelte"
 
 const globalContext = getContext(GLOBAL_CONTEXT) as ContextBundle
 
@@ -32,220 +25,167 @@ assertAuthentication(globalContext, {
 	goto
 })
 
-let isRequestingDependencies = true
+let selectedCollection = $state<Collection|null>(null)
+let currencies = $state<Currency[]>([])
+let accounts = $state<Account[]>([])
+let accountCollections = $state<AccountCollection[]>([])
 
-let selectedCollection: Collection|null = null
-let collections: Collection[] = []
-let currencies: Currency[] = []
-let accounts: Account[] = []
-let accountCollections: AccountCollection[] = []
-
-let searchMode: SearchMode = SEARCH_NORMALLY
-let sortCriterion: string = "name"
-let sortOrder: SortOrder = ASCENDING_ORDER
-let lastOffset: number = 0
-let progressRate = 0
-
-const collectiveName = "collections"
-const partialPath = `/api/v1/${collectiveName}`
-let parameters: [string, string][] = [
-	[ "filter[search_mode]", searchMode as string ],
-	[ "sort[0][0]", sortCriterion ],
-	[ "sort[0][1]", sortOrder as string ]
-]
-let completePath = writable(partialPath)
-$: {
-	parameters = [
-		[ "filter[search_mode]", searchMode as string ],
-		[ "sort[0][0]", sortCriterion ],
-		[ "sort[0][1]", sortOrder as string ],
-	]
-
-	completePath.set(`${partialPath}?${
-		new URLSearchParams([
-			...parameters,
-			[ "page[offset]", `${lastOffset}` ],
-			[ "page[limit]", MAXIMUM_PAGINATED_LIST_LENGTH ]
-		]).toString()
-	}`)
+function deriveID(resource: unknown): string {
+	return `${(resource as Collection).id}`
 }
 
-const dependencyErrors = writable([] as GeneralError[])
+let name = $state("")
+let description = $state("")
 
-let { isConnecting, errors, send } = makeJSONRequester({
-	"path": completePath,
-	"defaultRequestConfiguration": {
-		"method": "GET"
-	},
-	"manualResponseHandlers": [
-		{
-			"statusCode": 200,
-			"action": async (response: Response) => {
-				let responseDocument = await response.json()
-				errors.set([])
-				collections = responseDocument[collectiveName]
-				lastOffset = Math.max(0, responseDocument[collectiveName].length - 1)
-			}
+function makeNewResourceObject(): Record<string, unknown> {
+	return {
+		"collection": {
+			name,
+			description
 		}
-	],
-	"expectedErrorStatusCodes": [ 401 ]
-})
-
-const allErrors = derived([ dependencyErrors, errors ], ([ dependencyErrors, errors ]) => [
-	...dependencyErrors,
-	...errors
-])
-
-async function reloadCollections() {
-	collections = []
-	await send({})
-}
-
-async function loadList() {
-	const currentServerURL = get(globalContext.serverURL as any)
-
-	if (currentServerURL === "") {
-		setTimeout(loadList, 1000)
-		return
 	}
-
-	isRequestingDependencies = true
-
-	await loadAllDependencies<Account|AccountCollection>(globalContext, [
-		{
-			"partialPath": "/api/v1/account_collections",
-			"mainSortCriterion": "id",
-			"resourceKey": "account_collections",
-			"getResources": () => accountCollections,
-			"setResources": (
-				(newResources: AccountCollection[]) => { accountCollections = newResources }
-			) as (newResources: (Account|AccountCollection)[]) => void
-		},
-		{
-			"partialPath": "/api/v1/accounts",
-			"mainSortCriterion": "name",
-			"resourceKey": "accounts",
-			"getResources": () => accounts,
-			"setResources": (
-				(newResources: Account[]) => { accounts = newResources }
-			) as (newResources: (Account|AccountCollection)[]) => void,
-			"getLinkedResources": () => [
-				{
-					"resourceKey": "currencies",
-					"resources": currencies
-				}
-			],
-			"setLinkedResources": newResources => {
-				currencies = newResources[0] as Currency[]
-			}
-		}
-	], {
-		"updateProgressRate": newProgressRate => { progressRate = newProgressRate },
-		"updateErrors": newErrors => { dependencyErrors.set(newErrors) }
-	})
-
-	isRequestingDependencies = false
-	await reloadCollections()
 }
 
-onMount(loadList)
+function processCreatedResourceObject(document: Record<string, unknown>): unknown {
+	name = ""
+	description = ""
 
-function addCollection(event: CustomEvent<Collection>) {
-	if (searchMode === "only_deleted") return
-
-	const newCollection = event.detail
-	collections = [
-		newCollection,
-		...collections
-	]
+	const { collection } = document
+	return collection
 }
 
-function viewCollection(event: CustomEvent<Collection>) {
-	selectedCollection = event.detail
-}
-
-function addCollections(event: CustomEvent<unknown[]>) {
-	const newCollections = event.detail as Collection[]
-	collections = [
-		...collections,
-		...newCollections
-	]
-}
-
-function removeCollection(event: CustomEvent<Collection>) {
-	const oldCollection = event.detail
-	collections = collections.filter(collection => collection.id !== oldCollection.id)
-}
-
-function addAccountCollection(event: CustomEvent<AccountCollection>) {
-	const newAccountCollection = event.detail
+function addAccountCollection(newAccountCollection: AccountCollection) {
 	accountCollections = [
 		newAccountCollection,
 		...accountCollections
 	]
 }
 
-function removeAccountCollection(event: CustomEvent<AccountCollection>) {
-	const oldAccountCollection = event.detail
+function removeAccountCollection(oldAccountCollection: AccountCollection) {
 	accountCollections = accountCollections.filter(
 		accountCollection => accountCollection.id !== oldAccountCollection.id
 	)
 }
 
-$: linkedAccountCollections = accountCollections.filter(
+let linkedAccountCollections = $derived(accountCollections.filter(
 	accountCollection => selectedCollection !== null
 		&& accountCollection.collection_id === selectedCollection.id
-)
-$: linkedAccountCollectionIDs = linkedAccountCollections.map(
+))
+let linkedAccountCollectionIDs = $derived(linkedAccountCollections.map(
 	accountCollection => accountCollection.account_id
-)
-$: linkedAccounts = accounts.filter(account => linkedAccountCollectionIDs.includes(account.id))
+))
+let linkedAccounts = $derived(accounts.filter(
+	account => linkedAccountCollectionIDs.includes(account.id)
+))
+
+$inspect(linkedAccountCollections, selectedCollection, accountCollections)
+
+function viewCollection(collection: Collection) {
+	selectedCollection = collection
+}
 </script>
 
-<svelte:head>
-	<title>Collections</title>
-</svelte:head>
-
-<ArticleGrid>
-	<InnerGrid>
-		<GridCell kind="full">
-			<PrimaryHeading>Collections</PrimaryHeading>
-		</GridCell>
-		<AddForm on:create={addCollection}/>
-		<CollectionCollection
-			data={collections}
-			bind:searchMode={searchMode}
-			bind:sortCriterion={sortCriterion}
-			bind:sortOrder={sortOrder}
-			isConnecting={$isConnecting || isRequestingDependencies}
-			{progressRate}
-			listErrors={$allErrors}
-			on:remove={removeCollection}
-			on:view={viewCollection}/>
-		<ExtraResourceLoader
-			isConnectingForInitialList={$isConnecting || isRequestingDependencies}
-			{partialPath}
-			{parameters}
-			{collectiveName}
-			bind:lastOffset={lastOffset}
-			on:reloadFully={reloadCollections}
-			on:addResources={addCollections}/>
+<CompleteResourcePage
+	pageTitle="Collections"
+	createTitle="Add Collection"
+	listTitle="Collections"
+	collectiveName="collections"
+	defaultSortCriterion="name"
+	availableSortCriteria={[
+		"name",
+		"created_at",
+		"updated_at",
+		"deleted_at"
+	]}
+	dependencies={[ accounts ]}
+	dependencyInfos={[
+		{
+			"partialPath": "/api/v2/account_collections",
+			"mainSortCriterion": "id",
+			"resourceKey": "account_collections",
+			"getResources": () => accountCollections,
+			"setResources": newResources => {
+				accountCollections = newResources as AccountCollection[]
+			}
+		},
+		{
+			"partialPath": "/api/v2/accounts",
+			"mainSortCriterion": "name",
+			"resourceKey": "accounts",
+			"getResources": () => accounts,
+			"setResources": newResources => {
+				accounts = newResources as Account[]
+			}
+		},
+		{
+			"partialPath": "/api/v2/currencies",
+			"mainSortCriterion": "name",
+			"resourceKey": "currencies",
+			"getResources": () => currencies,
+			"setResources": newResources => { currencies = newResources as Currency[] }
+		}
+	]}
+	{deriveID}
+	{makeNewResourceObject}
+	{processCreatedResourceObject}>
+	{#snippet general_description()}
+		<TextContainer>
+			<ElementalParagraph>
+				Collections are used to group multiple financial accounts. They help in calculating a
+				formula or presenting data.
+			</ElementalParagraph>
+		</TextContainer>
+	{/snippet}
+	{#snippet form({ IDPrefix, isConnecting, errors, onsubmit, button_group })}
+		<BasicForm
+			bind:name={name}
+			bind:description={description}
+			{isConnecting}
+			{IDPrefix}
+			{errors}
+			{onsubmit}
+			{button_group}/>
+	{/snippet}
+	{#snippet requirement()}
+		<ElementalParagraph>
+			At least one account must exist in the profile to show the form for
+			creating collections.
+		</ElementalParagraph>
+	{/snippet}
+	{#snippet filled_collection_description()}
+		Below are the collections that you have added on to your profile.
+		They may have several financial accounts.
+	{/snippet}
+	{#snippet empty_collection_description({ isPresent, isPresentAndArchived, isArchived })}
+		There are no available collection at the moment.
+		{#if isPresent}Create{/if}{#if isPresentAndArchived}/{/if}{#if isArchived}Delete{/if}
+		a collection to view.
+	{/snippet}
+	{#snippet collection_items({ resources, updateResource, removeResource })}
+		{#each resources as entity, i((entity as Collection).id)}
+			<Card
+				bind:data={
+					() => entity as Collection,
+					updatedResource => updateResource(updatedResource, i)
+				}
+				remove={removeResource}
+				view={viewCollection}/>
+		{/each}
+	{/snippet}
+	{#snippet list_grid_cell_rear()}
 		{#if selectedCollection !== null}
 			<AddLinkForm
 				collection={selectedCollection}
 				{currencies}
 				{accounts}
 				{linkedAccounts}
-				isLoadingInitialData={isRequestingDependencies}
-				on:create={addAccountCollection}/>
+				create={addAccountCollection}/>
 			<LinkCollection
 				{selectedCollection}
 				data={linkedAccountCollections}
 				{currencies}
 				{accounts}
-				isConnecting={isRequestingDependencies}
-				{progressRate}
-				on:remove={removeAccountCollection}/>
+				remove={removeAccountCollection}/>
 		{/if}
-	</InnerGrid>
-</ArticleGrid>
+	{/snippet}
+</CompleteResourcePage>
