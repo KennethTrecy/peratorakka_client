8c4efe07268f2d1ac3f4c563b32310d6f3a32aa7
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-25 23:11:04 +0800

    fix(component): require precision format for star formatter

--- src/components/public/utility/format_star.ts
+++ src/components/public/utility/format_star.ts
@@ -1,4 +1,4 @@
-import type { Currency, AcceptableFormulaOutputFormat } from "+/entity"
+import type { PrecisionFormat, Currency, AcceptableFormulaOutputFormat } from "+/entity"
 import type { Star } from "+/rest"
 
 import {
@@ -6,24 +6,30 @@ import {
 	PERCENTAGE_FORMULA_OUTPUT_FORMAT
 } from "#/entity"
 
-import { makeFormattedAmount } from "!/index"
+import { makeFormattedAmount, makeRawFormattedAmount } from "!/index"
 import cleanValue from "$/utility/clean_value"
-import parenthesizeValue from "$/utility/parenthesize_value"
+import dashValue from "$/utility/dash_value"
 
 export default function formatStar(
 	outputFormat: AcceptableFormulaOutputFormat,
+	associatedPrecisionFormat: PrecisionFormat | null,
 	currency: Currency|null,
 	star: Star
 ): string {
 	const [ isNegative, cleanAmount ] = cleanValue(star.display_value)
-	const formattedAmount = parenthesizeValue(
-		isNegative,
-		outputFormat === CURRENCY_FORMULA_OUTPUT_FORMAT
-			? makeFormattedAmount(currency ?? undefined, cleanAmount)
-			: outputFormat === PERCENTAGE_FORMULA_OUTPUT_FORMAT
-				? `${cleanAmount}%`
-				: cleanAmount
-	)
 
-	return formattedAmount
+	if (outputFormat === CURRENCY_FORMULA_OUTPUT_FORMAT) {
+		return dashValue(isNegative, makeFormattedAmount(
+			associatedPrecisionFormat ?? undefined,
+			currency ?? undefined,
+			cleanAmount
+		))
+	} else if (outputFormat === PERCENTAGE_FORMULA_OUTPUT_FORMAT) {
+		return dashValue(isNegative, `${makeRawFormattedAmount(
+			associatedPrecisionFormat ?? undefined,
+			cleanAmount
+		)}%`)
+	}
+
+	return dashValue(isNegative, cleanAmount)
 }
