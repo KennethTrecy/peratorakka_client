9e61587711f75f8b6d2e3602588a872f20ce5131
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-25 22:46:17 +0800

    fix(formula): migrate page

--- src/routes/formulae/+page.svelte
+++ src/routes/formulae/+page.svelte
@@ -1,30 +1,25 @@
 <script lang="ts">
-import type { Formula, Currency } from "+/entity"
+import type {
+	PrecisionFormat,
+	Formula,
+	AcceptableFormulaOutputFormat
+} from "+/entity"
 import type { ContextBundle } from "+/component"
-import type { GeneralError, SearchMode, SortOrder } from "+/rest"
 
-import { get, writable, derived } from "svelte/store"
-import { onMount, getContext } from "svelte"
+import { getContext } from "svelte"
 import { afterNavigate, beforeNavigate, goto } from "$app/navigation"
 
 import { GLOBAL_CONTEXT } from "#/contexts"
-import {
-	SEARCH_NORMALLY,
-	ASCENDING_ORDER,
-	MAXIMUM_PAGINATED_LIST_LENGTH
-} from "#/rest"
+import { UNKNOWN_OPTION } from "#/component"
+import { acceptableFormulaOutputFormats } from "#/entity"
 
 import assertAuthentication from "$/page_requirement/assert_authentication"
-import loadAllDependencies from "$/rest/load_all_dependencies"
-import makeJSONRequester from "$/rest/make_json_requester"
 
-import AddForm from "%/formulae/add_form.svelte"
-import ArticleGrid from "$/layout/article_grid.svelte"
-import Collection from "%/formulae/collection.svelte"
-import ExtraResourceLoader from "$/catalog/extra_resource_loader.svelte"
-import GridCell from "$/layout/grid_cell.svelte"
-import InnerGrid from "$/layout/inner_grid.svelte"
-import PrimaryHeading from "$/typography/primary_heading.svelte"
+import BasicForm from "%/formulae/basic_form.svelte"
+import CompleteResourcePage from "$/layout/complete_resource_page.svelte"
+import ElementalParagraph from "$/typography/elemental_paragraph.svelte"
+import Card from "%/formulae/formula_card.svelte"
+import TextContainer from "$/typography/text_container.svelte"
 
 const globalContext = getContext(GLOBAL_CONTEXT) as ContextBundle
 
@@ -34,158 +29,126 @@ assertAuthentication(globalContext, {
 	goto
 })
 
-let isRequestingDependencies = true
+let precisionFormats: PrecisionFormat[] = $state([])
 
-let currencies: Currency[] = []
-let formulae: Formula[] = []
-
-let searchMode: SearchMode = SEARCH_NORMALLY
-let sortCriterion: string = "name"
-let sortOrder: SortOrder = ASCENDING_ORDER
-let lastOffset: number = 0
-let progressRate = 0
-
-const collectiveName = "formulae"
-const partialPath = `/api/v1/${collectiveName}`
-let parameters: [string, string][] = [
-	[ "filter[search_mode]", searchMode as string ],
-	[ "sort[0][0]", sortCriterion ],
-	[ "sort[0][1]", sortOrder as string ]
-]
-const completePath = writable(partialPath)
-$: {
-	parameters = [
-		[ "filter[search_mode]", searchMode as string ],
-		[ "sort[0][0]", sortCriterion ],
-		[ "sort[0][1]", sortOrder as string ]
-	]
-
-	completePath.set(`${partialPath}?${
-		new URLSearchParams([
-			...parameters,
-			[ "page[offset]", `${lastOffset}` ],
-			[ "page[limit]", MAXIMUM_PAGINATED_LIST_LENGTH ]
-		]).toString()
-	}`)
+function deriveID(resource: unknown): string {
+	return `${(resource as Formula).id}`
 }
 
-const dependencyErrors = writable([] as GeneralError[])
+let precisionFormatID = $state(UNKNOWN_OPTION)
+let name = $state<string>("")
+let description = $state<string>("")
+let outputFormat = $state<AcceptableFormulaOutputFormat>(acceptableFormulaOutputFormats[0])
+let expression = $state<string>("")
 
-let {
-	isConnecting,
-	errors,
-	send
-} = makeJSONRequester({
-	"path": completePath,
-	"defaultRequestConfiguration": {
-		"method": "GET"
-	},
-	"manualResponseHandlers": [
-		{
-			"statusCode": 200,
-			"action": async (response: Response) => {
-				let responseDocument = await response.json()
-				formulae = responseDocument[collectiveName]
-				lastOffset = Math.max(0, responseDocument[collectiveName].length - 1)
-			}
+function makeNewResourceObject(): Record<string, unknown> {
+	return {
+		"formula": {
+			"precision_format_id": parseInt(precisionFormatID),
+			name,
+			description,
+			"output_format": outputFormat,
+			expression
 		}
-	],
-	"expectedErrorStatusCodes": [ 401 ]
-})
-
-const allErrors = derived([ dependencyErrors, errors ], ([ dependencyErrors, errors ]) => [
-	...dependencyErrors,
-	...errors
-])
-
-async function reloadFormulae() {
-	formulae = []
-	await send({})
-}
-
-async function loadList() {
-	const currentServerURL = get(globalContext.serverURL as any)
-
-	if (currentServerURL === "") {
-		setTimeout(loadList, 1000)
-		return
 	}
-
-	isRequestingDependencies = true
-
-	await loadAllDependencies(globalContext, [
-		{
-			"partialPath": "/api/v1/currencies",
-			"mainSortCriterion": "name",
-			"resourceKey": "currencies",
-			"getResources": () => currencies,
-			"setResources": newResources => { currencies = newResources }
-		}
-	], {
-		"updateProgressRate": newProgressRate => { progressRate = newProgressRate },
-		"updateErrors": newErrors => { dependencyErrors.set(newErrors) }
-	})
-
-	isRequestingDependencies = false
-	await reloadFormulae()
 }
 
-onMount(loadList)
+function processCreatedResourceObject(document: Record<string, unknown>): unknown {
+	precisionFormatID = `${precisionFormats[0].id}`
+	name = ""
+	description = ""
+	expression = ""
 
-function addFormula(event: CustomEvent<Formula>) {
-	if (searchMode === "only_deleted") return
-
-	const newFormula = event.detail
-	formulae = [
-		newFormula,
-		...formulae
-	]
+	const { formula } = document
+	return formula
 }
 
-function addFormulae(event: CustomEvent<unknown[]>) {
-	const newFormulae = event.detail as Formula[]
-	formulae = [
-		...formulae,
-		...newFormulae
-	]
-}
-
-function removeFormula(event: CustomEvent<Formula>) {
-	const oldFormula = event.detail
-	formulae = formulae.filter(account => account.id !== oldFormula.id)
-}
-
+let existingPrecisionFormats = $derived(precisionFormats.filter(
+	precisionFormat => precisionFormat.deleted_at === null
+))
 </script>
 
-<svelte:head>
-	<title>Formulae</title>
-</svelte:head>
-
-<ArticleGrid>
-	<InnerGrid>
-		<GridCell kind="full">
-			<PrimaryHeading>Formulae</PrimaryHeading>
-		</GridCell>
-		<AddForm
-			{currencies}
-			isLoadingInitialData={isRequestingDependencies}
-			on:create={addFormula}/>
-		<Collection
-			{currencies}
-			data={formulae}
-			bind:searchMode={searchMode}
-			bind:sortCriterion={sortCriterion}
-			bind:sortOrder={sortOrder}
-			isConnecting={$isConnecting || isRequestingDependencies}
-			listErrors={$allErrors}
-			on:remove={removeFormula}/>
-		<ExtraResourceLoader
-			isConnectingForInitialList={$isConnecting || isRequestingDependencies}
-			{partialPath}
-			{parameters}
-			{collectiveName}
-			bind:lastOffset={lastOffset}
-			on:reloadFully={reloadFormulae}
-			on:addResources={addFormulae}/>
-	</InnerGrid>
-</ArticleGrid>
+<CompleteResourcePage
+	pageTitle="Formulae"
+	createTitle="Add Formula"
+	listTitle="Available Formulae"
+	collectiveName="formulae"
+	defaultSortCriterion="name"
+	availableSortCriteria={[
+		"name",
+		"created_at",
+		"updated_at",
+		"deleted_at"
+	]}
+	dependencies={[ existingPrecisionFormats ]}
+	dependencyInfos={[
+		{
+			"partialPath": "/api/v2/precision_formats",
+			"mainSortCriterion": "name",
+			"resourceKey": "precision_formats",
+			"getResources": () => precisionFormats,
+			"setResources": newResources => { precisionFormats = newResources as PrecisionFormat[] }
+		}
+	]}
+	{deriveID}
+	{makeNewResourceObject}
+	{processCreatedResourceObject}>
+	{#snippet general_description()}
+		<TextContainer>
+			<ElementalParagraph>
+				Formulas (or formulae) summarize multiple data into meaningful information that a person
+				may use. This may be inflation rate of expenses, total annual income, or total amount of
+				loans need to be paid.
+			</ElementalParagraph>
+			<ElementalParagraph>
+				To create a formula to be used in the system, choose a base currency which will be used
+				as the basis for calculating amounts from multiple currencies. An output format
+				indicates the type of result after calculation.
+			</ElementalParagraph>
+			<ElementalParagraph>
+				Exchange rate basis may be periodic (which is the latest exchange rate on a paricular
+				period) or latest (which is the latest overall exchange rate regardless of the time). It
+				also has presentation precision which is used when displayed by a numerical tool.
+			</ElementalParagraph>
+		</TextContainer>
+	{/snippet}
+	{#snippet form({ IDPrefix, isConnecting, errors, onsubmit, button_group })}
+		<BasicForm
+			precisionFormats={existingPrecisionFormats}
+			bind:precisionFormatID={precisionFormatID}
+			bind:name={name}
+			bind:description={description}
+			bind:outputFormat={outputFormat}
+			bind:expression={expression}
+			{isConnecting}
+			{IDPrefix}
+			{errors}
+			{onsubmit}
+			{button_group}/>
+	{/snippet}
+	{#snippet requirement()}
+		<ElementalParagraph>
+			At least one currency must exist in the profile to show the form for creating formulae.
+		</ElementalParagraph>
+	{/snippet}
+	{#snippet filled_collection_description()}
+		Below are the formulae that you have added on to your profile.
+		They can be associated to numerical tools in order to display their results.
+	{/snippet}
+	{#snippet empty_collection_description({ isPresent, isPresentAndArchived, isArchived })}
+		There are no available formulae at the moment.
+		{#if isPresent}Create{/if}{#if isPresentAndArchived}/{/if}{#if isArchived}Delete{/if} a
+		formula to view.
+	{/snippet}
+	{#snippet collection_items({ resources, updateResource, removeResource })}
+		{#each resources as entity, i((entity as Formula).id)}
+			<Card
+				{precisionFormats}
+				bind:data={
+					() => entity as Formula,
+					updatedResource => updateResource(updatedResource, i)
+				}
+				remove={removeResource}/>
+		{/each}
+	{/snippet}
+</CompleteResourcePage>
