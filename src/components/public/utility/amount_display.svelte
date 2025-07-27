60438569de59b9ecc2ea8a0d515e84ed6de64ab8
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-07-27 12:44:30 +0800

    internal(component): make amount display

---
+++ src/components/public/utility/amount_display.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import separateNumericParts from "$/utility/separate_numeric_parts"
+
+let { shownAmount }: {
+	shownAmount: string
+} = $props()
+
+let parts = $derived(separateNumericParts(shownAmount))
+let wholePart = $derived(parts[0])
+let visibleDecimalPart = $derived(parts[1])
+let invisbleDecimalPart = $derived(parts[2])
+let postDecimalPart = $derived(parts[3])
+</script>
+
+{wholePart}{visibleDecimalPart}{#if invisbleDecimalPart.length > 0}<span>{invisbleDecimalPart}</span>{/if}{postDecimalPart}
+
+<style lang="scss">
+span {
+	color: transparent;
+}
+</style>
