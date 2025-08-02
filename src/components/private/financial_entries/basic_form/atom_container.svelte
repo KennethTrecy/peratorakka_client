b3739f0382b3847c8c7135a7d894888b0a93f33f
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-08-02 08:28:29 +0800

    fix(financial entry): track parent value properly

--- src/components/private/financial_entries/basic_form/atom_container.svelte
+++ src/components/private/financial_entries/basic_form/atom_container.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 import type { GeneralError } from "+/rest"
-import type { ChoiceInfo } from "+/component"
-import type { Account, CompleteFinancialEntryAtomInput } from "+/entity"
+import type { CompleteFinancialEntryAtomInput } from "+/entity"
 
 import { REAL_DEBIT_MODIFIER_ATOM_KIND } from "#/entity"
+
+import { untrack } from "svelte"
 
 import TextField from "$/form/text_field.svelte"
 import GeneralFieldContainer from "$/form/general_field_container.svelte"
@@ -27,6 +28,30 @@ let label = $derived(`${
 		? "Debit Amount"
 		: "Credit Amount"
 } for ${atom.account.name} (${atom.currency.code})`)
+
+let value = $state(atom.input.numerical_value)
+
+$effect(() => {
+	if (untrack(() => atom.input.numerical_value) !== value) {
+		untrack(() => {
+			atom = {
+				...atom,
+				"input": {
+					...atom.input,
+					"numerical_value": value
+				}
+			}
+		})
+	}
+})
+
+$effect(() => {
+	if (atom.input.numerical_value !== untrack(() => value)) {
+		untrack(() => {
+			value = atom.input.numerical_value
+		})
+	}
+})
 </script>
 
 <GeneralFieldContainer tag="fieldset">
@@ -34,15 +59,7 @@ let label = $derived(`${
 		fieldName={label}
 		errorFieldID="financial_entry_atoms"
 		disabled={isConnecting || disabled}
-		bind:value={
-			() => atom.input.numerical_value,
-			value => atom = {
-				...atom,
-				"input": {
-					...atom.input,
-					"numerical_value": value
-				}
-			}}
+		bind:value={value}
 		{IDPrefix}
 		{errors}/>
 </GeneralFieldContainer>
