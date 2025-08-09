fa27fec2c1a151d3c765450e6c34bb9a7741244a
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-08-09 12:07:33 +0800

    feat(account): add field for item configuration

--- src/components/private/accounts/basic_form.svelte
+++ src/components/private/accounts/basic_form.svelte
@@ -1,26 +1,45 @@
 <script lang="ts">
 import type { Snippet } from "svelte"
 import type { GeneralError } from "+/rest"
-import type { Currency, AcceptableAccountKind, Account } from "+/entity"
+import type {
+	Currency,
+	AcceptableAccountKind,
+	Account,
+	ItemConfigurationInput,
+	ItemConfiguration,
+	ItemDetail
+} from "+/entity"
 
-import { acceptableAccountKinds, ACCOUNT_KIND_DESCRIPTIONS } from "#/entity"
+import { untrack } from "svelte"
+
+import {
+	acceptableAccountKinds,
+	acceptableValuationMethods,
+	ACCOUNT_KIND_DESCRIPTIONS,
+	ITEMIZED_ASSET_ACCOUNT_KIND
+} from "#/entity"
 
 import transformCurrency from "$/form/choice_info_transformer/transform_currency"
 import transformString from "$/form/choice_info_transformer/transform_string"
+import transformItemDetail from "$/form/choice_info_transformer/transform_item_detail"
 
 import BasicForm from "$/form/basic_form.svelte"
 import ChoiceListField from "$/form/choice_list_field.svelte"
 import TextField from "$/form/text_field.svelte"
+import ShortParagraph from "$/typography/short_paragraph.svelte"
 
 const ACCEPTABLE_ACCOUNT_KINDS = [ ...acceptableAccountKinds ]
+const ACCEPTABLE_VALUATION_METHODS = [ ...acceptableValuationMethods ]
 
 let {
 	IDPrefix,
 	currencies,
+	itemDetails,
 	currencyID = $bindable(),
 	name = $bindable(),
 	description = $bindable(),
 	kind = $bindable(),
+	configuration = $bindable(),
 	forceDisabledFields = [],
 	isConnecting,
 	errors,
@@ -30,11 +49,13 @@ let {
 }: {
 	IDPrefix: string
 	currencies: Currency[]
+	itemDetails: ItemDetail[]
 	currencyID: string
 	name: string
 	description: string
 	kind: AcceptableAccountKind
-	forceDisabledFields?: (keyof Account)[]
+	configuration: ItemConfigurationInput|null
+	forceDisabledFields?: (keyof Account|keyof ItemConfiguration)[]
 	isConnecting: boolean
 	errors: GeneralError[]
 	id?: string|null
@@ -43,6 +64,23 @@ let {
 } = $props()
 
 let accountKindSupportText = $derived(ACCOUNT_KIND_DESCRIPTIONS[kind])
+
+$effect(() => {
+	if (kind === ITEMIZED_ASSET_ACCOUNT_KIND) {
+		if (untrack(() => configuration) === null && itemDetails.length > 0) {
+			untrack(() => {
+				configuration = {
+					"item_detail_id": itemDetails[0].id,
+					"valuation_method": acceptableValuationMethods[0]
+				}
+			})
+		}
+	} else {
+		untrack(() => {
+			configuration = null
+		})
+	}
+})
 </script>
 
 <BasicForm {id} {isConnecting} {errors} {onsubmit} {button_group}>
@@ -77,5 +115,41 @@ let accountKindSupportText = $derived(ACCOUNT_KIND_DESCRIPTIONS[kind])
 			bind:value={description}
 			{IDPrefix}
 			{errors}/>
+		{#if kind === ITEMIZED_ASSET_ACCOUNT_KIND && configuration !== null}
+			{#if itemDetails.length === 0}
+				<ShortParagraph>
+					You must have at least one item detail before you can create an itemized asset account.
+				</ShortParagraph>
+			{:else}
+				{@const filledConfiguration = configuration}
+				{@const { item_detail_id, valuation_method } = filledConfiguration}
+				<ChoiceListField
+					fieldName="Item Detail"
+					disabled={isConnecting || forceDisabledFields.includes("item_detail_id")}
+					bind:value={
+						() => `${item_detail_id}`,
+						itemDetailID => {
+							configuration = { ...filledConfiguration, "item_detail_id": +itemDetailID }
+						}
+					}
+					rawChoices={itemDetails}
+					choiceConverter={transformItemDetail}
+					{IDPrefix}
+					{errors}/>
+				<ChoiceListField
+					fieldName="Valuation Method"
+					disabled={isConnecting || forceDisabledFields.includes("valuation_method")}
+					bind:value={
+						() => valuation_method,
+						valuationMethod => {
+							configuration = { ...filledConfiguration, "valuation_method": valuationMethod }
+						}
+					}
+					rawChoices={ACCEPTABLE_VALUATION_METHODS}
+					choiceConverter={transformString}
+					{IDPrefix}
+					{errors}/>
+			{/if}
+		{/if}
 	{/snippet}
 </BasicForm>
