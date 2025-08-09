00e40173829a519cbcb7202c6a1621d2563d3eb8
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-08-09 12:08:10 +0800

    feat(account): indicate available item configuration in the card

--- src/components/private/accounts/account_card.svelte
+++ src/components/private/accounts/account_card.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
-import type { Currency, Account, AcceptableAccountKind } from "+/entity"
+import type {
+	Currency,
+	Account,
+	AcceptableAccountKind,
+	ItemConfigurationInput,
+	ItemConfiguration,
+	ItemDetail
+} from "+/entity"
 
-import { acceptableAccountKinds } from "#/entity"
+import { untrack } from "svelte"
+
+import { acceptableAccountKinds, WEIGHTED_AVERAGE_VALUATION_METHOD } from "#/entity"
 
 import checkArchivedState from "$/utility/check_archived_state"
 import convertSnakeCaseToProperCase from "$/utility/convert_snake_case_to_proper_case"
@@ -14,11 +23,15 @@ import ShortParagraph from "$/typography/short_paragraph.svelte"
 
 let {
 	currencies,
+	itemDetails,
 	data = $bindable(),
+	subdata = $bindable(),
 	remove
 }: {
 	currencies: Currency[]
+	itemDetails: ItemDetail[]
 	data: Account
+	subdata: ItemConfiguration[]
 	remove: (resource: Account) => void
 } = $props()
 
@@ -26,6 +39,7 @@ let currencyID = $state(`${data.currency_id}`)
 let name = $state(data.name)
 let description = $state(data.description)
 let kind = $state(fallbackToAceptableKind(data.kind))
+let configuration = $state<ItemConfigurationInput|null>(null)
 let forceDisabledFields: (keyof Account)[] = [
 	"currency_id",
 	// "kind"
@@ -38,6 +52,32 @@ let associatedCurrency = $derived(currencies.find(
 	currency => currency.id === parseInt(currencyID)
 ))
 let friendlyKind = $derived(convertSnakeCaseToProperCase(data.kind).toLocaleLowerCase())
+let associatedItemConfigurations = $derived(subdata.filter(
+	configuration => configuration.account_id === data.id
+))
+let hasItemConfigurations = $derived(associatedItemConfigurations.length === 1)
+$effect(() => {
+	if (
+		hasItemConfigurations
+		&& JSON.stringify(untrack(() => configuration)) !== JSON.stringify(
+			associatedItemConfigurations[0]
+		)
+	) {
+		untrack(() => {
+			configuration = associatedItemConfigurations[0]
+		})
+	}
+})
+let associatedItemDetail = $derived(itemDetails.find(
+	itemDetail => itemDetail.id === configuration?.item_detail_id
+) ?? null)
+let friendlyValuationMethod = $derived(
+	hasItemConfigurations
+	&& associatedItemConfigurations[0].valuation_method === WEIGHTED_AVERAGE_VALUATION_METHOD
+		? "weighted average"
+		: ""
+)
+
 let restorableItemOptions = $derived(makeRestorableItemOptions(
 	`/api/v2/accounts/${data.id}`,
 	{
@@ -49,6 +89,17 @@ let restorableItemOptions = $derived(makeRestorableItemOptions(
 				description,
 				kind
 			}
+
+			subdata = subdata.map(oldConfiguration => {
+				if (oldConfiguration.account_id === data.id) {
+					return {
+						...oldConfiguration,
+						...configuration
+					}
+				}
+
+				return oldConfiguration
+			})
 		},
 		"removeCacheData": () => remove(data),
 		"makeUpdatedBody": () => ({
@@ -56,7 +107,12 @@ let restorableItemOptions = $derived(makeRestorableItemOptions(
 				"currency_id": parseInt(currencyID),
 				name,
 				description,
-				kind
+				kind,
+				...(configuration === null ? {} : {
+					"@relationship": {
+						"item_configuration": configuration
+					}
+				})
 			}
 		})
 	}
@@ -90,8 +146,10 @@ function isAcceptable(kind: string): kind is AcceptableAccountKind {
 			bind:name={name}
 			bind:description={description}
 			bind:kind={kind}
+			bind:configuration={configuration}
 			{IDPrefix}
 			{currencies}
+			{itemDetails}
 			{isConnecting}
 			{errors}
 			{forceDisabledFields}
@@ -132,8 +190,10 @@ function isAcceptable(kind: string): kind is AcceptableAccountKind {
 			<ShortParagraph>{data.description}</ShortParagraph>
 		{/if}
 		<ShortParagraph>
-			This {friendlyKind} account uses {associatedCurrency?.code ?? "???"} as its currency. The
-			account ID is {data.id}.
+			This {friendlyKind} account uses {associatedCurrency?.code ?? "???"} as its currency. {#if
+			hasItemConfigurations && associatedItemDetail !== null}The items in this account are
+			denoted by <em>{associatedItemDetail.unit}</em> with {friendlyValuationMethod} as its
+			valuation method.{/if} The account ID is {data.id}.
 		</ShortParagraph>
 	{/snippet}
 </CardItem>
