e7191dae29d2cfe83549e71c8e714eba4802c818
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-08-09 12:17:14 +0800

    internal(component): make container for support texts

---
+++ src/components/public/typography/supporting_text.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import type { Snippet } from "svelte"
+
+let { children }: {
+	children: Snippet
+} = $props()
+</script>
+
+<p class="supporting-text">
+	{@render children()}
+</p>
+
+<style lang="scss">
+p {
+	width: 100%;
+}
+</style>
