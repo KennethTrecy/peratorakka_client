bb5a464ca3e6ec3ed5372b9fba7863a5a4682ffd
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2026-02-07 21:21:46 +0800

    feat(frozen period): show exact amounts as title for total amount rows

--- src/components/private/frozen_periods/financial_statements/cash_flow_statement/total_amount_row.svelte
+++ src/components/private/frozen_periods/financial_statements/cash_flow_statement/total_amount_row.svelte
@@ -7,19 +7,22 @@ import DataTableRow from "$/catalog/data_table_row.svelte"
 let {
 	rowName,
 	shownAmount,
+	exactAmount,
 	hasEmptyTrailingRow = false
 }: {
 	rowName: string
 	shownAmount: string
+	exactAmount: string
 	hasEmptyTrailingRow?: boolean
 } = $props()
 
 let isNegative = $derived(shownAmount.indexOf(")") > -1)
+let title = $derived(`Exact value is ${exactAmount}`)
 </script>
 
 <DataTableRow>
 	<DataTableHeader scope="row" columnSpan={2}>{rowName}</DataTableHeader>
-	<DataTableCell kind="numeric"><AmountDisplay {shownAmount}/>{#if isNegative}<span aria-hidden="true">)</span>{/if}</DataTableCell>
+	<DataTableCell kind="numeric" {title}><AmountDisplay {shownAmount}/>{#if isNegative}<span aria-hidden="true">)</span>{/if}</DataTableCell>
 </DataTableRow>
 {#if hasEmptyTrailingRow}
 	<DataTableRow/>
